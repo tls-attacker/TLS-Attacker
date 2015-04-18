2846c60cd4ae70cf6d878b785a379cbe06d88df0
Florian Pfützenreuter <florian.pfuetzenreuter@rub.de>
2015-04-18 16:37:19 +0200

    Add testPrepareMessage to ClientHelloHandlerTest

--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/handlers/ClientHelloHandlerTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/handlers/ClientHelloHandlerTest.java
@@ -17,12 +17,22 @@
  */
 package de.rub.nds.tlsattacker.tls.protocol.handshake.handlers;
 
+import de.rub.nds.tlsattacker.tls.constants.ProtocolVersion;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.constants.CipherSuite;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.constants.CompressionMethod;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.constants.HandshakeMessageType;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.messages.ClientHelloMessage;
 import de.rub.nds.tlsattacker.tls.workflow.TlsContext;
+import de.rub.nds.tlsattacker.util.ArrayConverter;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * 
  * @author Juraj Somorovsky <juraj.somorovsky@rub.de>
+ * @author Florian Pfützenreuter <florian.pfuetzenreuter@rub.de>
  */
 public class ClientHelloHandlerTest {
 
@@ -37,6 +47,31 @@ public class ClientHelloHandlerTest {
      */
     @Test
     public void testPrepareMessage() {
-	// TODO Florian
+	handler.initializeProtocolMessage();
+        
+        ClientHelloMessage message = (ClientHelloMessage) handler.getProtocolMessage(); 
+        
+        List<CipherSuite> cipherSuites = new ArrayList();
+        cipherSuites.add(CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384);
+        message.setSupportedCipherSuites(cipherSuites);
+        
+        List<CompressionMethod> compressionMethods = new ArrayList();
+        compressionMethods.add(CompressionMethod.NULL);
+        message.setSupportedCompressionMethods(compressionMethods);      
+        
+        byte[] returned = handler.prepareMessageAction();
+        byte[] expected = ArrayConverter.concatenate(
+                new byte[]{HandshakeMessageType.CLIENT_HELLO.getValue()},
+                new byte[]{0x00, 0x00, 0x29},
+                ProtocolVersion.TLS12.getValue(),
+                message.getUnixTime().getValue(),
+                message.getRandom().getValue(),
+                new byte[]{0x00, 0x00, 0x02},
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384.getValue(),
+                new byte[]{0x01, CompressionMethod.NULL.getValue()});
+        
+        assertNotNull("Confirm function didn't return 'NULL'", returned); 
+        assertArrayEquals("Confirm returned message equals the expected message",
+                expected, returned);
     }
 }
