6a76e7afabeb8dd5fb8388469e2ca1e09fed15c1
Florian Pfützenreuter <florian.pfuetzenreuter@rub.de>
2015-06-08 18:03:32 +0200

    Create DTLS handshake and record messages

---
+++ TLS/src/main/java/de/rub/nds/tlsattacker/dtls/protocol/messages/ClientHelloMessage.java
@@ -0,0 +1,44 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS.
+ *
+ * Copyright (C) 2015 Florian Pfützenreuter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.rub.nds.tlsattacker.dtls.protocol.handshake.messages;
+
+import de.rub.nds.tlsattacker.modifiablevariable.ModifiableVariableFactory;
+import de.rub.nds.tlsattacker.modifiablevariable.ModifiableVariableProperty;
+import de.rub.nds.tlsattacker.modifiablevariable.bytearray.ModifiableByteArray;
+
+/**
+ * @author Florian Pfützenreuter <Florian.Pfuetzenreuter@rub.de>
+ */
+public class ClientHelloMessage
+extends de.rub.nds.tlsattacker.tls.protocol.handshake.messages.ClientHelloMessage{
+    
+    @ModifiableVariableProperty(type = ModifiableVariableProperty.Type.COOKIE)
+    ModifiableByteArray cookie;
+    
+    public ModifiableByteArray getCookie() {
+        return cookie;
+    }
+    
+    public void setCookie(byte[] cookie) {
+        this.cookie = ModifiableVariableFactory.safelySetValue(this.cookie, cookie);
+    }
+    
+    public void setCookie(ModifiableByteArray cookie) {
+        this.cookie = cookie;
+    }
+}
