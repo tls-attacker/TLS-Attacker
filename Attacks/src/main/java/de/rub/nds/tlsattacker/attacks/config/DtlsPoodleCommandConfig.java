a5e5adfc3240f0ac28ecd5efd9a43a0b3b5a39c2
Florian Pfützenreuter <florian.pfuetzenreuter@rub.de>
2015-07-29 02:57:46 +0200

    Add DtlsPoodleAttack.

---
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/config/DtlsPoodleCommandConfig.java
@@ -0,0 +1,72 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS.
+ *
+ * Copyright (C) 2015 Chair for Network and Data Security,
+ *                    Ruhr University Bochum
+ *                    (juraj.somorovsky@rub.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.rub.nds.tlsattacker.attacks.config;
+
+import com.beust.jcommander.Parameter;
+import de.rub.nds.tlsattacker.tls.config.ClientCommandConfig;
+import de.rub.nds.tlsattacker.tls.constants.ProtocolVersion;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.constants.CipherSuite;
+import de.rub.nds.tlsattacker.tls.workflow.WorkflowTraceType;
+import de.rub.nds.tlsattacker.transport.TransportHandlerType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Florian Pfützenreuter <florian.pfuetzenreuter@rub.de>
+ */
+public class DtlsPoodleCommandConfig extends ClientCommandConfig {
+
+    public static final String ATTACK_COMMAND = "dtls_poodle";
+
+    @Parameter(names = "-messagespertrain", description = "number of messages per train")
+    public int messagesPerTrain = 10;
+
+    @Parameter(names = "-trainmessagesize", description = "message size of a trains messages")
+    public int trainMessageSize = 1450;
+
+    public DtlsPoodleCommandConfig() {
+	// Just to be sure
+	transportHandlerType = TransportHandlerType.UDP;
+	protocolVersion = ProtocolVersion.DTLS12;
+	workflowTraceType = WorkflowTraceType.HANDSHAKE;
+
+	// Until all dtls workflow factories are adapted, this is to make sure
+	// the right workflow factory is used
+	List<CipherSuite> cs = new ArrayList<>();
+	cs.add(CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA);
+	cipherSuites = cs;
+    }
+
+    public int getMessagesPerTrain() {
+	return messagesPerTrain;
+    }
+
+    public int getTrainMessageSize() {
+	return trainMessageSize;
+    }
+
+    public void setMessagesPerTrain(int messagesPerTrain) {
+	this.messagesPerTrain = messagesPerTrain;
+    }
+
+    public void setTrainMessageSize(int trainMessageSize) {
+	this.trainMessageSize = trainMessageSize;
+    }
+}
