eb08ed800057722e9fc16f61a979a4d2d627560b
Juraj Somorovsky <juraj.somorovsky@gmail.com>
2015-09-18 10:43:31 +0200

    ModifiableLong

---
+++ ModifiableVariable/src/main/java/de/rub/nds/tlsattacker/modifiablevariable/mlong/LongXorModification.java
@@ -0,0 +1,55 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS.
+ *
+ * Copyright (C) 2015 Chair for Network and Data Security,
+ *                    Ruhr University Bochum
+ *                    (juraj.somorovsky@rub.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.rub.nds.tlsattacker.modifiablevariable.mlong;
+
+import de.rub.nds.tlsattacker.modifiablevariable.VariableModification;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * @author Juraj Somorovsky - juraj.somorovsky@rub.de
+ */
+@XmlRootElement
+@XmlType(propOrder = { "xor", "modificationFilter", "postModification" })
+public class LongXorModification extends VariableModification<Long> {
+
+    private Long xor;
+
+    public LongXorModification() {
+
+    }
+
+    public LongXorModification(Long bi) {
+	this.xor = bi;
+    }
+
+    @Override
+    protected Long modifyImplementationHook(final Long input) {
+	return input ^ xor;
+    }
+
+    public Long getXor() {
+	return xor;
+    }
+
+    public void setXor(Long xor) {
+	this.xor = xor;
+    }
+}
---
+++ ModifiableVariable/src/main/java/de/rub/nds/tlsattacker/modifiablevariable/mlong/ModifiableLong.java
@@ -0,0 +1,57 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS.
+ *
+ * Copyright (C) 2015 Chair for Network and Data Security,
+ *                    Ruhr University Bochum
+ *                    (juraj.somorovsky@rub.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.rub.nds.tlsattacker.modifiablevariable.mlong;
+
+import de.rub.nds.tlsattacker.modifiablevariable.ModifiableVariable;
+import de.rub.nds.tlsattacker.modifiablevariable.VariableModification;
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * 
+ * @author Juraj Somorovsky - juraj.somorovsky@rub.de
+ */
+@XmlRootElement
+@XmlSeeAlso({ LongAddModification.class, LongExplicitValueModification.class, LongSubtractModification.class,
+	LongXorModification.class })
+@XmlType(propOrder = { "originalValue", "modification" })
+public class ModifiableLong extends ModifiableVariable<Long> implements Serializable {
+
+    @Override
+    protected void createRandomModification() {
+	VariableModification<Long> vm = LongModificationFactory.createRandomModification();
+	setModification(vm);
+    }
+
+    public Long getOriginalValue() {
+	return originalValue;
+    }
+
+    public void setOriginalValue(Long originalValue) {
+	this.originalValue = originalValue;
+    }
+
+    @Override
+    public boolean isOriginalValueModified() {
+	return originalValue != null && originalValue.compareTo(getValue()) != 0;
+    }
+}
