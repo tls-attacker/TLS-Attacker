31f5823654a6d839df8ebb0f04c2302a1c7917ea
philiprisen <philip_riese@web.de>
2015-10-05 16:33:37 +0200

    Test Methods added

--- ModifiableVariable/src/test/java/de/rub/nds/tlsattacker/modifiablevariable/mlong/LongModificationTest.java
+++ ModifiableVariable/src/test/java/de/rub/nds/tlsattacker/modifiablevariable/mlong/LongModificationTest.java
@@ -19,6 +19,8 @@
  */
 package de.rub.nds.tlsattacker.modifiablevariable.mlong;
 
+import de.rub.nds.tlsattacker.modifiablevariable.VariableModification;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,29 +31,60 @@ import org.junit.Test;
  */
 public class LongModificationTest {
 
+    private ModifiableLong start;
+
+    private Long expectedResult, result;
+
     @Before
     public void setUp() {
-	
+
+	start = new ModifiableLong();
+	start.setOriginalValue(10L);
+	expectedResult = null;
+	result = null;
+
     }
 
     @Test
     public void testAdd() {
-	
+
+	VariableModification<Long> modifier = LongModificationFactory.add(1L);
+	start.setModification(modifier);
+	expectedResult = 11L;
+	result = start.getValue();
+	assertEquals(expectedResult, result);
+	assertEquals(new Long(10L), start.getOriginalValue());
+
     }
 
     @Test
     public void testSub() {
-	
+	VariableModification<Long> modifier = LongModificationFactory.sub(1L);
+	start.setModification(modifier);
+	expectedResult = 9L;
+	result = start.getValue();
+	assertEquals(expectedResult, result);
+	assertEquals(new Long(10L), start.getOriginalValue());
     }
 
     @Test
     public void testXor() {
-	
+	VariableModification<Long> modifier = LongModificationFactory.xor(2L);
+	start.setModification(modifier);
+	expectedResult = 8L;
+	result = start.getValue();
+	assertEquals(expectedResult, result);
+	assertEquals(new Long(10L), start.getOriginalValue());
     }
 
     @Test
     public void testExplicitValue() {
-	
+	VariableModification<Long> modifier = LongModificationFactory.explicitValue(7L);
+	start.setModification(modifier);
+	expectedResult = 7L;
+	result = start.getValue();
+	assertEquals(expectedResult, result);
+	assertEquals(new Long(10L), start.getOriginalValue());
     }
 
 }
