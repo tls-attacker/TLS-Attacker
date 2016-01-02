6e6c80e359aea663504dd4b00957f4404f1521c7
Juraj Somorovsky <juraj.somorovsky@gmail.com>
2016-01-02 17:53:05 +0100

    SNI test allows one to send an additional server name indication extension in the renegotiation message

---
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/config/SniTestCommandConfig.java
@@ -0,0 +1,50 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS.
+ *
+ * Copyright (C) 2015 Chair for Network and Data Security,
+ *                    Ruhr University Bochum
+ *                    (juraj.somorovsky@rub.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.rub.nds.tlsattacker.attacks.config;
+
+import com.beust.jcommander.Parameter;
+import de.rub.nds.tlsattacker.tls.config.ClientCommandConfig;
+import de.rub.nds.tlsattacker.tls.constants.CipherSuite;
+import java.util.LinkedList;
+
+/**
+ * 
+ * @author Juraj Somorovsky <juraj.somorovsky@rub.de>
+ */
+public class SniTestCommandConfig extends ClientCommandConfig {
+
+    public static final String ATTACK_COMMAND = "sni_test";
+
+    @Parameter(names = "-server_name2", description = "Servername for HostName TLS extension, used in the second ClientHello message.")
+    protected String serverName2;
+
+    public SniTestCommandConfig() {
+	cipherSuites = new LinkedList<>();
+	cipherSuites.add(CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA);
+    }
+
+    public String getServerName2() {
+	return serverName2;
+    }
+
+    public void setServerName2(String serverName2) {
+	this.serverName2 = serverName2;
+    }
+}
---
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/SniTest.java
@@ -0,0 +1,75 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS.
+ *
+ * Copyright (C) 2015 Chair for Network and Data Security, Ruhr University
+ * Bochum (juraj.somorovsky@rub.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.rub.nds.tlsattacker.attacks.impl;
+
+import de.rub.nds.tlsattacker.attacks.config.SniTestCommandConfig;
+import de.rub.nds.tlsattacker.tls.Attacker;
+import de.rub.nds.tlsattacker.tls.config.ConfigHandler;
+import de.rub.nds.tlsattacker.tls.constants.NameType;
+import de.rub.nds.tlsattacker.tls.protocol.ProtocolMessage;
+import de.rub.nds.tlsattacker.tls.protocol.extension.messages.ServerNameIndicationExtensionMessage;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.messages.CertificateMessage;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.messages.ClientHelloMessage;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.messages.ServerHelloMessage;
+import de.rub.nds.tlsattacker.tls.workflow.TlsContext;
+import de.rub.nds.tlsattacker.tls.workflow.WorkflowExecutor;
+import de.rub.nds.tlsattacker.tls.workflow.WorkflowTrace;
+import de.rub.nds.tlsattacker.transport.TransportHandler;
+import de.rub.nds.tlsattacker.util.UnoptimizedDeepCopy;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Sends different server names in the SNI extension in the ClientHello
+ * messages.
+ * 
+ * @author Juraj Somorovsky <juraj.somorovsky@rub.de>
+ */
+public class SniTest extends Attacker<SniTestCommandConfig> {
+
+    public static Logger LOGGER = LogManager.getLogger(SniTest.class);
+
+    public SniTest(SniTestCommandConfig config) {
+	super(config);
+    }
+
+    @Override
+    public void executeAttack(ConfigHandler configHandler) {
+	TransportHandler transportHandler = configHandler.initializeTransportHandler(config);
+	TlsContext tlsContext = configHandler.initializeTlsContext(config);
+	WorkflowExecutor workflowExecutor = configHandler.initializeWorkflowExecutor(transportHandler, tlsContext);
+
+	WorkflowTrace trace = tlsContext.getWorkflowTrace();
+	List<ProtocolMessage> messages = trace.getProtocolMessages();
+	ServerNameIndicationExtensionMessage sni = new ServerNameIndicationExtensionMessage();
+	sni.setServerNameConfig(config.getServerName2());
+	sni.setNameTypeConfig(NameType.HOST_NAME);
+	ClientHelloMessage ch2 = (ClientHelloMessage) UnoptimizedDeepCopy.copy(messages.get(0));
+	ch2.addExtension(sni);
+	messages.add(ch2);
+	messages.add(new ServerHelloMessage());
+	messages.add(new CertificateMessage());
+
+	workflowExecutor.executeWorkflow();
+	transportHandler.closeConnection();
+
+    }
+
+}
