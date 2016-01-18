f4d4bf0f3c4c4a1aa3e8db200b87fa90a6e67176
Juraj Somorovsky <juraj.somorovsky@gmail.com>
2016-01-18 12:24:17 +0100

    Server now responds with an ack message

--- TLS-Server/src/main/java/de/rub/nds/tlsattacker/tlsserver/ConnectionHandler.java
+++ TLS-Server/src/main/java/de/rub/nds/tlsattacker/tlsserver/ConnectionHandler.java
@@ -20,8 +20,10 @@
 package de.rub.nds.tlsattacker.tlsserver;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -52,9 +54,12 @@ public class ConnectionHandler implements Runnable {
 
 	try {
 	    final BufferedReader br = new BufferedReader(new InputStreamReader(applicationSocket.getInputStream()));
+	    final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(applicationSocket.getOutputStream()));
 	    String line = "";
 	    while ((line = br.readLine()) != null) {
 		LOGGER.debug(line);
+		bw.write("ack");
+		bw.flush();
 	    }
 	} catch (IOException e) {
 	    LOGGER.debug(e.getLocalizedMessage(), e);
