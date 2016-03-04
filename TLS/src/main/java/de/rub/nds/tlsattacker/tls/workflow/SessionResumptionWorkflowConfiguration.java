c4075612e4cd14a092fbf9f1421868bcc7d653b3
philiprisen <philip_riese@web.de>
2016-03-04 15:24:53 +0100

    improved workflow generation

--- TLS/src/main/java/de/rub/nds/tlsattacker/tls/workflow/SessionResumptionWorkflowConfiguration.java
+++ TLS/src/main/java/de/rub/nds/tlsattacker/tls/workflow/SessionResumptionWorkflowConfiguration.java
@@ -19,14 +19,19 @@
  */
 package de.rub.nds.tlsattacker.tls.workflow;
 
+import de.rub.nds.tlsattacker.tls.config.ClientCommandConfig;
+import de.rub.nds.tlsattacker.tls.config.CommandConfig;
+import de.rub.nds.tlsattacker.tls.config.ServerCommandConfig;
 import de.rub.nds.tlsattacker.tls.constants.ConnectionEnd;
+import de.rub.nds.tlsattacker.tls.exceptions.ConfigurationException;
 import de.rub.nds.tlsattacker.tls.protocol.ProtocolMessage;
-import de.rub.nds.tlsattacker.tls.protocol.ProtocolMessageTypeHolder;
 import de.rub.nds.tlsattacker.tls.protocol.application.ApplicationMessage;
 import de.rub.nds.tlsattacker.tls.protocol.ccs.ChangeCipherSpecMessage;
 import de.rub.nds.tlsattacker.tls.protocol.handshake.ClientHelloMessage;
 import de.rub.nds.tlsattacker.tls.protocol.handshake.FinishedMessage;
 import de.rub.nds.tlsattacker.tls.protocol.handshake.ServerHelloMessage;
+import static de.rub.nds.tlsattacker.tls.workflow.WorkflowConfigurationFactory.initializeClientHelloExtensions;
+import static de.rub.nds.tlsattacker.tls.workflow.WorkflowConfigurationFactory.initializeProtocolMessageOrder;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -37,19 +42,60 @@ import java.util.List;
  */
 public class SessionResumptionWorkflowConfiguration {
 
-    public final TlsContext tlsContext;
+    private final TlsContext tlsContext;
+    private final CommandConfig config;
 
-    public SessionResumptionWorkflowConfiguration(TlsContext tlsContext) {
+    public SessionResumptionWorkflowConfiguration(TlsContext tlsContext, CommandConfig config) {
 	this.tlsContext = tlsContext;
+	this.config = config;
     }
 
-    public void createHandshakeWorkflow() {
+    public void createWorkflow() {
+	tlsContext.setSessionResumption(true);
+	tlsContext.getDigest().reset();
+	WorkflowTraceType workflowTraceType;
+	if (tlsContext.getMyConnectionEnd() == ConnectionEnd.CLIENT) {
+	    ClientCommandConfig ccConfig = (ClientCommandConfig) config;
+	    workflowTraceType = ccConfig.getWorkflowTraceType();
+	} else {
+	    ServerCommandConfig ccConfig = (ServerCommandConfig) config;
+	    workflowTraceType = ccConfig.getWorkflowTraceType();
+	}
+
+	WorkflowTrace workflowTrace;
+
+	switch (workflowTraceType) {
+	    case FULL_SERVER_RESPONSE:
+		workflowTrace = createFullSRWorkflow();
+		break;
+	    case FULL:
+		workflowTrace = createFullWorkflow();
+		break;
+	    case HANDSHAKE:
+		workflowTrace = createHandshakeWorkflow();
+		break;
+	    default:
+		throw new ConfigurationException("not supported workflow type: " + workflowTraceType);
+	}
+
+	tlsContext.setWorkflowTrace(workflowTrace);
+
+	initializeProtocolMessageOrder(tlsContext);
+    }
+
+    private WorkflowTrace createHandshakeWorkflow() {
 
 	WorkflowTrace workflowTrace = new WorkflowTrace();
 
 	List<ProtocolMessage> protocolMessages = new LinkedList<>();
 
-	protocolMessages.add(new ClientHelloMessage(ConnectionEnd.CLIENT));
+	ClientHelloMessage ch = new ClientHelloMessage(ConnectionEnd.CLIENT);
+	protocolMessages.add(ch);
+
+	ch.setSupportedCipherSuites(config.getCipherSuites());
+	ch.setSupportedCompressionMethods(config.getCompressionMethods());
+
+	initializeClientHelloExtensions(config, ch);
 
 	protocolMessages.add(new ServerHelloMessage(ConnectionEnd.SERVER));
 
@@ -61,57 +107,34 @@ public class SessionResumptionWorkflowConfiguration {
 
 	workflowTrace.setProtocolMessages(protocolMessages);
 
-	tlsContext.setWorkflowTrace(workflowTrace);
-
-	initializeProtocolMessageOrder(tlsContext);
-
-	tlsContext.setSessionResumption(true);
+	return workflowTrace;
 
     }
 
-    public void createFullWorkflow() {
+    private WorkflowTrace createFullWorkflow() {
 
-	WorkflowTrace workflowTrace = new WorkflowTrace();
+	WorkflowTrace workflowTrace = this.createHandshakeWorkflow();
 
-	List<ProtocolMessage> protocolMessages = new LinkedList<>();
-
-	protocolMessages.add(new ClientHelloMessage(ConnectionEnd.CLIENT));
-
-	protocolMessages.add(new ServerHelloMessage(ConnectionEnd.SERVER));
-
-	protocolMessages.add(new ChangeCipherSpecMessage(ConnectionEnd.SERVER));
-	protocolMessages.add(new FinishedMessage(ConnectionEnd.SERVER));
-
-	protocolMessages.add(new ChangeCipherSpecMessage(ConnectionEnd.CLIENT));
-	protocolMessages.add(new FinishedMessage(ConnectionEnd.CLIENT));
+	List<ProtocolMessage> protocolMessages = workflowTrace.getProtocolMessages();
 
 	protocolMessages.add(new ApplicationMessage(ConnectionEnd.CLIENT));
+
+	workflowTrace.setProtocolMessages(protocolMessages);
+
+	return workflowTrace;
+    }
+
+    private WorkflowTrace createFullSRWorkflow() {
+
+	WorkflowTrace workflowTrace = this.createFullWorkflow();
+
+	List<ProtocolMessage> protocolMessages = workflowTrace.getProtocolMessages();
+
 	protocolMessages.add(new ApplicationMessage(ConnectionEnd.SERVER));
 
 	workflowTrace.setProtocolMessages(protocolMessages);
 
-	tlsContext.setWorkflowTrace(workflowTrace);
-
-	initializeProtocolMessageOrder(tlsContext);
-
-	tlsContext.setSessionResumption(true);
-
-    }
-
-    /**
-     * Initializes the preconfigured protocol message order according to the
-     * workflow trace. This protocol message order can be used to compare the
-     * configured and real message order.
-     * 
-     * @param context
-     */
-    public static void initializeProtocolMessageOrder(TlsContext context) {
-	List<ProtocolMessageTypeHolder> configuredProtocolMessageOrder = new LinkedList<>();
-	for (ProtocolMessage pm : context.getWorkflowTrace().getProtocolMessages()) {
-	    ProtocolMessageTypeHolder pmth = new ProtocolMessageTypeHolder(pm);
-	    configuredProtocolMessageOrder.add(pmth);
-	}
-	context.setPreconfiguredProtocolMessages(configuredProtocolMessageOrder);
+	return workflowTrace;
     }
 
 }
