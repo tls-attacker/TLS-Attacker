c7382cac90478caf0ff5f45a6f7bb30a7c1c46b5
philiprisen <philip_riese@web.de>
2016-03-23 11:45:48 +0100

    set from the new negotiated attributes a new TLSRecordCipher

--- TLS/src/main/java/de/rub/nds/tlsattacker/tls/protocol/ccs/ChangeCipherSpecHandler.java
+++ TLS/src/main/java/de/rub/nds/tlsattacker/tls/protocol/ccs/ChangeCipherSpecHandler.java
@@ -53,26 +53,26 @@ public class ChangeCipherSpecHandler extends ProtocolMessageHandler<ChangeCipher
     @Override
     public int parseMessageAction(byte[] message, int pointer) {
 	if (tlsContext.getMyConnectionEnd() == ConnectionEnd.SERVER || tlsContext.isSessionResumption()) {
-	    if(tlsContext.isRenegotiation() && tlsContext.getMyConnectionEnd() == ConnectionEnd.SERVER){
-                try {
+	    if (tlsContext.isRenegotiation() && tlsContext.getMyConnectionEnd() == ConnectionEnd.SERVER) {
+		try {
 		    TlsRecordBlockCipher tlsRecordBlockCipher = new TlsRecordBlockCipher(tlsContext);
 		    tlsContext.getRecordHandler().setRecordCipher(tlsRecordBlockCipher);
-                } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
-                        | InvalidKeyException ex) {
-                    throw new CryptoException(ex);
-                }
-            }else {
-                try {
-                    if (tlsContext.getRecordHandler().getRecordCipher() == null) {
-                        TlsRecordBlockCipher tlsRecordBlockCipher = new TlsRecordBlockCipher(tlsContext);
-                        tlsContext.getRecordHandler().setRecordCipher(tlsRecordBlockCipher);
-                    }
-                } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
-                        | InvalidKeyException ex) {
-                    throw new CryptoException(ex);
-                }
-            }
-        }
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
+			| InvalidKeyException ex) {
+		    throw new CryptoException(ex);
+		}
+	    } else {
+		try {
+		    if (tlsContext.getRecordHandler().getRecordCipher() == null) {
+			TlsRecordBlockCipher tlsRecordBlockCipher = new TlsRecordBlockCipher(tlsContext);
+			tlsContext.getRecordHandler().setRecordCipher(tlsRecordBlockCipher);
+		    }
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
+			| InvalidKeyException ex) {
+		    throw new CryptoException(ex);
+		}
+	    }
+	}
 	protocolMessage.setCcsProtocolType(message[pointer]);
 	return pointer + 1;
     }
