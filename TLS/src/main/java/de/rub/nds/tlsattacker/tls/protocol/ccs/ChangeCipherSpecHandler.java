d6ee53e54036a2531c1c9663bae6ba8ad210bd4e
philiprisen <philip_riese@web.de>
2016-04-10 22:35:15 +0200

    public method for BlockCipher creation and invocation in prepare/parse MessageAction

--- TLS/src/main/java/de/rub/nds/tlsattacker/tls/protocol/ccs/ChangeCipherSpecHandler.java
+++ TLS/src/main/java/de/rub/nds/tlsattacker/tls/protocol/ccs/ChangeCipherSpecHandler.java
@@ -45,36 +45,37 @@ public class ChangeCipherSpecHandler extends ProtocolMessageHandler<ChangeCipher
     @Override
     public byte[] prepareMessageAction() {
 	protocolMessage.setCcsProtocolType(CCS_PROTOCOL_TYPE);
-
+	if ((tlsContext.isRenegotiation() && tlsContext.getMyConnectionEnd() == ConnectionEnd.CLIENT)
+		|| tlsContext.getRecordHandler().getRecordCipher() == null) {
+	    setRecordCipher();
+	}
 	byte[] result = { protocolMessage.getCcsProtocolType().getValue() };
 	return result;
     }
 
     @Override
     public int parseMessageAction(byte[] message, int pointer) {
-	if (tlsContext.getMyConnectionEnd() == ConnectionEnd.SERVER || tlsContext.isSessionResumption()) {
-	    if (tlsContext.isRenegotiation() && tlsContext.getMyConnectionEnd() == ConnectionEnd.SERVER) {
-		try {
-		    TlsRecordBlockCipher tlsRecordBlockCipher = new TlsRecordBlockCipher(tlsContext);
-		    tlsContext.getRecordHandler().setRecordCipher(tlsRecordBlockCipher);
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
-			| InvalidKeyException ex) {
-		    throw new CryptoException(ex);
-		}
-	    } else {
-		try {
-		    if (tlsContext.getRecordHandler().getRecordCipher() == null) {
-			TlsRecordBlockCipher tlsRecordBlockCipher = new TlsRecordBlockCipher(tlsContext);
-			tlsContext.getRecordHandler().setRecordCipher(tlsRecordBlockCipher);
-		    }
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
-			| InvalidKeyException ex) {
-		    throw new CryptoException(ex);
-		}
-	    }
+	if ((tlsContext.isRenegotiation() && tlsContext.getMyConnectionEnd() == ConnectionEnd.SERVER)
+		|| tlsContext.getRecordHandler().getRecordCipher() == null) {
+	    setRecordCipher();
 	}
 	protocolMessage.setCcsProtocolType(message[pointer]);
 	return pointer + 1;
     }
 
+    public void setRecordCipher() {
+	try {
+	    TlsRecordBlockCipher tlsRecordBlockCipher = new TlsRecordBlockCipher(tlsContext);
+	    tlsContext.getRecordHandler().setRecordCipher(tlsRecordBlockCipher);
+	} catch (InvalidKeyException ex) {
+	    throw new CryptoException(
+		    "It was not possible to initialize an algorithm from "
+			    + tlsContext.getSelectedCipherSuite()
+			    + ". Most probably your platform does not support unlimited policy strength and you have to "
+			    + "install Java Cryptography Extension (JCE) Unlimited Strength Jurisdiction Policy Files. Stupid, I know.",
+		    ex);
+	} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException ex) {
+	    throw new CryptoException(ex);
+	}
+    }
 }
