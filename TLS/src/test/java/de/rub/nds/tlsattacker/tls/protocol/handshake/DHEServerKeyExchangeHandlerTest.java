d779be101fcd4d8e58aa6d8043ac73ab87f6a7c2
philiprisen <philip_riese@web.de>
2016-04-18 18:09:18 +0200

    extended testPrepareMessageRSA()

--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/DHEServerKeyExchangeHandlerTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/DHEServerKeyExchangeHandlerTest.java
@@ -135,10 +135,30 @@ public class DHEServerKeyExchangeHandlerTest {
 	DHEServerKeyExchangeMessage message = (DHEServerKeyExchangeMessage) handler.getProtocolMessage();
 
 	byte[] result = handler.prepareMessageAction();
-
+        
+        byte [] pTestArray = ArrayConverter
+                .hexStringToByteArray("ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc"
+                    + "74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d"
+                    + "51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24"
+                    + "117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83"
+                    + "655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca1821"
+                    + "7c32905e462e36ce3be39e772c180e86039b2783a2ec07a28fb5c55df06f4c52c9de2bcbf695"
+                    + "5817183995497cea956ae515d2261898fa051015728e5a8aacaa68ffffffffffffffff");
+        BigInteger pTest = new BigInteger(1, pTestArray);
+        
 	assertNotNull("Confirm function didn't return 'NULL'", result);
 	assertEquals("Message type must be ServerKeyExchange", HandshakeMessageType.SERVER_KEY_EXCHANGE,
 		message.getHandshakeMessageType());
+	assertEquals("p length must be 256", new Integer(256), message.getSerializedPLength().getValue());
+        assertEquals("p must be pTest", pTest, message.getP().getValue());
+	assertEquals("g length must be 1", new Integer(1), message.getSerializedGLength().getValue());
+	assertEquals("g must be 2", new BigInteger("2"), message.getG().getValue());
+        assertEquals("Public key length is 256", new Integer(256), message.getSerializedPublicKeyLength().getValue());
+	assertEquals("Hash must be SHA1", HashAlgorithm.SHA1,
+		HashAlgorithm.getHashAlgorithm(message.getHashAlgorithm().getValue()));
+	assertEquals("Signature must be RSA", SignatureAlgorithm.RSA,
+		SignatureAlgorithm.getSignatureAlgorithm(message.getSignatureAlgorithm().getValue()));
+	assertEquals("Signature length must be 128", new Integer(128), message.getSignatureLength().getValue());
 
     }
 }
