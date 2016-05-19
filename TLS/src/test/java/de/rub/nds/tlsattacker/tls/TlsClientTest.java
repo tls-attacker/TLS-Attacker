206a6629c47863523516c8a91b525de9825bceb6
Juraj Somorovsky <juraj.somorovsky@gmail.com>
2016-05-19 13:16:27 +0200

    Modified the license

--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/TlsClientTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/TlsClientTest.java
@@ -1,20 +1,10 @@
 /**
- * TLS-Attacker - A Modular Penetration Testing Framework for TLS.
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
  *
- * Copyright (C) 2015 Chair for Network and Data Security, Ruhr University
- * Bochum (juraj.somorovsky@rub.de)
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
+ * Licensed under Apache License 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
  */
 package de.rub.nds.tlsattacker.tls;
 
