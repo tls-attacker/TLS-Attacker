dfb00deef9a175c785bc673a2d27f1ca6c3db963
Juraj Somorovsky <juraj.somorovsky@gmail.com>
2016-05-31 12:08:30 +0200

    License formatting

--- TestSuite/src/main/java/de/rub/nds/tlsattacker/testsuite/impl/TestSuite.java
+++ TestSuite/src/main/java/de/rub/nds/tlsattacker/testsuite/impl/TestSuite.java
@@ -1,3 +1,11 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
