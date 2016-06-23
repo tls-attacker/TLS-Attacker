2f730a291206747d2120930cb3c9454f4a7dc0e0
Robert Merget <Robert.Merget@rub.de>
2016-06-23 20:26:20 +0200

    Fixxed Import

--- EvolutionaryFuzzer/src/main/java/tls/rub/evolutionaryfuzzer/TLSExecutor.java
+++ EvolutionaryFuzzer/src/main/java/tls/rub/evolutionaryfuzzer/TLSExecutor.java
@@ -7,7 +7,6 @@
  */
 package tls.rub.evolutionaryfuzzer;
 
-import com.sun.xml.internal.ws.message.saaj.SAAJHeader;
 import de.rub.nds.tlsattacker.tls.config.ConfigHandler;
 import de.rub.nds.tlsattacker.tls.config.ConfigHandlerFactory;
 import de.rub.nds.tlsattacker.tls.config.GeneralConfig;
