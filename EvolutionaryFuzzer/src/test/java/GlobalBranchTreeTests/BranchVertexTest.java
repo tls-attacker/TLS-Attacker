338265c165858754e2ee18b1c2389ab405dcd3cf
Robert Merget <Robert.Merget@rub.de>
2016-06-23 20:26:46 +0200

    Removed Unessesary Comments

--- EvolutionaryFuzzer/src/test/java/GlobalBranchTreeTests/BranchVertexTest.java
+++ EvolutionaryFuzzer/src/test/java/GlobalBranchTreeTests/BranchVertexTest.java
@@ -55,9 +55,6 @@ public class BranchVertexTest {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
 
     /**
      *
--- EvolutionaryFuzzer/src/test/java/GlobalBranchTreeTests/CountVertexTest.java
+++ EvolutionaryFuzzer/src/test/java/GlobalBranchTreeTests/CountVertexTest.java
@@ -55,9 +55,6 @@ public class CountVertexTest {
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
 
     /**
      *
