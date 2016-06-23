2fdd535967d2b82b84c6b2ed9bd4380d720cf33a
Robert Merget <Robert.Merget@rub.de>
2016-06-23 17:13:44 +0200

    Removed Double closeConnection

--- EvolutionaryFuzzer/src/main/java/tls/rub/evolutionaryfuzzer/TLSExecutor.java
+++ EvolutionaryFuzzer/src/main/java/tls/rub/evolutionaryfuzzer/TLSExecutor.java
@@ -158,7 +158,6 @@ public class TLSExecutor extends Executor
             }
             transportHandler.closeConnection();
 
-            transportHandler.closeConnection();
             //TODO What if server never exited?
             while (!server.exited())
             {
