3c39444bdd357dff563b2ad3a46500a15e2382c1
Robert Merget <Robert.Merget@rub.de>
2016-06-24 11:26:07 +0200

    Added a Test to check if AFL is already compiled

---
+++ EvolutionaryFuzzer/src/test/java/GlobalBranchTreeTests/AFLTest.java
@@ -0,0 +1,57 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package GlobalBranchTreeTests;
+
+import java.io.File;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public class AFLTest
+{
+
+    public AFLTest()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    public void testIsCompiled()
+    {
+        File f = new File("AFL/afl-as");
+        assertTrue(f.exists());
+        f = new File("AFL/afl-showmap");
+        assertTrue(f.exists());
+        
+    }
+}
