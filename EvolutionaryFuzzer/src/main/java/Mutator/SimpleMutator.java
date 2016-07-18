3f100f885790c8ceed2fe9818d8f053c4431a929
Robert Merget <Robert.Merget@rub.de>
2016-07-19 01:59:02 +0200

    Now makes sure that a new Mutation is generated on every run. Also duplicates Messages from now on

--- EvolutionaryFuzzer/src/main/java/Mutator/SimpleMutator.java
+++ EvolutionaryFuzzer/src/main/java/Mutator/SimpleMutator.java
@@ -46,50 +46,63 @@ public class SimpleMutator extends Mutator {
      */
     @Override
     public WorkflowTrace getNewMutation() {
-	// TODO make sure we modified sth
 	Random r = new Random();
 	// chose a random trace from the list
 	WorkflowTrace tempTrace;
-	if (ResultContainer.getInstance().getGoodTraces().isEmpty()) {
-	    tempTrace = new WorkflowTrace();
-	    ResultContainer.getInstance().getGoodTraces().add(tempTrace);
-	} else {
-	    // Choose a random Trace to modify
-	    tempTrace = ResultContainer.getInstance().getGoodTraces()
-		    .get(r.nextInt(ResultContainer.getInstance().getGoodTraces().size()));
-	}
-
-	WorkflowTrace trace = (WorkflowTrace) UnoptimizedDeepCopy.copy(tempTrace);
-	// perhaps add a message
-	if (trace.getProtocolMessages().isEmpty() || r.nextInt(100) < config.getAddMessagePercentage()) {
-	    FuzzingHelper.addRandomMessage(trace);
-	}
-	// perhaps remove a message
-	if (r.nextInt(100) <= config.getRemoveMessagePercentage()) {
-	    FuzzingHelper.removeRandomMessage(trace);
-	}
-	if (trace.getProtocolMessages().isEmpty()) {
-	    FuzzingHelper.addRandomMessage(trace);
-	}
-	// perhaps add records
-	if (r.nextInt(100) <= config.getAddRecordPercentage()) {
-	    FuzzingHelper.addRecordsAtRandom(trace, ConnectionEnd.CLIENT);
-	}
-
-	// Modify a random field:
-	if (r.nextInt(100) <= config.getModifyVariablePercentage()) {
-	    List<ModifiableVariableField> variableList = getAllModifiableVariableFieldsRecursively(trace,
-		    ConnectionEnd.CLIENT);
-	    // LOG.log(Level.INFO, ""+trace.getProtocolMessages().size());
-	    if (variableList.size() > 0) {
-		ModifiableVariableField field = variableList.get(r.nextInt(variableList.size()));
-		String currentFieldName = field.getField().getName();
-		String currentMessageName = field.getObject().getClass().getSimpleName();
-		// LOG.log(Level.INFO, "Fieldname:{0} Message:{1}", new
-		// Object[]{currentFieldName, currentMessageName});
-		executeModifiableVariableModification((ModifiableVariableHolder) field.getObject(), field.getField());
+	WorkflowTrace trace = null;
+	boolean modified = false;
+	do {
+	    if (ResultContainer.getInstance().getGoodTraces().isEmpty()) {
+		tempTrace = new WorkflowTrace();
+		ResultContainer.getInstance().getGoodTraces().add(tempTrace);
+		modified = true;
+	    } else {
+		// Choose a random Trace to modify
+		tempTrace = ResultContainer.getInstance().getGoodTraces()
+			.get(r.nextInt(ResultContainer.getInstance().getGoodTraces().size()));
 	    }
-	}
+
+	    trace = (WorkflowTrace) UnoptimizedDeepCopy.copy(tempTrace);
+	    // perhaps add a message
+	    if (trace.getProtocolMessages().isEmpty() || r.nextInt(100) < config.getAddMessagePercentage()) {
+		FuzzingHelper.addRandomMessage(trace);
+		modified = true;
+	    }
+	    // perhaps remove a message
+	    if (r.nextInt(100) <= config.getRemoveMessagePercentage()) {
+		FuzzingHelper.removeRandomMessage(trace);
+		modified = true;
+	    }
+	    if (trace.getProtocolMessages().isEmpty()) {
+		FuzzingHelper.addRandomMessage(trace);
+		modified = true;
+	    }
+	    // perhaps add records
+	    if (r.nextInt(100) <= config.getAddRecordPercentage()) {
+		FuzzingHelper.addRecordsAtRandom(trace, ConnectionEnd.CLIENT);
+		modified = true;
+	    }
+	    // Modify a random field:
+	    if (r.nextInt(100) <= config.getModifyVariablePercentage()) {
+		List<ModifiableVariableField> variableList = getAllModifiableVariableFieldsRecursively(trace,
+			ConnectionEnd.CLIENT);
+		// LOG.log(Level.INFO, ""+trace.getProtocolMessages().size());
+		if (variableList.size() > 0) {
+		    ModifiableVariableField field = variableList.get(r.nextInt(variableList.size()));
+		    String currentFieldName = field.getField().getName();
+		    String currentMessageName = field.getObject().getClass().getSimpleName();
+		    // LOG.log(Level.INFO, "Fieldname:{0} Message:{1}", new
+		    // Object[]{currentFieldName, currentMessageName});
+		    executeModifiableVariableModification((ModifiableVariableHolder) field.getObject(),
+			    field.getField());
+		    modified = true;
+		}
+	    }
+	    if (r.nextInt(100) <= config.getDuplicateMessagePercentage()) {
+		FuzzingHelper.duplicateRandomProtocolMessage(trace, ConnectionEnd.CLIENT);
+		modified = true;
+	    }
+	} while (!modified);
 	return trace;
 
     }
