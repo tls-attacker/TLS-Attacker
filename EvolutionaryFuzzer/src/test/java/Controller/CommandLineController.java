d7ecc975951e088bf8c1d4fb8e3ac077416ff703
Robert Merget <Robert.Merget@rub.de>
2016-09-05 22:59:07 +0200

    Renamed TestCase

---
+++ EvolutionaryFuzzer/src/test/java/Controller/CommandLineController.java
@@ -0,0 +1,34 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package Controller;
+
+import Config.EvolutionaryFuzzerConfig;
+import Controller.CommandLineController;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * 
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public class CommandLineController {
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    public CommandLineController() {
+    }
+
+}
