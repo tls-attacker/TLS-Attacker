998b1d4ff5054dc0e153a4665d534e40376bf2a6
Robert Merget <Robert.Merget@rub.de>
2016-09-08 14:41:29 +0200

    Hotfix for botan calibration

--- EvolutionaryFuzzer/src/main/java/tlsattacker/fuzzer/calibration/TimeoutCalibrator.java
+++ EvolutionaryFuzzer/src/main/java/tlsattacker/fuzzer/calibration/TimeoutCalibrator.java
@@ -25,6 +25,7 @@ import de.rub.nds.tlsattacker.tls.constants.CipherSuite;
 import de.rub.nds.tlsattacker.tls.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.tls.constants.ProtocolVersion;
 import de.rub.nds.tlsattacker.tls.constants.PublicKeyAlgorithm;
+import de.rub.nds.tlsattacker.tls.exceptions.ConfigurationException;
 import de.rub.nds.tlsattacker.tls.protocol.ArbitraryMessage;
 import de.rub.nds.tlsattacker.tls.protocol.ProtocolMessage;
 import de.rub.nds.tlsattacker.tls.workflow.TlsContext;
@@ -40,12 +41,14 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import tlsattacker.fuzzer.config.ConfigManager;
 
 /**
- * 
+ *
  * @author Robert Merget - robert.merget@rub.de
  */
-public class TimeoutCalibrator {
+public class TimeoutCalibrator
+{
     // We try to find the lowest Timeout that does not alter with Workflow
     // execution and
     // then multiply the number with the gain Factor
@@ -54,170 +57,237 @@ public class TimeoutCalibrator {
     private int limit = 1000;
     private final CalibrationConfig config;
 
-    public int getLimit() {
-	return limit;
+    public int getLimit()
+    {
+        return limit;
     }
 
-    public void setLimit(int limit) {
-	this.limit = limit;
+    public void setLimit(int limit)
+    {
+        this.limit = limit;
     }
 
-    public double getGainFactor() {
-	return gainFactor;
+    public double getGainFactor()
+    {
+        return gainFactor;
     }
 
-    public void setGainFactor(double gainFactor) {
-	this.gainFactor = gainFactor;
+    public void setGainFactor(double gainFactor)
+    {
+        this.gainFactor = gainFactor;
     }
 
-    public TimeoutCalibrator(CalibrationConfig config) {
-	this.config = config;
-	Security.addProvider(new BouncyCastleProvider());
+    public TimeoutCalibrator(CalibrationConfig config)
+    {
+        this.config = config;
+        Security.addProvider(new BouncyCastleProvider());
     }
 
-    public int calibrateTimeout() {
-	LOG.log(Level.INFO, "Calibrating Timeout, this may take some time.");
-	return (int) (getHighestTimeoutGlobal() * gainFactor);
+    public int calibrateTimeout()
+    {
+        LOG.log(Level.INFO, "Calibrating Timeout, this may take some time.");
+        return (int) (getHighestTimeoutGlobal() * gainFactor);
     }
 
-    private int getHighestTimeoutGlobal() {
-	int highestTimeout = 0;
-	FixedCertificateMutator mutator = new FixedCertificateMutator();
+    private int getHighestTimeoutGlobal()
+    {
+        int highestTimeout = 0;
+        FixedCertificateMutator mutator = new FixedCertificateMutator();
 
-	for (ServerCertificateStructure serverCert : mutator.getServerPairList()) {
-	    LOG.log(Level.INFO, "Grabbing supported Ciphersuites for "
-		    + serverCert.getCertificateFile().getAbsolutePath());
-	    List<CipherSuite> supportedList = getWorkingCiphersuites(serverCert);
-	    LOG.log(Level.INFO, "Finished grabbing");
+        for (ServerCertificateStructure serverCert : mutator.getServerPairList())
+        {
+            LOG.log(Level.INFO, "Grabbing supported Ciphersuites for "
+                    + serverCert.getCertificateFile().getAbsolutePath());
+            List<CipherSuite> supportedList = getWorkingCiphersuites(serverCert);
+            LOG.log(Level.INFO, "Finished grabbing");
 
-	    for (CipherSuite suite : supportedList) {
-		int localSmall = getSmallestTimeoutPossible(serverCert, suite);
-		LOG.log(Level.INFO, "Lowest Timeout for " + suite.name() + " is " + localSmall);
-		if (localSmall > highestTimeout) {
-		    LOG.log(Level.INFO, "Found a new highest timeout!");
-		    highestTimeout = localSmall;
-		}
-	    }
-	}
+            for (CipherSuite suite : supportedList)
+            {
+                int localSmall = getSmallestTimeoutPossible(serverCert, suite);
+                LOG.log(Level.INFO, "Lowest Timeout for " + suite.name() + " is " + localSmall);
+                if (localSmall > highestTimeout)
+                {
+                    LOG.log(Level.INFO, "Found a new highest timeout!");
+                    highestTimeout = localSmall;
+                }
+            }
+        }
 
-	return highestTimeout;
+        return highestTimeout;
     }
 
-    private List<CipherSuite> getWorkingCiphersuites(ServerCertificateStructure serverCerts) {
-	List<CipherSuite> workingCipherSuites = new LinkedList<>();
-	List<CipherSuite> ciperSuiteList = CipherSuite.getImplemented();
+    private List<CipherSuite> getWorkingCiphersuites(ServerCertificateStructure serverCerts)
+    {
+        List<CipherSuite> workingCipherSuites = new LinkedList<>();
+        List<CipherSuite> ciperSuiteList = CipherSuite.getImplemented();
 
-	for (CipherSuite ciphersuite : ciperSuiteList) {
-	    if (testCiphersuite(serverCerts, ciphersuite, limit)) {
-		workingCipherSuites.add(ciphersuite);
-	    }
-	}
-	return workingCipherSuites;
+        for (CipherSuite ciphersuite : ciperSuiteList)
+        {
+            if (testCiphersuite(serverCerts, ciphersuite, limit))
+            {
+                workingCipherSuites.add(ciphersuite);
+            }
+        }
+        return workingCipherSuites;
     }
 
     /**
      * Test of executeWorkflow method, of class WorkflowExecutor.
-     * 
+     *
      * @param algorithm
      * @param port
      */
-    public boolean testCiphersuite(ServerCertificateStructure serverCerts, CipherSuite suite, int timeout) {
+    public boolean testCiphersuite(ServerCertificateStructure serverCerts, CipherSuite suite, int timeout)
+    {
 
-	TLSServer server = ServerManager.getInstance().getFreeServer();
-	boolean result = true;
-	try {
-	    Agent agent = AgentFactory.generateAgent(config, serverCerts);
-	    agent.applicationStart(server);
-	    try {
-		Thread.sleep(200);
-	    } catch (InterruptedException ex) {
-		Logger.getLogger(TimeoutCalibrator.class.getName()).log(Level.SEVERE, null, ex);
-	    }
-	    GeneralConfig generalConfig = new GeneralConfig();
-	    generalConfig.setLogLevel(org.apache.logging.log4j.Level.OFF);
-	    ConfigHandler configHandler = ConfigHandlerFactory.createConfigHandler("client");
-	    configHandler.initialize(generalConfig);
-	    ClientCommandConfig config = new ClientCommandConfig();
-	    config.setConnect(server.getIp() + ":" + server.getPort());
-	    config.setTlsTimeout(timeout);
+        TLSServer server = ServerManager.getInstance().getFreeServer();
+        boolean result = true;
+        try
+        {
+            Agent agent = AgentFactory.generateAgent(config, serverCerts);
+            agent.applicationStart(server);
+            try
+            {
+                Thread.sleep(200);
+            }
+            catch (InterruptedException ex)
+            {
+                Logger.getLogger(TimeoutCalibrator.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            GeneralConfig generalConfig = new GeneralConfig();
+            generalConfig.setLogLevel(org.apache.logging.log4j.Level.OFF);
+            ConfigHandler configHandler = ConfigHandlerFactory.createConfigHandler("client");
+            configHandler.initialize(generalConfig);
+            ClientCommandConfig config = new ClientCommandConfig();
+            config.setConnect(server.getIp() + ":" + server.getPort());
+            config.setTlsTimeout(timeout);
 
-	    List<CipherSuite> supportedCipers = new LinkedList<>();
-	    supportedCipers.add(suite);
-	    config.setCipherSuites(supportedCipers);
-	    result &= testExecuteWorkflow(configHandler, config);
-	    agent.applicationStop(server);
-	} finally {
-	    server.release();
-	}
-	return result;
+            List<CipherSuite> supportedCipers = new LinkedList<>();
+            supportedCipers.add(suite);
+            config.setCipherSuites(supportedCipers);
+            result &= testExecuteWorkflow(configHandler, config, agent, server);
+            agent.applicationStop(server);
+        }
+        finally
+        {
+            server.release();
+        }
+        return result;
+    }
+    //TODO cleanup
+    private boolean testExecuteWorkflow(ConfigHandler configHandler, ClientCommandConfig config, Agent agent, TLSServer server)
+    {
+
+        long time = System.currentTimeMillis();
+        TransportHandler transportHandler = null;
+        int counter = 0;
+        while (transportHandler == null)
+        {
+            try
+            {
+                transportHandler = configHandler.initializeTransportHandler(config);
+            }
+            catch (ConfigurationException E)
+            {
+			// It may happen that the implementation is not ready
+                // yet
+                if (time + ConfigManager.getInstance().getConfig().getTimeout() < System.currentTimeMillis())
+                {
+                    LOG.log(java.util.logging.Level.FINE, "Could not start Server! Trying to Restart it!");
+                    agent.applicationStop(server);
+                    agent.applicationStart(server);
+                    time = System.currentTimeMillis();
+                    counter++;
+                }
+                if (counter >= 5)
+                {
+                    throw new ConfigurationException(
+                            "Could not start TLS Server, check your configuration Files!");
+                }
+            }
+        }
+        TlsContext tlsContext = configHandler.initializeTlsContext(config);
+        WorkflowExecutor workflowExecutor = configHandler.initializeWorkflowExecutor(transportHandler, tlsContext);
+
+        WorkflowTrace trace = tlsContext.getWorkflowTrace();
+        trace.makeGeneric();
+        tlsContext.setWorkflowTrace(trace);
+        try
+        {
+
+            workflowExecutor.executeWorkflow();
+        }
+        catch (Exception E)
+        {
+            return false;
+        }
+        finally
+        {
+            transportHandler.closeConnection();
+        }
+        return isWorkflowTraceReasonable(tlsContext.getWorkflowTrace());
     }
 
-    private boolean testExecuteWorkflow(ConfigHandler configHandler, ClientCommandConfig config) {
-
-	TransportHandler transportHandler = configHandler.initializeTransportHandler(config);
-
-	TlsContext tlsContext = configHandler.initializeTlsContext(config);
-	WorkflowExecutor workflowExecutor = configHandler.initializeWorkflowExecutor(transportHandler, tlsContext);
-
-	WorkflowTrace trace = tlsContext.getWorkflowTrace();
-	trace.makeGeneric();
-	tlsContext.setWorkflowTrace(trace);
-	try {
-
-	    workflowExecutor.executeWorkflow();
-	} catch (Exception E) {
-	    return false;
-	} finally {
-	    transportHandler.closeConnection();
-	}
-	return isWorkflowTraceReasonable(tlsContext.getWorkflowTrace());
-    }
-
-    private boolean isWorkflowTraceReasonable(WorkflowTrace trace) {
-	int counter = 0;
-	for (ProtocolMessage configuredMessage : trace.getAllConfiguredMessages()) {
-	    if (counter >= trace.getAllExecutedMessages().size()) {
-		return false;
-	    }
-	    ProtocolMessage receivedMessage = trace.getAllExecutedMessages().get(counter);
-	    if (configuredMessage.getClass().equals(ArbitraryMessage.class)) {
-		break;
-	    }
-	    if (configuredMessage.getClass() != receivedMessage.getClass()) {
-		if (configuredMessage.isRequired()) {
-		    return false;
-		}
-	    } else {
-		counter++;
-	    }
-	}
-	return (!trace.getActuallyRecievedHandshakeMessagesOfType(HandshakeMessageType.FINISHED).isEmpty());
+    private boolean isWorkflowTraceReasonable(WorkflowTrace trace)
+    {
+        int counter = 0;
+        for (ProtocolMessage configuredMessage : trace.getAllConfiguredMessages())
+        {
+            if (counter >= trace.getAllExecutedMessages().size())
+            {
+                return false;
+            }
+            ProtocolMessage receivedMessage = trace.getAllExecutedMessages().get(counter);
+            if (configuredMessage.getClass().equals(ArbitraryMessage.class))
+            {
+                break;
+            }
+            if (configuredMessage.getClass() != receivedMessage.getClass())
+            {
+                if (configuredMessage.isRequired())
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                counter++;
+            }
+        }
+        return (!trace.getActuallyRecievedHandshakeMessagesOfType(HandshakeMessageType.FINISHED).isEmpty());
     }
 
     private static final java.util.logging.Logger LOG = java.util.logging.Logger.getLogger(TimeoutCalibrator.class
-	    .getName());
+            .getName());
 
-    private int getSmallestTimeoutPossible(ServerCertificateStructure serverCerts, CipherSuite suite) {
-	int lowerEnd = 0;
-	int higherEnd = limit;
-	int testedTimeout = higherEnd / 2;
-	do {
-	    System.out.println("" + testedTimeout + " " + lowerEnd + " " + higherEnd);
-	    boolean result = testCiphersuite(serverCerts, suite, testedTimeout);
-	    if (result) {
-		// The ciphersuite still executed fine, lower the testedTimeout
-		higherEnd = testedTimeout;
-	    } else {
-		// the ciphersuite did not execute properly, raise the timeout
-		lowerEnd = testedTimeout;
-	    }
-	    testedTimeout = lowerEnd + ((higherEnd - lowerEnd) / 2);
-	    // we dont care about cornercases just return higher end and we good
-	    if (higherEnd - lowerEnd <= 5) {
-		return higherEnd;
-	    }
-	} while (testedTimeout != lowerEnd && testedTimeout != higherEnd);
-	return testedTimeout;
+    private int getSmallestTimeoutPossible(ServerCertificateStructure serverCerts, CipherSuite suite)
+    {
+        int lowerEnd = 0;
+        int higherEnd = limit;
+        int testedTimeout = higherEnd / 2;
+        do
+        {
+            System.out.println("" + testedTimeout + " " + lowerEnd + " " + higherEnd);
+            boolean result = testCiphersuite(serverCerts, suite, testedTimeout);
+            if (result)
+            {
+                // The ciphersuite still executed fine, lower the testedTimeout
+                higherEnd = testedTimeout;
+            }
+            else
+            {
+                // the ciphersuite did not execute properly, raise the timeout
+                lowerEnd = testedTimeout;
+            }
+            testedTimeout = lowerEnd + ((higherEnd - lowerEnd) / 2);
+            // we dont care about cornercases just return higher end and we good
+            if (higherEnd - lowerEnd <= 5)
+            {
+                return higherEnd;
+            }
+        }
+        while (testedTimeout != lowerEnd && testedTimeout != higherEnd);
+        return testedTimeout;
     }
 
 }
