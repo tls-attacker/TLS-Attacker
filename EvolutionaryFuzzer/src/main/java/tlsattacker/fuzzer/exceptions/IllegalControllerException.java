531ba90932eb94ed8fe222c6308f7d1e5cf091f0
Robert Merget <Robert.Merget@rub.de>
2016-10-17 18:51:06 +0200

    Added IllegalControllerException

---
+++ EvolutionaryFuzzer/src/main/java/tlsattacker/fuzzer/exceptions/IllegalControllerException.java
@@ -0,0 +1,20 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package tlsattacker.fuzzer.exceptions;
+
+/**
+ * 
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public class IllegalControllerException extends Exception {
+
+    public IllegalControllerException(String string) {
+	super(string);
+    }
+
+}
