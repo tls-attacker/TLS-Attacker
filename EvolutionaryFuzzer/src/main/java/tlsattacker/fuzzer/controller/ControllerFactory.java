31216a27ca6c9901a66dbe83960ab2ba25fffcf7
Robert Merget <Robert.Merget@rub.de>
2016-10-17 18:51:19 +0200

    Added ControllerFactory

---
+++ EvolutionaryFuzzer/src/main/java/tlsattacker/fuzzer/controller/ControllerFactory.java
@@ -0,0 +1,35 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package tlsattacker.fuzzer.controller;
+
+import tlsattacker.fuzzer.config.EvolutionaryFuzzerConfig;
+import tlsattacker.fuzzer.exceptions.IllegalCertificateMutatorException;
+import tlsattacker.fuzzer.exceptions.IllegalControllerException;
+import tlsattacker.fuzzer.exceptions.IllegalMutatorException;
+import tlsattacker.fuzzer.mutator.Mutator;
+import tlsattacker.fuzzer.mutator.NoneMutator;
+import tlsattacker.fuzzer.mutator.SimpleMutator;
+import tlsattacker.fuzzer.mutator.certificate.CertificateMutator;
+
+/**
+ *
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public class ControllerFactory
+{
+    public static Controller getController(EvolutionaryFuzzerConfig config)
+            throws IllegalControllerException, IllegalMutatorException, IllegalCertificateMutatorException {
+	switch (config.getMutator()) {
+            case CommandLineController.optionName:
+		return new CommandLineController(config);
+            default:
+		throw new IllegalControllerException("Illegal Value for Controller:" + config.getController());
+
+	}
+    }
+}
