5c6cb9ccfef08d8de9a730ae524a20b515458608
Robert Merget <Robert.Merget@rub.de>
2016-10-28 09:54:46 +0200

    Added java doku, and made class deprecated

--- EvolutionaryFuzzer/src/main/java/tlsattacker/fuzzer/result/ResultContainer.java
+++ EvolutionaryFuzzer/src/main/java/tlsattacker/fuzzer/result/ResultContainer.java
@@ -3,6 +3,7 @@ package tlsattacker.fuzzer.result;
 import tlsattacker.fuzzer.analyzer.RuleAnalyzer;
 import tlsattacker.fuzzer.config.EvolutionaryFuzzerConfig;
 import java.util.logging.Logger;
+import tlsattacker.fuzzer.analyzer.Analyzer;
 
 /**
  * This Class manages the BranchTraces and merges newly obtained Workflows with
@@ -10,13 +11,8 @@ import java.util.logging.Logger;
  * 
  * @author Robert Merget - robert.merget@rub.de
  */
+@Deprecated
 public class ResultContainer {
-
-    /**
-     *
-     */
-    private static final Logger LOG = Logger.getLogger(ResultContainer.class.getName());
-
     /**
      * Singleton
      * 
@@ -27,64 +23,50 @@ public class ResultContainer {
     }
 
     /**
-     *
+     * Config to use
      */
     private EvolutionaryFuzzerConfig evolutionaryFuzzerConfig;
 
     /**
-     *
+     * The Analyzer to use
      */
-    private final RuleAnalyzer analyzer;
+    private final Analyzer analyzer;
 
-    /**
-     *
-     */
     private ResultContainer() {
 	evolutionaryFuzzerConfig = tlsattacker.fuzzer.config.ConfigManager.getInstance().getConfig();
 	analyzer = new RuleAnalyzer(evolutionaryFuzzerConfig);
 
     }
 
-    /**
-     * 
-     * @param evolutionaryFuzzerConfig
-     */
     public void setEvolutionaryFuzzerConfig(EvolutionaryFuzzerConfig evolutionaryFuzzerConfig) {
 	this.evolutionaryFuzzerConfig = evolutionaryFuzzerConfig;
     }
 
-    /**
-     * 
-     * @return
-     */
-    public RuleAnalyzer getAnalyzer() {
+    public Analyzer getAnalyzer() {
 	return analyzer;
     }
 
     /**
-     * 
-     * @param r
+     * Analyzes a Result
+     * @param r Result to analyze
      */
     public void commit(Result r) {
 	analyzer.analyze(r);
     }
 
-    // Singleton
-
     /**
-     *
+     * Singleton
      */
     private static class ResultContainerHolder {
 
 	/**
-         *
+         * Singleton
          */
 	private static final ResultContainer INSTANCE = new ResultContainer();
 
-	/**
-         *
-         */
 	private ResultContainerHolder() {
 	}
     }
+
+    private static final Logger LOG = Logger.getLogger(ResultContainer.class.getName());
 }
