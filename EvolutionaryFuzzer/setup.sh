fc253cd3a3ced27e79a11f72c84e02af2a844077
Robert Merget <Robert.Merget@rub.de>
2016-10-28 03:39:36 +0200

    Fixed the need to download and compile openssl twice while fuzzing. now openssl just has to be installed on the system. The unit tests now use either a small test program or openssl itself

--- EvolutionaryFuzzer/setup.sh
+++ EvolutionaryFuzzer/setup.sh
@@ -1,8 +1,6 @@
 rm implementations -rf
 rm PIN -rf
 mkdir implementations
-git clone git://git.openssl.org/openssl.git implementations/openssl
-cp implementations/openssl implementations/openssl_afl -r
 wget -qO- http://software.intel.com/sites/landingpage/pintool/downloads/pin-3.0-76991-gcc-linux.tar.gz | tar xvz
 mv pin-3.0-76991-gcc-linux PIN
 cd PinScripts
@@ -11,16 +9,4 @@ cd ..
 cd AFL
 make clean
 make
-cd ..
-cd implementations/openssl
-./config -fsanitize=address --prefix=`pwd`/openssl/ --openssldir=`pwd`/openssl/ no-shared -funroll-loops -Wall -D_FORTIFY_SOURCE=2 -g -Wno-pointer-sign
-make
-make install
-cd ..
-cd openssl_afl
-make clean
-export AFL_USE_ASAN=1;
-export AFL_HARDEN=1; 
-CC=../../AFL/afl-gcc ./config --prefix=`pwd`/openssl/ --openssldir=`pwd`/openssl/ no-shared -funroll-loops -Wall -D_FORTIFY_SOURCE=2 -g -Wno-pointer-sign
-make
-make install
+./afl-gcc test.c -o test
