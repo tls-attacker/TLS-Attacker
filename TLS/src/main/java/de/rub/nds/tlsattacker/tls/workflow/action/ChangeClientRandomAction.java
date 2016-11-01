153b85daf21a70a77c06ead7216564d37b62247e
Robert Merget <Robert.Merget@rub.de>
2016-11-01 18:50:43 +0100

    Removed unused Import

--- TLS/src/main/java/de/rub/nds/tlsattacker/tls/workflow/action/ChangeClientRandomAction.java
+++ TLS/src/main/java/de/rub/nds/tlsattacker/tls/workflow/action/ChangeClientRandomAction.java
@@ -11,7 +11,6 @@ import de.rub.nds.tlsattacker.tls.exceptions.WorkflowExecutionException;
 import de.rub.nds.tlsattacker.tls.workflow.TlsContext;
 import de.rub.nds.tlsattacker.tls.workflow.action.executor.ActionExecutor;
 import java.io.IOException;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  * 
