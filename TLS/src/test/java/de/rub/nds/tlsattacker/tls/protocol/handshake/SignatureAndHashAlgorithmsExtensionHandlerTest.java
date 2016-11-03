1a69c80578223fed0a748eadda574b86646dd0e0
m-terlinde <matthias.terlinde@rub.de>
2016-11-03 11:52:33 +0100

    Initial file commit

---
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/SignatureAndHashAlgorithmsExtensionHandlerTest.java
@@ -0,0 +1,17 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.tls.protocol.handshake;
+
+/**
+ *
+ * @author Matthias Terlinde <matthias.terlinde@rub.de>
+ */
+public class SignatureAndHashAlgorithmsExtensionHandlerTest {
+    
+}
