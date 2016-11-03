05b48c057eb245dccc443d74351416e48f725a48
m-terlinde <matthias.terlinde@rub.de>
2016-11-03 18:19:35 +0100

    Don't need that sysout

--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/SignatureAndHashAlgorithmsExtensionHandlerTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/SignatureAndHashAlgorithmsExtensionHandlerTest.java
@@ -47,7 +47,6 @@ public class SignatureAndHashAlgorithmsExtensionHandlerTest {
     {
         msgHandler = null;
         message = null;
-        System.out.println("for Breakpoint");
     }
 
     @Test
