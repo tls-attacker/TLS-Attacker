33f0ac02aba8a327ed06bd9e666d162db437cfef
m-terlinde <matthias.terlinde@rub.de>
2016-11-05 01:46:10 +0100

    fixed import
    uses the original import, not the bouncycasle one.

--- TLS/src/main/java/de/rub/nds/tlsattacker/tls/protocol/extension/SignatureAndHashAlgorithmsExtensionHandler.java
+++ TLS/src/main/java/de/rub/nds/tlsattacker/tls/protocol/extension/SignatureAndHashAlgorithmsExtensionHandler.java
@@ -14,6 +14,7 @@ import de.rub.nds.tlsattacker.tls.constants.SignatureAndHashAlgorithm;
 import de.rub.nds.tlsattacker.util.ArrayConverter;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * @author Juraj Somorovsky <juraj.somorovsky@rub.de>
