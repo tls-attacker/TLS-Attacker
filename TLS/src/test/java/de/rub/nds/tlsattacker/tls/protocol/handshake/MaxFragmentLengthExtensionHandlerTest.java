a06e5af8dc6af52b238e7981791f3c2dccbb10d1
m-terlinde <matthias.terlinde@rub.de>
2016-11-15 15:58:33 +0100

    first test statement
    ref #62

--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/MaxFragmentLengthExtensionHandlerTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/MaxFragmentLengthExtensionHandlerTest.java
@@ -8,10 +8,25 @@
  */
 package de.rub.nds.tlsattacker.tls.protocol.handshake;
 
+import de.rub.nds.tlsattacker.tls.protocol.extension.MaxFragmentLengthExtensionHandler;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  *
  * @author Matthias Terlinde <matthias.terlinde@rub.de>
  */
 public class MaxFragmentLengthExtensionHandlerTest {
+
+    private final byte[] extensionMessage = {(byte) 00, (byte) 01, // Extension type
+        (byte) 00, (byte) 02, // Extension length
+        (byte) 00, (byte) 04}; // max_fragment_length is set to 2^12
     
+    @Test
+    public void testParseExtension () {
+        MaxFragmentLengthExtensionHandler handler = MaxFragmentLengthExtensionHandler.getInstance();
+        int newPointer = handler.parseExtension(extensionMessage, 0);
+        Assert.assertEquals((int) 6, newPointer);
+        
+    }
 }
