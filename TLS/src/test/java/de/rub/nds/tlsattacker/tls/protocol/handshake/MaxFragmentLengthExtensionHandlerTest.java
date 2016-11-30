dd9e87befb26eea3a2a33c8f94fab969282c0a95
m-terlinde <matthias.terlinde@rub.de>
2016-11-30 14:45:07 +0100

    Finished MaxFragmentLengthExtensionHandlerTest
    ref #62

--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/MaxFragmentLengthExtensionHandlerTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/handshake/MaxFragmentLengthExtensionHandlerTest.java
@@ -8,7 +8,10 @@
  */
 package de.rub.nds.tlsattacker.tls.protocol.handshake;
 
+import de.rub.nds.tlsattacker.tls.constants.ExtensionType;
+import de.rub.nds.tlsattacker.tls.constants.MaxFragmentLength;
 import de.rub.nds.tlsattacker.tls.protocol.extension.MaxFragmentLengthExtensionHandler;
+import de.rub.nds.tlsattacker.tls.protocol.extension.MaxFragmentLengthExtensionMessage;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -18,15 +21,96 @@ import org.junit.Test;
  */
 public class MaxFragmentLengthExtensionHandlerTest {
 
-    private final byte[] extensionMessage = {(byte) 00, (byte) 01, // Extension type
-        (byte) 00, (byte) 02, // Extension length
-        (byte) 00, (byte) 04}; // max_fragment_length is set to 2^12
-    
+    private final byte[] extensionMessage
+            = {ExtensionType.MAX_FRAGMENT_LENGTH.getValue()[0], ExtensionType.MAX_FRAGMENT_LENGTH.getValue()[1], // Extension type
+                (byte) 00, (byte) 01, // Extension length
+                MaxFragmentLength.TWO_12.getValue()}; // max_fragment_length is set to 2^12
+
+    private final int newPointer;
+    private final MaxFragmentLengthExtensionMessage parsedMessage;
+    private final MaxFragmentLengthExtensionMessage initializedMessage;
+
+    public MaxFragmentLengthExtensionHandlerTest() {
+        MaxFragmentLengthExtensionHandler maxFragmentHandlerParsed = MaxFragmentLengthExtensionHandler.getInstance();
+        newPointer = maxFragmentHandlerParsed.parseExtension(extensionMessage, 0);
+        parsedMessage = (MaxFragmentLengthExtensionMessage) maxFragmentHandlerParsed.getExtensionMessage();
+
+        MaxFragmentLengthExtensionHandler maxFragmentHandlerInitialised = MaxFragmentLengthExtensionHandler.getInstance();
+        initializedMessage = new MaxFragmentLengthExtensionMessage();
+        initializedMessage.setMaxFragmentLengthConfig(MaxFragmentLength.TWO_12);
+        maxFragmentHandlerInitialised.initializeClientHelloExtension(initializedMessage);
+    }
+
+    /**
+     * Tests the returned pointer of the parseExtension method.
+     */
     @Test
-    public void testParseExtension () {
-        MaxFragmentLengthExtensionHandler handler = MaxFragmentLengthExtensionHandler.getInstance();
-        int newPointer = handler.parseExtension(extensionMessage, 0);
-        Assert.assertEquals((int) 6, newPointer);
-        
+    public void testParseExtensionPointer() {
+        Assert.assertEquals((int) 5, newPointer);
+    }
+
+    /**
+     * Tests the extension bytes of the parseExtension method.
+     */
+    @Test
+    public void testParseExtensionExtensionBytes() {
+        Assert.assertArrayEquals(extensionMessage, parsedMessage.getExtensionBytes().getValue());
+    }
+
+    /**
+     * Tests the extension length of the parseExtension method.
+     */
+    @Test
+    public void testParseExtensionLength() {
+        Assert.assertEquals(new Integer(1), parsedMessage.getExtensionLength().getValue());
+    }
+
+    /**
+     * Tests the extension type of the parseExtension method.
+     */
+    @Test
+    public void testParseExtensionType() {
+        Assert.assertArrayEquals(ExtensionType.MAX_FRAGMENT_LENGTH.getValue(), parsedMessage.getExtensionType().getValue());
+    }
+
+    /**
+     * Tests the max fragment length of the parseExtension method.
+     */
+    @Test
+    public void testParseExtensionMaxFragmentLength() {
+        Assert.assertArrayEquals(MaxFragmentLength.TWO_12.getArrayValue(), parsedMessage.getMaxFragmentLength().getValue());
+    }
+
+    /**
+     * Tests the extension bytes of the initializeClientHelloExtension method.
+     */
+    @Test
+    public void testInitializeExtensionExtensionBytes() {
+        Assert.assertArrayEquals(extensionMessage, initializedMessage.getExtensionBytes().getValue());
+    }
+
+    /**
+     * Tests the extension length of the initializeClientHelloExtension method.
+     */
+    @Test
+    public void testInitializeExtensionLength() {
+        Assert.assertEquals(new Integer(1), initializedMessage.getExtensionLength().getValue());
+    }
+
+    /**
+     * Tests the extension type of the initializeClientHelloExtension method.
+     */
+    @Test
+    public void testInitializeExtensionType() {
+        Assert.assertArrayEquals(ExtensionType.MAX_FRAGMENT_LENGTH.getValue(), initializedMessage.getExtensionType().getValue());
+    }
+
+    /**
+     * Tests the max fragment length of the initializeClientHelloExtension
+     * method.
+     */
+    @Test
+    public void testInitializeExtensionMaxFragmentLength() {
+        Assert.assertArrayEquals(MaxFragmentLength.TWO_12.getArrayValue(), initializedMessage.getMaxFragmentLength().getValue());
     }
 }
