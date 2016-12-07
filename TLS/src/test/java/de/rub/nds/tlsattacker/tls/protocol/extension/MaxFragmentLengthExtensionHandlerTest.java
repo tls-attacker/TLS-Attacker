5d912b861b9de7b18e040778bf6c1cd8c463a4e3
Juraj Somorovsky <juraj.somorovsky@gmail.com>
2016-12-07 11:11:30 +0100

    Moved to a different dir

--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/extension/MaxFragmentLengthExtensionHandlerTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/extension/MaxFragmentLengthExtensionHandlerTest.java
@@ -6,12 +6,10 @@
  * Licensed under Apache License 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
-package de.rub.nds.tlsattacker.tls.protocol.handshake;
+package de.rub.nds.tlsattacker.tls.protocol.extension;
 
 import de.rub.nds.tlsattacker.tls.constants.ExtensionType;
 import de.rub.nds.tlsattacker.tls.constants.MaxFragmentLength;
-import de.rub.nds.tlsattacker.tls.protocol.extension.MaxFragmentLengthExtensionHandler;
-import de.rub.nds.tlsattacker.tls.protocol.extension.MaxFragmentLengthExtensionMessage;
 import org.junit.Assert;
 import org.junit.Test;
 
