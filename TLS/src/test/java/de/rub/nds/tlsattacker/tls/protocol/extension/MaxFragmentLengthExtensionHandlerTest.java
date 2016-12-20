2e8696c5888fc8fc7dc16111eebda9bbeea96550
m-terlinde <matthias.terlinde@rub.de>
2016-12-20 12:58:37 +0100

    Reworked the tests
    ref #62 #77

--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/extension/MaxFragmentLengthExtensionHandlerTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/extension/MaxFragmentLengthExtensionHandlerTest.java
@@ -24,91 +24,46 @@ public class MaxFragmentLengthExtensionHandlerTest {
                 (byte) 00, (byte) 01, // Extension length
                 MaxFragmentLength.TWO_12.getValue()}; // max_fragment_length is set to 2^12
 
-    private final int newPointer;
-    private final MaxFragmentLengthExtensionMessage parsedMessage;
-    private final MaxFragmentLengthExtensionMessage initializedMessage;
+    /**
+     * Tests the parseExtension method of MaxFragmentLengthExtensionHandler
+     */
+    @Test
+    public void testParseExtension() {
+        MaxFragmentLengthExtensionHandler extensionHandler = MaxFragmentLengthExtensionHandler.getInstance();
+        int returnedPointer = extensionHandler.parseExtension(extensionMessage, 0);
+        MaxFragmentLengthExtensionMessage parsedMessage = (MaxFragmentLengthExtensionMessage) extensionHandler.getExtensionMessage();
 
-    public MaxFragmentLengthExtensionHandlerTest() {
-        MaxFragmentLengthExtensionHandler maxFragmentHandlerParsed = MaxFragmentLengthExtensionHandler.getInstance();
-        newPointer = maxFragmentHandlerParsed.parseExtension(extensionMessage, 0);
-        parsedMessage = (MaxFragmentLengthExtensionMessage) maxFragmentHandlerParsed.getExtensionMessage();
+        Assert.assertEquals("Tests the returned pointer of the parseExtension method",
+                (int) 5, returnedPointer);
+        Assert.assertArrayEquals("Tests if the parseExtension method creates the correct extension bytes",
+                extensionMessage, parsedMessage.getExtensionBytes().getValue());
+        Assert.assertEquals("Tests the extensionLength of the parseExtension method",
+                new Integer(1), parsedMessage.getExtensionLength().getValue());
+        Assert.assertArrayEquals("Tests if the extensionType is set correctly",
+                ExtensionType.MAX_FRAGMENT_LENGTH.getValue(), parsedMessage.getExtensionType().getValue());
+        Assert.assertArrayEquals("Tests if the MaxFragmentLength is set correctly",
+                MaxFragmentLength.TWO_12.getArrayValue(), parsedMessage.getMaxFragmentLength().getValue());
+    }
 
+    /**
+     * Tests the initializeClientHelloExtension method of the MaxFragmentLengthExtensionHandler
+     */
+    @Test
+    public void testInitializeClientHelloExtension() {
+        MaxFragmentLengthExtensionMessage initializedMessage;
         MaxFragmentLengthExtensionHandler maxFragmentHandlerInitialised = MaxFragmentLengthExtensionHandler.getInstance();
         initializedMessage = new MaxFragmentLengthExtensionMessage();
         initializedMessage.setMaxFragmentLengthConfig(MaxFragmentLength.TWO_12);
         maxFragmentHandlerInitialised.initializeClientHelloExtension(initializedMessage);
-    }
 
-    /**
-     * Tests the returned pointer of the parseExtension method.
-     */
-    @Test
-    public void testParseExtensionPointer() {
-        Assert.assertEquals((int) 5, newPointer);
-    }
+        Assert.assertArrayEquals("Tests if the extension bytes are set correctly by the initializeClientHelloExtension method",
+                extensionMessage, initializedMessage.getExtensionBytes().getValue());
+        Assert.assertEquals("Tests if the extension length is set correctly by the initializeClientHelloExtension method",
+                new Integer(1), initializedMessage.getExtensionLength().getValue());
+        Assert.assertArrayEquals("Tests if the extension type method is set correctly by the initializeClientHelloExtension method",
+                ExtensionType.MAX_FRAGMENT_LENGTH.getValue(), initializedMessage.getExtensionType().getValue());
+        Assert.assertArrayEquals("Tests if the max fragment length is set correctly by the initializeClientHelloExtension method",
+                MaxFragmentLength.TWO_12.getArrayValue(), initializedMessage.getMaxFragmentLength().getValue());
 
-    /**
-     * Tests the extension bytes of the parseExtension method.
-     */
-    @Test
-    public void testParseExtensionExtensionBytes() {
-        Assert.assertArrayEquals(extensionMessage, parsedMessage.getExtensionBytes().getValue());
-    }
-
-    /**
-     * Tests the extension length of the parseExtension method.
-     */
-    @Test
-    public void testParseExtensionLength() {
-        Assert.assertEquals(new Integer(1), parsedMessage.getExtensionLength().getValue());
-    }
-
-    /**
-     * Tests the extension type of the parseExtension method.
-     */
-    @Test
-    public void testParseExtensionType() {
-        Assert.assertArrayEquals(ExtensionType.MAX_FRAGMENT_LENGTH.getValue(), parsedMessage.getExtensionType().getValue());
-    }
-
-    /**
-     * Tests the max fragment length of the parseExtension method.
-     */
-    @Test
-    public void testParseExtensionMaxFragmentLength() {
-        Assert.assertArrayEquals(MaxFragmentLength.TWO_12.getArrayValue(), parsedMessage.getMaxFragmentLength().getValue());
-    }
-
-    /**
-     * Tests the extension bytes of the initializeClientHelloExtension method.
-     */
-    @Test
-    public void testInitializeExtensionExtensionBytes() {
-        Assert.assertArrayEquals(extensionMessage, initializedMessage.getExtensionBytes().getValue());
-    }
-
-    /**
-     * Tests the extension length of the initializeClientHelloExtension method.
-     */
-    @Test
-    public void testInitializeExtensionLength() {
-        Assert.assertEquals(new Integer(1), initializedMessage.getExtensionLength().getValue());
-    }
-
-    /**
-     * Tests the extension type of the initializeClientHelloExtension method.
-     */
-    @Test
-    public void testInitializeExtensionType() {
-        Assert.assertArrayEquals(ExtensionType.MAX_FRAGMENT_LENGTH.getValue(), initializedMessage.getExtensionType().getValue());
-    }
-
-    /**
-     * Tests the max fragment length of the initializeClientHelloExtension
-     * method.
-     */
-    @Test
-    public void testInitializeExtensionMaxFragmentLength() {
-        Assert.assertArrayEquals(MaxFragmentLength.TWO_12.getArrayValue(), initializedMessage.getMaxFragmentLength().getValue());
     }
 }
--- TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/extension/SignatureAndHashAlgorithmsExtensionHandlerTest.java
+++ TLS/src/test/java/de/rub/nds/tlsattacker/tls/protocol/extension/SignatureAndHashAlgorithmsExtensionHandlerTest.java
@@ -13,21 +13,15 @@ import de.rub.nds.tlsattacker.tls.constants.SignatureAndHashAlgorithm;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import org.junit.After;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- *
  * @author Matthias Terlinde <matthias.terlinde@rub.de>
  */
 public class SignatureAndHashAlgorithmsExtensionHandlerTest {
 
-    private SignatureAndHashAlgorithmsExtensionHandler msgHandler;
-    private SignatureAndHashAlgorithmsExtensionMessage parseMethodMessage;
-    private int gotPointer;
     private final byte[] createdExtension = {(byte) 0, (byte) 13, // Extension type is signature_algorithms
         (byte) 0, (byte) 6, // Extension length
         (byte) 0, (byte) 4, //Count of supported_signature_algorithms bytes
@@ -36,31 +30,13 @@ public class SignatureAndHashAlgorithmsExtensionHandlerTest {
     private final byte[] originalAlgorithms = {(byte) 2, (byte) 2, (byte) 1, (byte) 1};
 
     /**
-     * Creates all new handlers and messages before each test.
-     */
-    @Before
-    public void prepareSAHAEMessage() {
-        msgHandler = SignatureAndHashAlgorithmsExtensionHandler.getInstance();
-        gotPointer = msgHandler.parseExtension(createdExtension, 0);
-        parseMethodMessage = (SignatureAndHashAlgorithmsExtensionMessage) msgHandler.getExtensionMessage();
-    }
-
-    /**
-     * Overwrites the handler and the parseMethodMessage after each test.
-     */
-    @After
-    public void cleanupSAHAEMessage() {
-        msgHandler = null;
-        parseMethodMessage = null;
-    }
-
-    /**
      * Tests the initializeClientHelloExtension method. The
      * SignatureAndHashAlgorithmsConfig is provided, the method fills in the
      * other values. The method works on the reference of the object.
      */
     @Test
     public void testInitializeClientHelloMethod() {
+        byte[] correctExtensionBytes = {(byte) 00, (byte) 13, (byte) 00, (byte) 4, (byte) 0, (byte) 2, (byte) 1, (byte) 1};
         SignatureAndHashAlgorithmsExtensionMessage initializeMethodMessage = new SignatureAndHashAlgorithmsExtensionMessage();
         ArrayList<SignatureAndHashAlgorithm> config = new ArrayList<>();
         config.add(new SignatureAndHashAlgorithm(new byte[]{01, 01}));
@@ -69,99 +45,52 @@ public class SignatureAndHashAlgorithmsExtensionHandlerTest {
         SignatureAndHashAlgorithmsExtensionHandler clientHelloHandler = SignatureAndHashAlgorithmsExtensionHandler.getInstance();
         clientHelloHandler.initializeClientHelloExtension(initializeMethodMessage);
 
-        assertArrayEquals(new byte[]{(byte) 00, (byte) 13, (byte) 00, (byte) 4, (byte) 0, (byte) 2, (byte) 1, (byte) 1},
-                initializeMethodMessage.getExtensionBytes().getValue());
-        assertEquals(new Integer(4), initializeMethodMessage.getExtensionLength().getValue());
-        assertArrayEquals(new byte[]{(byte) 0, (byte) 13}, initializeMethodMessage.getExtensionType().getValue());
-        assertArrayEquals(new byte[]{(byte) 1, (byte) 1}, initializeMethodMessage.getSignatureAndHashAlgorithms().getValue());
-        assertEquals(new Integer(2), initializeMethodMessage.getSignatureAndHashAlgorithmsLength().getValue());
+        assertArrayEquals("Tests the complete extension bytes returned by the initializeClientHello method",
+                correctExtensionBytes, initializeMethodMessage.getExtensionBytes().getValue());
+        assertEquals("Tests the extension length returned by the initializeClientHello method",
+                new Integer(4), initializeMethodMessage.getExtensionLength().getValue());
+        assertArrayEquals("Tests the extension type returned by the initializeClientHello method",
+                new byte[]{(byte) 0, (byte) 13}, initializeMethodMessage.getExtensionType().getValue());
+        assertArrayEquals("Tests the set signature and hash algorithms returned by the initializeClientHello method",
+                new byte[]{(byte) 1, (byte) 1}, initializeMethodMessage.getSignatureAndHashAlgorithms().getValue());
+        assertEquals("Tests the signature and hash algorithms length returned by the initializeClientHello method",
+                new Integer(2), initializeMethodMessage.getSignatureAndHashAlgorithmsLength().getValue());
 
     }
 
-    /**
-     * Tests the returned pointer, it should point onto the next byte after the
-     * extension.
-     */
     @Test
-    public void testPointer() {
-        assertEquals("The new pointer must be 10", (int) 10, gotPointer);
-    }
+    public void testParseExtensionMethod() throws IOException {
+        SignatureAndHashAlgorithmsExtensionHandler msgHandler;
+        SignatureAndHashAlgorithmsExtensionMessage parseMethodMessage;
+        int gotPointer;
 
-    /**
-     * Tests if the extension bytes are copied correctly.
-     */
-    @Test
-    public void testExtensionBytes() {
-        assertArrayEquals("Extension Message should be 00 13 00 06 00 04 02 02 01 01", createdExtension,
+        msgHandler = SignatureAndHashAlgorithmsExtensionHandler.getInstance();
+        gotPointer = msgHandler.parseExtension(createdExtension, 0);
+        parseMethodMessage = (SignatureAndHashAlgorithmsExtensionMessage) msgHandler.getExtensionMessage();
+
+        assertEquals("Tests the returned pointer",
+                (int) 10, gotPointer);
+        assertArrayEquals("Tests the extension bytes",
+                createdExtension,
                 parseMethodMessage.getExtensionBytes().getValue());
-    }
-
-    /**
-     * Tests if the ExtensionType Value is set correctly.
-     */
-    @Test
-    public void testExtensionType() {
-        assertArrayEquals("Extension Type should be 00 13", ExtensionType.SIGNATURE_AND_HASH_ALGORITHMS.getValue(),
+        assertArrayEquals("Tests the extension type",
+                ExtensionType.SIGNATURE_AND_HASH_ALGORITHMS.getValue(),
                 parseMethodMessage.getExtensionType().getValue());
-    }
+        assertEquals("Tests the signature and hash algorithms length",
+                new Integer(4), parseMethodMessage.getSignatureAndHashAlgorithmsLength().getValue());
 
-    /**
-     * If the parser gets a wrong extension, the parsing method should throw an
-     * exception.
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void testWrongExtension() {
-        SignatureAndHashAlgorithmsExtensionHandler newMsgHandler = SignatureAndHashAlgorithmsExtensionHandler.getInstance();
-        byte[] newCreatedExtension = {(byte) 1, (byte) 12, // wrong Extension
-            (byte) 0, (byte) 6,
-            (byte) 0, (byte) 4,
-            (byte) 2, (byte) 2,
-            (byte) 1, (byte) 1};
-        newMsgHandler.parseExtension(newCreatedExtension, 0);
-    }
-
-    /**
-     * Tests if the SignatureAndHashAlgorithmsLength value is set correctly.
-     */
-    @Test
-    public void testSignatureAndHashAlgorithmLength() {
-        assertEquals("The length should be 4, hence there are 2 combinations", new Integer(4), parseMethodMessage.getSignatureAndHashAlgorithmsLength().getValue());
-    }
-
-    /**
-     * Tests if the extension config is set correctly. Checks the array list,
-     * not the byte value.
-     *
-     * @throws IOException
-     */
-    @Test
-    public void testSignatureAndHashAlgorithmConfig() throws IOException {
         ByteArrayOutputStream parsedAlgorithms = new ByteArrayOutputStream();
         for (SignatureAndHashAlgorithm alg : parseMethodMessage.getSignatureAndHashAlgorithmsConfig()) {
             parsedAlgorithms.write(alg.getByteValue());
         }
         /* The ArrayList can't be compared directly due to the hashmap in the datatype SignatureAndHashAlgorithm
         assertEquals detects different ArrayLists, even if the values are identical. */
-        assertArrayEquals(originalAlgorithms, parsedAlgorithms.toByteArray());
+        assertArrayEquals("Tests the set signature and hash algorithms config",
+                originalAlgorithms, parsedAlgorithms.toByteArray());
 
+        assertArrayEquals("Tests the set signature and hash algorithms bytes",
+                originalAlgorithms, parseMethodMessage.getSignatureAndHashAlgorithms().getValue());
+        assertEquals("Tests the extension length",
+                (int) 6, (int) parseMethodMessage.getExtensionLength().getValue());
     }
-
-    /**
-     * Tests if the extension byte values are set correctly. Doesn't check the
-     * array list.
-     */
-    @Test
-    public void testSignatureAndHashAlgorithms() {
-        assertArrayEquals(originalAlgorithms, parseMethodMessage.getSignatureAndHashAlgorithms().getValue());
-    }
-
-    /**
-     * Tests if the extension length value is set correctly.
-     */
-    @Test
-    public void testExtensionLength() {
-        assertEquals((int) 6,
-                (int) parseMethodMessage.getExtensionLength().getValue());
-    }
-
 }
