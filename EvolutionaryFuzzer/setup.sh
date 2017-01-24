3ea64b3e9909ede2b206ebff032ba94e48bdd1a6
ic0ns <robert.merget@rub.de>
2017-01-24 11:53:13 +0100

    installScript now also installs the newest Version of AFL

--- EvolutionaryFuzzer/setup.sh
+++ EvolutionaryFuzzer/setup.sh
@@ -1,7 +1,12 @@
 rm PIN -rf
+rm AFL -rf
 mkdir implementations
-wget -qO- http://software.intel.com/sites/landingpage/pintool/downloads/pin-3.0-76991-gcc-linux.tar.gz | tar xvz
-mv pin-3.0-76991-gcc-linux PIN
+mkdir PIN
+mkdir AFL
+wget -qO- http://software.intel.com/sites/landingpage/pintool/downloads/pin-3.0-76991-gcc-linux.tar.gz | tar xvz -C PIN/ --strip-components=1
 cd PinScripts
 make -f makefile PIN_ROOT=../PIN/
 cd ..
+wget -qO- http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz | tar xvz -C AFL/ --strip-components=1
+cd AFL
+make
