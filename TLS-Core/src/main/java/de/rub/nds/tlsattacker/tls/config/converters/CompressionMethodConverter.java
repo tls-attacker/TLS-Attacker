270fc6877026fd1323747bfbfb9ea52b3a725bf4
ic0ns <robert.merget@rub.de>
2017-01-25 10:00:08 +0100

    Moved classes around

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/tls/config/converters/CompressionMethodConverter.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/tls/config/converters/CompressionMethodConverter.java
@@ -6,7 +6,7 @@
  * Licensed under Apache License 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
-package de.rub.nds.tlsattacker.tls.util;
+package de.rub.nds.tlsattacker.tls.config.converters;
 
 import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.ParameterException;
