6370a5832401742d64aa5bdd2bdaac651826b780
ic0ns <robert.merget@rub.de>
2017-02-23 19:10:00 +0100

    Added preparator (wip)

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/tls/protocol/serializer/preparator/HelloMessagePreparator.java
@@ -0,0 +1,27 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.tls.protocol.serializer.preparator;
+
+import de.rub.nds.tlsattacker.tls.protocol.handshake.HandshakeMessage;
+import de.rub.nds.tlsattacker.tls.protocol.handshake.HelloMessage;
+import de.rub.nds.tlsattacker.tls.workflow.TlsContext;
+
+/**
+ *
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public abstract class HelloMessagePreparator<T extends HelloMessage> extends HandshakeMessagePreparator<HandshakeMessage>{
+
+    private final HelloMessage message;
+    
+    public HelloMessagePreparator(TlsContext context, HelloMessage message) {
+        super(context, message);
+        this.message = message;
+    }
+    
+}
