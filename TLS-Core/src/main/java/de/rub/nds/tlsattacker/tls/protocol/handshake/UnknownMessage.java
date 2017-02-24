8934af7963299a8f6c16c99552db134c69fd3729
Robert Merget <Robert.Merget@rub.de>
2017-02-24 12:42:00 +0100

    Added unknown Message

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/tls/protocol/handshake/UnknownMessage.java
@@ -0,0 +1,52 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.tls.protocol.handshake;
+
+import de.rub.nds.tlsattacker.tls.protocol.ProtocolMessage;
+import de.rub.nds.tlsattacker.tls.protocol.ProtocolMessageHandler;
+import de.rub.nds.tlsattacker.tls.workflow.TlsConfig;
+import de.rub.nds.tlsattacker.tls.workflow.TlsContext;
+
+/**
+ *
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public class UnknownMessage extends ProtocolMessage{
+
+    private byte[] dataConfig;
+
+    public UnknownMessage()
+    {
+        super();
+    }
+    
+    public UnknownMessage(TlsConfig config)
+    {
+        super();
+    }
+
+    public byte[] getDataConfig() {
+        return dataConfig;
+    }
+
+    public void setDataConfig(byte[] dataConfig) {
+        this.dataConfig = dataConfig;
+    }
+
+    @Override
+    public ProtocolMessageHandler<? extends ProtocolMessage> getProtocolMessageHandler(TlsContext tlsContext) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toCompactString() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
