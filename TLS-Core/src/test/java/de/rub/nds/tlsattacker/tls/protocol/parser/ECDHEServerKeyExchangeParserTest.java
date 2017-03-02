cdfd5ffb6da4a41b8b3034a3ccdc3774ac5a98fb
ic0ns <robert.merget@rub.de>
2017-03-02 09:22:37 +0100

    Updated Test Cases

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/tls/protocol/parser/ECDHEServerKeyExchangeParserTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/tls/protocol/parser/ECDHEServerKeyExchangeParserTest.java
@@ -10,6 +10,7 @@ package de.rub.nds.tlsattacker.tls.protocol.parser;
 import de.rub.nds.tlsattacker.tls.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.tls.protocol.message.ECDHClientKeyExchangeMessage;
 import de.rub.nds.tlsattacker.tls.protocol.message.ECDHEServerKeyExchangeMessage;
+import de.rub.nds.tlsattacker.util.ArrayConverter;
 import java.util.Arrays;
 import java.util.Collection;
 import org.junit.Before;
@@ -30,9 +31,9 @@ public class ECDHEServerKeyExchangeParserTest {
         return Arrays
                 .asList(new Object[][] {
                         {
+                            ArrayConverter.hexStringToByteArray(""),
                         },
-                        {
-                        }});
+                        });
     }
     private byte[] message;
     private int start;
