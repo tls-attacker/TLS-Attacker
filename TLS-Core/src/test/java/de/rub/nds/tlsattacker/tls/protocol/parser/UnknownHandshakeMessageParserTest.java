a4b6043e790c6755aa76fdb69863a952dbe352fc
ic0ns <robert.merget@rub.de>
2017-03-03 09:27:33 +0100

    Added UnknoqnHandshakeMessage test (with sessionticket msg)

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/tls/protocol/parser/UnknownHandshakeMessageParserTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/tls/protocol/parser/UnknownHandshakeMessageParserTest.java
@@ -10,6 +10,7 @@ package de.rub.nds.tlsattacker.tls.protocol.parser;
 
 import de.rub.nds.tlsattacker.tls.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.tls.protocol.message.UnknownHandshakeMessage;
+import de.rub.nds.tlsattacker.util.ArrayConverter;
 import java.util.Arrays;
 import java.util.Collection;
 import org.junit.Before;
@@ -27,7 +28,7 @@ public class UnknownHandshakeMessageParserTest {
 
     @Parameterized.Parameters
     public static Collection<Object[]> generateData() {
-        return Arrays.asList(new Object[][] { {}, {} });
+        return Arrays.asList(new Object[][] { {ArrayConverter.hexStringToByteArray("040000a60000012c00a02f8dbba0bca89176bf21d4e640f729dcbded6af280556e9b4b18a6c8218f01976780232a6765e278ecc516fb19bb9ec6e3913ed27a6123eefa188212c4e5d611c85c55fb32358c0896c00781392039aae9df79ebad27860e9d5016df72bd6de898502e6221481e0f375c949e44adb6fd7fcf33e9d431a223dcf7bb72fc585ae1d8df34178bbdc5e553657dd615dc38c59b49970129c937e961f1a87a60af1e26"),0,ArrayConverter.hexStringToByteArray("040000a60000012c00a02f8dbba0bca89176bf21d4e640f729dcbded6af280556e9b4b18a6c8218f01976780232a6765e278ecc516fb19bb9ec6e3913ed27a6123eefa188212c4e5d611c85c55fb32358c0896c00781392039aae9df79ebad27860e9d5016df72bd6de898502e6221481e0f375c949e44adb6fd7fcf33e9d431a223dcf7bb72fc585ae1d8df34178bbdc5e553657dd615dc38c59b49970129c937e961f1a87a60af1e26"),HandshakeMessageType.NEW_SESSION_TICKET,166,ArrayConverter.hexStringToByteArray("0000012c00a02f8dbba0bca89176bf21d4e640f729dcbded6af280556e9b4b18a6c8218f01976780232a6765e278ecc516fb19bb9ec6e3913ed27a6123eefa188212c4e5d611c85c55fb32358c0896c00781392039aae9df79ebad27860e9d5016df72bd6de898502e6221481e0f375c949e44adb6fd7fcf33e9d431a223dcf7bb72fc585ae1d8df34178bbdc5e553657dd615dc38c59b49970129c937e961f1a87a60af1e26")}, });
     }
 
     private byte[] message;
@@ -36,14 +37,16 @@ public class UnknownHandshakeMessageParserTest {
 
     private HandshakeMessageType type;
     private int length;
+    private byte[] data;
 
     public UnknownHandshakeMessageParserTest(byte[] message, int start, byte[] expectedPart, HandshakeMessageType type,
-            int length) {
+            int length, byte[] data) {
         this.message = message;
         this.start = start;
         this.expectedPart = expectedPart;
         this.type = type;
         this.length = length;
+        this.data = data;
     }
 
     /**
@@ -56,7 +59,7 @@ public class UnknownHandshakeMessageParserTest {
         assertArrayEquals(expectedPart, msg.getCompleteResultingMessage().getValue());
         assertTrue(msg.getLength().getValue() == length);
         assertTrue(msg.getType().getValue() == type.getValue());
-
+        assertArrayEquals(data, msg.getData().getValue());
     }
 
 }
