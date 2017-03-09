928e349e4c2b20ac71291f538a0ae39ce129cc1a
ic0ns <robert.merget@rub.de>
2017-03-09 10:23:17 +0100

    Added SerializerTests

---
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/tls/protocol/serializer/UnknownMessageSerializerTest.java
@@ -0,0 +1,36 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.tls.protocol.serializer;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public class UnknownMessageSerializerTest {
+
+    public UnknownMessageSerializerTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Test of serializeProtocolMessageContent method, of class
+     * UnknownMessageSerializer.
+     */
+    @Test
+    public void testSerializeProtocolMessageContent() {
+    }
+
+}
