c67600ccdae1585f35199ee3b7218d56cc7d7bad
ic0ns <robert.merget@rub.de>
2017-03-28 12:53:07 +0200

    Deactivated SSL2 Test since it is currently not working correctly since we cannot specify a SSL2 record layer

--- TLS-Scanner/src/main/java/de/rub/nds/tlsscanner/probe/ProtocolVersionProbe.java
+++ TLS-Scanner/src/main/java/de/rub/nds/tlsscanner/probe/ProtocolVersionProbe.java
@@ -56,12 +56,12 @@ public class ProtocolVersionProbe extends TLSProbe {
     public ProbeResult call() {
         List<ResultValue> resultList = new LinkedList<>();
         List<TLSCheck> checkList = new LinkedList<>();
-        LOGGER.info("Testing SSL2:");
-        boolean result = isProtocolVersionSupported(ProtocolVersion.SSL2);
-        resultList.add(new ResultValue("SSL 2", "" + result));
-        checkList.add(new TLSCheck(result, CheckType.PROTOCOLVERSION_SSL2, getConfig().getLanguage()));
+        //LOGGER.info("Testing SSL2:");
+        //boolean result = isProtocolVersionSupported(ProtocolVersion.SSL2);
+        //resultList.add(new ResultValue("SSL 2", "" + result));
+        //checkList.add(new TLSCheck(result, CheckType.PROTOCOLVERSION_SSL2, getConfig().getLanguage()));
         LOGGER.info("Testing SSL3:");
-        result = isProtocolVersionSupported(ProtocolVersion.SSL3);
+        boolean result = isProtocolVersionSupported(ProtocolVersion.SSL3);
         resultList.add(new ResultValue("SSL 3", "" + result));
         checkList.add(new TLSCheck(result, CheckType.PROTOCOLVERSION_SSL3, getConfig().getLanguage()));
         LOGGER.info("Testing TLS 1.0:");
