0d5240a815e6c04a9b5774bac27c57b91a07dd6d
ic0ns <robert.merget@rub.de>
2017-04-04 17:17:34 +0200

    Added IntegrationTest for the new RecordLayer parser

---
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/tls/record/layer/TlsRecordLayerIT.java
@@ -0,0 +1,47 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.tls.record.layer;
+
+import de.rub.nds.tlsattacker.tls.workflow.TlsConfig;
+import de.rub.nds.tlsattacker.tls.workflow.TlsContext;
+import de.rub.nds.tlsattacker.util.RandomHelper;
+import java.util.Random;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Robert Merget <robert.merget@rub.de>
+ */
+public class TlsRecordLayerIT {
+
+    private TlsRecordLayer layer;
+
+    public TlsRecordLayerIT() {
+    }
+
+    @Before
+    public void setUp() {
+        layer = new TlsRecordLayer(new TlsContext(new TlsConfig()));
+    }
+
+    /**
+     * Test of parseRecords method, of class TlsRecordLayer.
+     */
+    @Test
+    public void testParseRecords() {
+        Random r = RandomHelper.getRandom();
+        for (int i = 0; i < 10000; i++) {
+            byte[] data = new byte[r.nextInt(1000000)];
+            r.nextBytes(data);
+            layer.parseRecords(data);
+        }
+    }
+
+}
