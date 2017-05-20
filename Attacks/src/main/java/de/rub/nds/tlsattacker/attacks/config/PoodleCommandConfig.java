bcaf5a5fb903c32804e3f05710fef07ce927658d
Robert Merget <Robert.Merget@rub.de>
2017-05-20 02:06:59 +0200

    Added classic Poodle Check

---
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/config/PoodleCommandConfig.java
@@ -0,0 +1,42 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.attacks.config;
+
+import com.beust.jcommander.ParametersDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.ClientDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.GeneralDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.HostnameExtensionDelegate;
+import de.rub.nds.tlsattacker.core.workflow.TlsConfig;
+
+/**
+ *
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public class PoodleCommandConfig extends AttackConfig {
+
+    public static final String ATTACK_COMMAND = "poodle";
+    @ParametersDelegate
+    private final ClientDelegate clientDelegate;
+    @ParametersDelegate
+    private final HostnameExtensionDelegate hostnameExtensionDelegate;
+
+    public PoodleCommandConfig(GeneralDelegate delegate) {
+        super(delegate);
+        clientDelegate = new ClientDelegate();
+        hostnameExtensionDelegate = new HostnameExtensionDelegate();
+        addDelegate(clientDelegate);
+        addDelegate(hostnameExtensionDelegate);
+    }
+
+    @Override
+    public TlsConfig createConfig() {
+        return super.createConfig();
+    }
+
+}
---
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/PoodleAttacker.java
@@ -0,0 +1,70 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.attacks.impl;
+
+import de.rub.nds.tlsattacker.attacks.config.AttackConfig;
+import de.rub.nds.tlsattacker.attacks.config.PoodleCommandConfig;
+import de.rub.nds.tlsattacker.core.constants.CipherSuite;
+import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
+import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
+import de.rub.nds.tlsattacker.core.workflow.DefaultWorkflowExecutor;
+import de.rub.nds.tlsattacker.core.workflow.TlsConfig;
+import de.rub.nds.tlsattacker.core.workflow.TlsContext;
+import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Robert Merget - robert.merget@rub.de
+ */
+public class PoodleAttacker extends Attacker {
+
+    public PoodleAttacker(PoodleCommandConfig config) {
+        super(config, true);
+    }
+
+    @Override
+    public void executeAttack() {
+        throw new UnsupportedOperationException("Not supported yet."); // To
+                                                                       // change
+                                                                       // body
+                                                                       // of
+                                                                       // generated
+                                                                       // methods,
+                                                                       // choose
+                                                                       // Tools
+                                                                       // |
+                                                                       // Templates.
+    }
+
+    @Override
+    public Boolean isVulnerable() {
+        TlsConfig tlsConfig = config.createConfig();
+        TlsContext context = new TlsContext(tlsConfig);
+        context.getConfig().setHighestProtocolVersion(ProtocolVersion.SSL3);
+        context.getConfig().setSupportedCiphersuites(getCbcCiphers());
+        context.getConfig().setWorkflowTraceType(WorkflowTraceType.HELLO);
+        DefaultWorkflowExecutor executor = new DefaultWorkflowExecutor(context);
+        executor.executeWorkflow();
+        return context.getWorkflowTrace().getActuallyRecievedHandshakeMessagesOfType(HandshakeMessageType.SERVER_HELLO)
+                .size() > 0;
+    }
+
+    private List<CipherSuite> getCbcCiphers() {
+        List<CipherSuite> cbcs = new LinkedList<>();
+        for (CipherSuite suite : CipherSuite.getImplemented()) {
+            if (suite.isCBC()) {
+                cbcs.add(suite);
+            }
+        }
+        return cbcs;
+    }
+
+}
