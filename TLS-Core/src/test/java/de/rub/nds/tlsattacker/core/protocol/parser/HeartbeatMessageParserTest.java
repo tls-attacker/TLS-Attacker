87382cf746e5070badde143c27ea15ee1dae4d7b
Janis Fliegenschmidt <janisf@gmx.net>
2017-05-22 01:13:27 +0200

    The heartbeat message parser test now uses real heartbeat messages

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/parser/HeartbeatMessageParserTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/parser/HeartbeatMessageParserTest.java
@@ -8,13 +8,11 @@
  */
 package de.rub.nds.tlsattacker.core.protocol.parser;
 
-import de.rub.nds.tlsattacker.core.protocol.parser.HeartbeatMessageParser;
-import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlsattacker.core.protocol.message.HeartbeatMessage;
+import de.rub.nds.tlsattacker.util.ByteRepresentationConverter;
 import java.util.Arrays;
 import java.util.Collection;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
@@ -26,22 +24,34 @@ import org.junit.runners.Parameterized;
  */
 @RunWith(Parameterized.class)
 public class HeartbeatMessageParserTest {
+    private static String heartbeatRequest = "010012000075a6d1d422693ea31584902266171b14ee376d595f5c65aeba8d04b0378faeda";
+    private static String requestPayload = "000075a6d1d422693ea31584902266171b14";
+    private static String requestPadding = "ee376d595f5c65aeba8d04b0378faeda";
+    private static String heartbeatResponse = "020012000075a6d1d422693ea31584902266171b1429ee15bbaa07f19c012dc29e2449e1e1";
+    private static String responsePayload = requestPayload;
+    private static String responsePadding = "29ee15bbaa07f19c012dc29e2449e1e1";
 
     @Parameterized.Parameters
     public static Collection<Object[]> generateData() {
-        return Arrays.asList(new Object[][] {});
+        return Arrays.asList(new Object[][] {
+                { ByteRepresentationConverter.hexStringToByteArray(heartbeatRequest), 0,
+                        ByteRepresentationConverter.hexStringToByteArray(heartbeatRequest), (byte) 0x1, 18,
+                        ByteRepresentationConverter.hexStringToByteArray(requestPayload),
+                        ByteRepresentationConverter.hexStringToByteArray(requestPadding) },
+                { ByteRepresentationConverter.hexStringToByteArray(heartbeatResponse), 0,
+                        ByteRepresentationConverter.hexStringToByteArray(heartbeatResponse), (byte) 0x2, 18,
+                        ByteRepresentationConverter.hexStringToByteArray(responsePayload),
+                        ByteRepresentationConverter.hexStringToByteArray(responsePadding) } });
     }
 
-    // TODO get a real heartbeat message
+    private final byte[] message;
+    private final int start;
+    private final byte[] expectedPart;
 
-    private byte[] message;
-    private int start;
-    private byte[] expectedPart;
-
-    private byte heartBeatType;
-    private int payloadLength;
-    private byte[] payload;
-    private byte[] padding;
+    private final byte heartBeatType;
+    private final int payloadLength;
+    private final byte[] payload;
+    private final byte[] padding;
 
     public HeartbeatMessageParserTest(byte[] message, int start, byte[] expectedPart, byte heartBeatType,
             int payloadLength, byte[] payload, byte[] padding) {
@@ -66,5 +76,4 @@ public class HeartbeatMessageParserTest {
         assertArrayEquals(payload, msg.getPayload().getValue());
         assertArrayEquals(padding, msg.getPadding().getValue());
     }
-
 }
