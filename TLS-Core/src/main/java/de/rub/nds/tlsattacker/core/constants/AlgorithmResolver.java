72cf7ee30a5b050c9fafca486582ce9154b5851e
ic0ns <robert.merget@rub.de>
2017-05-22 09:28:14 +0200

    Moved RC2 to blockCiphers (ups)

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/AlgorithmResolver.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/AlgorithmResolver.java
@@ -192,9 +192,9 @@ public class AlgorithmResolver {
             return CipherType.AEAD;
         } else if (cipher.contains("AES") || cipher.contains("DES") || cipher.contains("IDEA")
                 || cipher.contains("WITH_FORTEZZA") || cipher.contains("CAMELLIA") || cipher.contains("GOST")
-                || cipher.contains("WITH_SEED") || cipher.contains("WITH_ARIA")) {
+                || cipher.contains("WITH_SEED") || cipher.contains("WITH_ARIA") || cipher.contains("RC2")) {
             return CipherType.BLOCK;
-        } else if (cipher.contains("RC4") || cipher.contains("RC2") || cipher.contains("WITH_NULL")
+        } else if (cipher.contains("RC4")  || cipher.contains("WITH_NULL")
                 || cipher.contains("CHACHA")) {
             return CipherType.STREAM;
         }
