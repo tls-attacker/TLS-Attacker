f48e58aeedde189fc2c2934a069eb8a1da22891b
Malena Ebert <malena-rub@ebert.li>
2017-06-17 13:09:36 +0200

    add tests, which use wireshark captured realworld data

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/parser/FinishedMessageParserTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/parser/FinishedMessageParserTest.java
@@ -33,18 +33,30 @@ public class FinishedMessageParserTest {
     private static final byte[] MSG_RECORD_LENGTH = new byte[] { 0x00, 0x00, 0x10 };
     private static final byte[] MSG_HANDSHAKE_FINISHED = new byte[] { 0x14 };
     private static final byte[] MSG_LENGTH = new byte[] { 0x00, 0x00, 0x0c };
-    private static final byte[] MSG_0_VERIFIED_DATA = new byte[] { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-            0x66, 0x55, 0x44, 0x33 };
+    private static final byte[] MSG_0_VERIFY_DATA = new byte[] { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x66,
+            0x55, 0x44, 0x33 };
     private static final byte[] MSG_0 = ArrayConverter.concatenate(MSG_HANDSHAKE_FINISHED, MSG_LENGTH,
-            MSG_0_VERIFIED_DATA);
+            MSG_0_VERIFY_DATA);
     private static final byte[] MSG_1 = ArrayConverter.concatenate(MSG_RECORD_HANDSHAKE, MSG_RECORD_VERSION,
             MSG_RECORD_LENGTH, MSG_0);
+    private static final byte[] MSG_DECRYPTED_WIRESHARK_CAP_0 = ArrayConverter
+            .hexStringToByteArray("1400000ccc111ca8d8d84321f1039b92");
+    private static final byte[] MSG_DECRYPTED_WIRESHARK_CAP_0_VERIFY_DATA = ArrayConverter
+            .hexStringToByteArray("cc111ca8d8d84321f1039b92");
+    private static final byte[] MSG_DECRYPTED_WIRESHARK_CAP_1 = ArrayConverter
+            .hexStringToByteArray("1400000c5ddfb413e7b592b4ec0186c5");
+    private static final byte[] MSG_DECRYPTED_WIRESHARK_CAP_1_VERIFY_DATA = ArrayConverter
+            .hexStringToByteArray("5ddfb413e7b592b4ec0186c5");
 
     @Parameterized.Parameters
     public static Collection<Object[]> generateData() {
         return Arrays.asList(new Object[][] {
-                { MSG_0, 0, MSG_0, HandshakeMessageType.FINISHED, 12, MSG_0_VERIFIED_DATA },
-                { MSG_1, 6, MSG_0, HandshakeMessageType.FINISHED, 12, MSG_0_VERIFIED_DATA } });
+                { MSG_0, 0, MSG_0, HandshakeMessageType.FINISHED, 12, MSG_0_VERIFY_DATA },
+                { MSG_1, 6, MSG_0, HandshakeMessageType.FINISHED, 12, MSG_0_VERIFY_DATA },
+                { MSG_DECRYPTED_WIRESHARK_CAP_0, 0, MSG_DECRYPTED_WIRESHARK_CAP_0, HandshakeMessageType.FINISHED, 12,
+                        MSG_DECRYPTED_WIRESHARK_CAP_0_VERIFY_DATA },
+                { MSG_DECRYPTED_WIRESHARK_CAP_1, 0, MSG_DECRYPTED_WIRESHARK_CAP_1, HandshakeMessageType.FINISHED, 12,
+                        MSG_DECRYPTED_WIRESHARK_CAP_1_VERIFY_DATA } });
     }
 
     private final byte[] message;
