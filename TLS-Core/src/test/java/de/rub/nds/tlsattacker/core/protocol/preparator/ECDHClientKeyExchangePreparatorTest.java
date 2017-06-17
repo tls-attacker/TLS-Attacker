6ad581d0f7dc81a8fdb54b389291a5e5b646cbb5
Malena Ebert <malena-rub@ebert.li>
2017-06-17 18:58:07 +0200

    rewrite ECDH Client Key Exchange unit test to asserts withouts calculations

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/preparator/ECDHClientKeyExchangePreparatorTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/preparator/ECDHClientKeyExchangePreparatorTest.java
@@ -16,18 +16,29 @@ import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlsattacker.core.protocol.preparator.ECDHClientKeyExchangePreparator;
 import de.rub.nds.tlsattacker.core.protocol.message.ECDHClientKeyExchangeMessage;
 import de.rub.nds.tlsattacker.core.workflow.TlsContext;
+
+import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.spec.ECFieldFp;
+
 import org.bouncycastle.asn1.x9.X962NamedCurves;
 import org.bouncycastle.asn1.x9.X9ECParameters;
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
 import org.bouncycastle.crypto.params.ECDomainParameters;
 import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+import org.bouncycastle.crypto.params.ECNamedDomainParameters;
 import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
 import org.bouncycastle.crypto.params.ECPublicKeyParameters;
 import org.bouncycastle.crypto.tls.TlsECCUtils;
+import org.bouncycastle.jcajce.provider.asymmetric.util.EC5Util;
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.ECPointUtil;
+import org.bouncycastle.jce.spec.ECParameterSpec;
+import org.bouncycastle.jce.spec.ECPublicKeySpec;
+import org.bouncycastle.math.ec.ECPoint;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -42,7 +53,13 @@ public class ECDHClientKeyExchangePreparatorTest {
     private TlsContext context;
     private ECDHClientKeyExchangeMessage message;
     private ECDHClientKeyExchangePreparator preparator;
-    final static String RANDOM = "CAFEBABECAFE";
+    private final static String RANDOM = "CAFEBABECAFE";
+    private final static byte[] SERVER_PUB_KEY = ArrayConverter
+            .hexStringToByteArray("023683c91035ad3815282ce7a4a273678487fa031f802508c2");
+    private final static byte[] PREMASTER_SECRET = ArrayConverter
+            .hexStringToByteArray("81d3ba4e7a54eea7c584cf487bd6bea7dcfc27201f42a543");
+    private final static byte[] MASTER_SECRET = ArrayConverter
+            .hexStringToByteArray("44569804ebaef0c715a08bfea7272396c74bc75a3e1cf5c68cf6026286c27a1ddfcac31488692f14691fc8de62042004");
 
     public ECDHClientKeyExchangePreparatorTest() {
     }
@@ -57,71 +74,38 @@ public class ECDHClientKeyExchangePreparatorTest {
     /**
      * Test of prepareHandshakeMessageContents method, of class
      * ECDHClientKeyExchangePreparator.
+     * 
      * @throws java.security.NoSuchAlgorithmException
      * @throws java.security.NoSuchProviderException
      * @throws java.security.InvalidAlgorithmParameterException
      */
     @Test
-    public void testPrepare() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
-        //prepare context
+    public void testPrepare() throws NoSuchAlgorithmException, NoSuchProviderException,
+            InvalidAlgorithmParameterException {
+        // prepare context
         context.setSelectedProtocolVersion(ProtocolVersion.TLS12);
         context.setSelectedCipherSuite(CipherSuite.TLS_ECDH_RSA_WITH_AES_128_CBC_SHA256);
         context.setClientRandom(ArrayConverter.hexStringToByteArray(RANDOM));
         context.setServerRandom(ArrayConverter.hexStringToByteArray(RANDOM));
-        //set server ECDH-parameters
+        // set server ECDH-parameters
         X9ECParameters curve = X962NamedCurves.getByName("prime192v1");
-        ECKeyPairGenerator generator = new ECKeyPairGenerator();
-        ECDomainParameters domainParameters = new ECDomainParameters(curve.getCurve(), curve.getG(), curve.getN());
-        generator.init(new ECKeyGenerationParameters(domainParameters, RandomHelper.getBadSecureRandom()));
-        AsymmetricCipherKeyPair ecKeyPair = generator.generateKeyPair();
-        context.setServerECPublicKeyParameters((ECPublicKeyParameters)ecKeyPair.getPublic());
-
+        context.setServerECPublicKeyParameters(new ECPublicKeyParameters(curve.getCurve().decodePoint(SERVER_PUB_KEY),
+                new ECDomainParameters(curve.getCurve(), curve.getG(), curve.getN())));
 
         preparator.prepareHandshakeMessageContents();
 
-
-        //Tests
-        assertNotNull(
-                message.getPublicKeyBaseX()
-        );
-        assertNotNull(
-                message.getPublicKeyBaseY()
-        );
+        // Tests
+        assertNotNull(message.getPublicKeyBaseX());
+        assertNotNull(message.getPublicKeyBaseY());
+        assertArrayEquals(PREMASTER_SECRET, message.getComputations().getPremasterSecret().getValue());
+        assertArrayEquals(MASTER_SECRET, message.getComputations().getMasterSecret().getValue());
+        assertEquals(HandshakeByteLength.MASTER_SECRET, message.getComputations().getMasterSecret().getValue().length);
+        assertNotNull(message.getSerializedPublicKeyLength().getValue());
+        assertNotNull(message.getSerializedPublicKey());
+        assertNotNull(message.getComputations().getClientRandom());
         assertArrayEquals(
-                TlsECCUtils.calculateECDHBasicAgreement(
-                        new ECPublicKeyParameters(
-                                domainParameters.getCurve().createPoint(
-                                        message.getPublicKeyBaseX().getValue(),
-                                        message.getPublicKeyBaseY().getValue()),
-                                domainParameters
-                        ),
-                        (ECPrivateKeyParameters) ecKeyPair.getPrivate()
-                ),
-                message.getComputations().getPremasterSecret().getValue()
-        );
-        assertNotNull(
-                message.getComputations().getMasterSecret().getValue()
-        );
-        assertEquals(
-                HandshakeByteLength.MASTER_SECRET,
-                message.getComputations().getMasterSecret().getValue().length
-        );
-        assertNotNull(
-                message.getSerializedPublicKeyLength().getValue()
-        );
-        assertNotNull(
-                message.getSerializedPublicKey()
-        );
-        assertNotNull(
-                message.getComputations().getClientRandom()
-        );
-        assertArrayEquals(
-                ArrayConverter.concatenate(
-                        ArrayConverter.hexStringToByteArray(RANDOM),
-                        ArrayConverter.hexStringToByteArray(RANDOM)
-                ),
-                message.getComputations().getClientRandom().getValue()
-        );
+                ArrayConverter.concatenate(ArrayConverter.hexStringToByteArray(RANDOM),
+                        ArrayConverter.hexStringToByteArray(RANDOM)), message.getComputations().getClientRandom()
+                        .getValue());
     }
-
 }
