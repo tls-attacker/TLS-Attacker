81a894f788c8fb32d1f76928e0130333603f045f
Malena Ebert <malena-rub@ebert.li>
2017-06-17 11:53:29 +0200

    implemented unit test for the Finished Message Parser Class

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/parser/FinishedMessageParserTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/parser/FinishedMessageParserTest.java
@@ -8,6 +8,7 @@
  */
 package de.rub.nds.tlsattacker.core.protocol.parser;
 
+import de.rub.nds.modifiablevariable.util.ArrayConverter;
 import de.rub.nds.tlsattacker.core.protocol.parser.FinishedMessageParser;
 import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
@@ -22,13 +23,28 @@ import org.junit.runners.Parameterized;
 /**
  *
  * @author Robert Merget - robert.merget@rub.de
+ * @author Malena Ebert - malena-rub@ebert.li
  */
 @RunWith(Parameterized.class)
 public class FinishedMessageParserTest {
 
+    private static final byte[] MSG_RECORD_HANDSHAKE = new byte[] { 0x16 };
+    private static final byte[] MSG_RECORD_VERSION = new byte[] { 0x03, 0x03 };
+    private static final byte[] MSG_RECORD_LENGTH = new byte[] { 0x00, 0x00, 0x10 };
+    private static final byte[] MSG_HANDSHAKE_FINISHED = new byte[] { 0x14 };
+    private static final byte[] MSG_LENGTH = new byte[] { 0x00, 0x00, 0x0c };
+    private static final byte[] MSG_0_VERIFIED_DATA = new byte[] { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+            0x66, 0x55, 0x44, 0x33 };
+    private static final byte[] MSG_0 = ArrayConverter.concatenate(MSG_HANDSHAKE_FINISHED, MSG_LENGTH,
+            MSG_0_VERIFIED_DATA);
+    private static final byte[] MSG_1 = ArrayConverter.concatenate(MSG_RECORD_HANDSHAKE, MSG_RECORD_VERSION,
+            MSG_RECORD_LENGTH, MSG_0);
+
     @Parameterized.Parameters
     public static Collection<Object[]> generateData() {
-        return Arrays.asList(new Object[][] {}); // TODO add testcases!!
+        return Arrays.asList(new Object[][] {
+                { MSG_0, 0, MSG_0, HandshakeMessageType.FINISHED, 12, MSG_0_VERIFIED_DATA },
+                { MSG_1, 6, MSG_0, HandshakeMessageType.FINISHED, 12, MSG_0_VERIFIED_DATA } });
     }
 
     private final byte[] message;
