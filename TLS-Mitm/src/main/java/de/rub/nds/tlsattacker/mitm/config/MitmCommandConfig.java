8d69a5a20cc9c563fd59d3f6436bb0406d0168d9
pfsk4tsh <lucas.hartmann@rub.de>
2017-06-22 14:28:41 +0200

    feat: Add skeleton for TLS-Mitm module

---
+++ TLS-Mitm/src/main/java/de/rub/nds/tlsattacker/mitm/config/MitmCommandConfig.java
@@ -0,0 +1,113 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.rub.nds.tlsattacker.mitm.config;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.beust.jcommander.ParametersDelegate;
+
+import de.rub.nds.tlsattacker.core.config.TLSDelegateConfig;
+import de.rub.nds.tlsattacker.core.config.delegate.CertificateDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.CiphersuiteDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.EllipticCurveDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.GeneralDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.HeartbeatDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.MaxFragmentLengthDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.ProtocolVersionDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.SignatureAndHashAlgorithmDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.TransportHandlerDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.WorkflowInputDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.WorkflowOutputDelegate;
+import de.rub.nds.tlsattacker.core.config.delegate.WorkflowTypeDelegate;
+import de.rub.nds.tlsattacker.core.workflow.TlsConfig;
+import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
+import de.rub.nds.tlsattacker.core.config.delegate.MitmDelegate;
+
+/**
+ *
+ * @author Lucas Hartmann <firstname.lastname@rub.de>
+ */
+
+public class MitmCommandConfig extends TLSDelegateConfig {
+
+    protected static final Logger LOGGER = LogManager.getLogger(MitmCommandConfig.class);
+
+    public static final String COMMAND = "mitm";
+
+    @ParametersDelegate
+    private final GeneralDelegate generalDelegate;
+    @ParametersDelegate
+    private final CiphersuiteDelegate ciphersuiteDelegate;
+    @ParametersDelegate
+    private final ProtocolVersionDelegate protocolVersionDelegate;
+    @ParametersDelegate
+    private final EllipticCurveDelegate ellipticCurveDelegate;
+    @ParametersDelegate
+    private final MitmDelegate mitmDelegate;
+    @ParametersDelegate
+    private final SignatureAndHashAlgorithmDelegate signatureAndHashAlgorithmDelegate;
+    @ParametersDelegate
+    private final WorkflowInputDelegate workflowInputDelegate;
+    @ParametersDelegate
+    private final WorkflowOutputDelegate workflowOutputDelegate;
+    @ParametersDelegate
+    private final WorkflowTypeDelegate workflowTypeDelegate;
+    @ParametersDelegate
+    private final TransportHandlerDelegate transportHandlerDelegate;
+    @ParametersDelegate
+    private final HeartbeatDelegate heartbeatDelegate;
+    @ParametersDelegate
+    private final MaxFragmentLengthDelegate maxFragmentLengthDelegate;
+    @ParametersDelegate
+    private final CertificateDelegate certificateDelegate;
+
+    public MitmCommandConfig(GeneralDelegate delegate) {
+        super(delegate);
+        this.generalDelegate = delegate;
+        this.ciphersuiteDelegate = new CiphersuiteDelegate();
+        this.heartbeatDelegate = new HeartbeatDelegate();
+        this.ellipticCurveDelegate = new EllipticCurveDelegate();
+        this.protocolVersionDelegate = new ProtocolVersionDelegate();
+        this.mitmDelegate = new MitmDelegate();
+        this.signatureAndHashAlgorithmDelegate = new SignatureAndHashAlgorithmDelegate();
+        this.transportHandlerDelegate = new TransportHandlerDelegate();
+        this.workflowInputDelegate = new WorkflowInputDelegate();
+        this.workflowOutputDelegate = new WorkflowOutputDelegate();
+        this.workflowTypeDelegate = new WorkflowTypeDelegate();
+        this.maxFragmentLengthDelegate = new MaxFragmentLengthDelegate();
+        this.certificateDelegate = new CertificateDelegate();
+        addDelegate(maxFragmentLengthDelegate);
+        addDelegate(ciphersuiteDelegate);
+        addDelegate(ellipticCurveDelegate);
+        addDelegate(protocolVersionDelegate);
+        addDelegate(mitmDelegate);
+        addDelegate(signatureAndHashAlgorithmDelegate);
+        addDelegate(heartbeatDelegate);
+        addDelegate(workflowInputDelegate);
+        addDelegate(workflowOutputDelegate);
+        addDelegate(workflowTypeDelegate);
+        addDelegate(transportHandlerDelegate);
+        addDelegate(certificateDelegate);
+    }
+
+    @Override
+    public TlsConfig createConfig() {
+        TlsConfig config = super.createConfig();
+        if (config.getWorkflowTraceType() == null) {
+            LOGGER.warn("Setting workflowTraceType to MITM");
+            config.setWorkflowTraceType(WorkflowTraceType.HANDSHAKE);
+        }
+        return config;
+    }
+}
