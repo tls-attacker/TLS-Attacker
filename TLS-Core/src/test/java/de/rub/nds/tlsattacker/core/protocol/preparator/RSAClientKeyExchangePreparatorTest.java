fba35ffb7a88358d8cea456126c1656a134cca2e
ic0ns <robert.merget@rub.de>
2017-07-03 17:25:53 +0200

    formatted

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/preparator/RSAClientKeyExchangePreparatorTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/preparator/RSAClientKeyExchangePreparatorTest.java
@@ -64,7 +64,7 @@ public class RSAClientKeyExchangePreparatorTest {
         assertEquals((byte) 0x02, message.getComputations().getPlainPaddedPremasterSecret().getValue()[1]);
         assertEquals((byte) 0x00, message.getComputations().getPlainPaddedPremasterSecret().getValue()[message
                 .getComputations().getPadding().getValue().length + 2]);
-        
+
         assertNotNull(message.getSerializedPublicKeyLength().getValue());
         assertNotNull(message.getSerializedPublicKey());
     }
