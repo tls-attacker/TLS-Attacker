9ada2b40843fb43f83f8322f8790c5da3570ed50
ic0ns <robert.merget@rub.de>
2017-07-10 15:41:22 +0200

    Added NullCheck to toString method

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/message/ApplicationMessage.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/message/ApplicationMessage.java
@@ -16,6 +16,7 @@ import de.rub.nds.tlsattacker.core.protocol.handler.ApplicationHandler;
 import de.rub.nds.tlsattacker.core.protocol.handler.ProtocolMessageHandler;
 import de.rub.nds.tlsattacker.core.workflow.TlsConfig;
 import de.rub.nds.tlsattacker.core.workflow.TlsContext;
+import java.util.Arrays;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -72,7 +73,9 @@ public class ApplicationMessage extends ProtocolMessage {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(toCompactString());
-        sb.append("\n  Data:").append(ArrayConverter.bytesToHexString(data.getValue()));
+        if (data != null && data.getValue() != null) {
+            sb.append("\n  Data:").append(ArrayConverter.bytesToHexString(data.getValue()));
+        }
         return sb.toString();
     }
 
@@ -85,4 +88,30 @@ public class ApplicationMessage extends ProtocolMessage {
     public ProtocolMessageHandler getHandler(TlsContext context) {
         return new ApplicationHandler(context);
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 43 * hash + Arrays.hashCode(this.dataConfig);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ApplicationMessage other = (ApplicationMessage) obj;
+        if (!Arrays.equals(this.dataConfig, other.dataConfig)) {
+            return false;
+        }
+        return true;
+    }
+
 }
