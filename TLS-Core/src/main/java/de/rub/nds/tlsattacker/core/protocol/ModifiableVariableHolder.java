b3d70241c2e83715771104806cbbc98d1a1a6582
ic0ns <robert.merget@rub.de>
2017-07-17 13:14:46 +0200

    Added Logger class to ModifiableVariableHolder

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/ModifiableVariableHolder.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/ModifiableVariableHolder.java
@@ -15,12 +15,17 @@ import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * @author Juraj Somorovsky <juraj.somorovsky@rub.de>
  */
 public abstract class ModifiableVariableHolder implements Serializable {
 
+    protected static final Logger LOGGER = LogManager.getLogger(ModifiableVariableHolder.class.getName());
+
+    
     /**
      * Lists all the modifiable variables declared in the class
      * 
