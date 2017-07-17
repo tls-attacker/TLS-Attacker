133226770e44419b7f6883af1cbd9ac053771a6b
ic0ns <robert.merget@rub.de>
2017-07-17 11:45:28 +0200

    Removed dh Certificate, Updated existing certificate code added TLSConfigTest to validate default certificates

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/workflow/TlsConfigTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/workflow/TlsConfigTest.java
@@ -8,7 +8,21 @@
  */
 package de.rub.nds.tlsattacker.core.workflow;
 
+import de.rub.nds.modifiablevariable.util.ArrayConverter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.Security;
+import org.bouncycastle.crypto.tls.Certificate;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemReader;
 import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -17,8 +31,23 @@ import org.junit.Test;
  * @author Robert Merget - robert.merget@rub.de
  */
 public class TlsConfigTest {
+
+    @Before
+    public void setUp() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
     @Test
     public void testReadFromResource() {
         assertNotNull(TlsConfig.createConfig());
+    }
+
+    @Test
+    public void testDefaultCertificates() throws IOException {
+        TlsConfig config = TlsConfig.createConfig();
+        Certificate cert = Certificate.parse(new ByteArrayInputStream(config.getDefaultRsaCertificate()));
+        cert = Certificate.parse(new ByteArrayInputStream(config.getDefaultEcCertificate()));
+        cert = Certificate.parse(new ByteArrayInputStream(config.getDefaultDsaCertificate()));
+
     }
 }
