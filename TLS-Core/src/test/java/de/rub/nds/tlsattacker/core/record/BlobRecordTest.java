2a698e87a17009c5bf95e1c11bfa964faefad9f0
ic0ns <robert.merget@rub.de>
2017-07-18 16:06:02 +0200

    Added Blobrecord Test template

---
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/record/BlobRecordTest.java
@@ -0,0 +1,70 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.core.record;
+
+import de.rub.nds.tlsattacker.core.constants.ChooserType;
+import de.rub.nds.tlsattacker.core.constants.ProtocolMessageType;
+import de.rub.nds.tlsattacker.core.record.crypto.Encryptor;
+import de.rub.nds.tlsattacker.core.record.preparator.BlobRecordPreparator;
+import de.rub.nds.tlsattacker.core.workflow.TlsConfig;
+import de.rub.nds.tlsattacker.core.workflow.TlsContext;
+import de.rub.nds.tlsattacker.core.workflow.chooser.Chooser;
+import de.rub.nds.tlsattacker.core.workflow.chooser.ChooserFactory;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Robert Merget <robert.merget@rub.de>
+ */
+public class BlobRecordTest {
+
+    private BlobRecord record;
+    private Chooser chooser;
+    private Encryptor<BlobRecord> encryptor;
+
+    @Before
+    public void setUp() {
+        record = new BlobRecord(TlsConfig.createConfig());
+        chooser = ChooserFactory.getChooser(ChooserType.DEFAULT, new TlsContext());
+
+    }
+
+    /**
+     * Test of getRecordPreparator method, of class BlobRecord.
+     */
+    @Test
+    public void testGetRecordPreparator() {
+        assertEquals(record.getRecordPreparator(chooser, encryptor, ProtocolMessageType.ALERT).getClass(),
+                BlobRecordPreparator.class);
+    }
+
+    /**
+     * Test of getRecordParser method, of class BlobRecord.
+     */
+    @Test
+    public void testGetRecordParser() {
+    }
+
+    /**
+     * Test of getRecordSerializer method, of class BlobRecord.
+     */
+    @Test
+    public void testGetRecordSerializer() {
+    }
+
+    /**
+     * Test of adjustContext method, of class BlobRecord.
+     */
+    @Test
+    public void testAdjustContext() {
+    }
+
+}
