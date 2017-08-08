834499d84cd4c1f2ae23132228fc6ae80cdc70ea
ic0ns <robert.merget@rub.de>
2017-08-08 10:25:21 +0200

    Changed LogLevel of ExceptionOutput in TLS-RecordLayer

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/record/layer/TlsRecordLayer.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/record/layer/TlsRecordLayer.java
@@ -68,7 +68,8 @@ public class TlsRecordLayer extends RecordLayer {
                 records.add(record);
                 dataPointer = parser.getPointer();
             } catch (ParserException E) {
-                LOGGER.debug("Could not parse Record, parsing as Blob", E);
+                LOGGER.debug("Could not parse Record, parsing as Blob");
+                LOGGER.trace(E);
                 BlobRecordParser blobParser = new BlobRecordParser(dataPointer, rawRecordData,
                         tlsContext.getSelectedProtocolVersion());
                 AbstractRecord record = blobParser.parse();
