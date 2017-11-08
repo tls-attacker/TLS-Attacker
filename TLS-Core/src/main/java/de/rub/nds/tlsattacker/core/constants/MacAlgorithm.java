477934bddea20eeef20daa9927dd050a7c816838
ic0ns <robert.merget@rub.de>
2017-11-08 18:13:21 +0100

    Added MacAlgorithm size

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/MacAlgorithm.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/MacAlgorithm.java
@@ -13,18 +13,18 @@ package de.rub.nds.tlsattacker.core.constants;
  */
 public enum MacAlgorithm {
 
-    NULL("null",0),
-    AEAD("null",0),
+    NULL("null", 0),
+    AEAD("null", 0),
     HMAC_MD5("HmacMD5", 16),
     HMAC_SHA1("HmacSHA1", 20),
     HMAC_SHA256("HmacSHA256", 32),
     HMAC_SHA384("HmacSHA384", 48),
     HMAC_SHA512("HmacSHA512", 64),
-    IMIT_GOST28147("IMIT_GOST28147",0), // java name not verified, size unknown
-    HMAC_GOSTR3411("HmacGOSTR3411",0);// java name not verified
-    
+    IMIT_GOST28147("IMIT_GOST28147", 0), // java name not verified, size unknown
+    HMAC_GOSTR3411("HmacGOSTR3411", 0);// java name not verified
+
     private int size;
-    
+
     MacAlgorithm(String javaName, int size) {
         this.javaName = javaName;
         this.size = size;
