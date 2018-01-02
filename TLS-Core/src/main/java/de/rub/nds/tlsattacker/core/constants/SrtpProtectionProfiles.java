ca63359ee899cec8a4a378d5928420b1133f1ce3
ic0ns <robert.merget@rub.de>
2018-01-02 11:12:47 +0100

    Added check to SRTP protection profiles to prevent index out of bounds exceptions

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/SrtpProtectionProfiles.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/SrtpProtectionProfiles.java
@@ -17,10 +17,10 @@ import java.util.Map;
  * RFC5764
  */
 public enum SrtpProtectionProfiles {
-    SRTP_AES128_CM_HMAC_SHA1_80(new byte[] { 0x00, 0x01 }),
-    SRTP_AES128_CM_HMAC_SHA1_32(new byte[] { 0x00, 0x02 }),
-    SRTP_NULL_HMAC_SHA1_80(new byte[] { 0x00, 0x05 }),
-    SRTP_NULL_HMAC_SHA1_32(new byte[] { 0x00, 0x06 });
+    SRTP_AES128_CM_HMAC_SHA1_80(new byte[]{0x00, 0x01}),
+    SRTP_AES128_CM_HMAC_SHA1_32(new byte[]{0x00, 0x02}),
+    SRTP_NULL_HMAC_SHA1_80(new byte[]{0x00, 0x05}),
+    SRTP_NULL_HMAC_SHA1_32(new byte[]{0x00, 0x06});
 
     private final byte[] srtpProtectionProfiles;
     private static final Map<Integer, SrtpProtectionProfiles> MAP;
@@ -45,10 +45,12 @@ public enum SrtpProtectionProfiles {
     }
 
     public static List<SrtpProtectionProfiles> getProfilesAsArrayList(byte[] value) {
-        List<SrtpProtectionProfiles> profileList = new ArrayList<>(value.length / 2);
+        List<SrtpProtectionProfiles> profileList = new ArrayList<>();
 
         for (int i = 0; i < value.length; i += 2) {
-            profileList.add(SrtpProtectionProfiles.getProfileByType(new byte[] { value[i], value[i + 1] }));
+            if (value.length > i) {
+                profileList.add(SrtpProtectionProfiles.getProfileByType(new byte[]{value[i], value[i + 1]}));
+            }
         }
 
         return profileList;
