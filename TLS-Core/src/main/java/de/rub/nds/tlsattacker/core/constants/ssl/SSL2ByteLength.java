1d5def9a613fc1bec09cd21fc9f39cf2f31b4eb4
ic0ns <robert.merget@rub.de>
2018-01-22 12:43:47 +0100

    Cleaned up the new SSL2ServerHelloParser code and added the peek() method to the parser

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/ssl/SSL2ByteLength.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/ssl/SSL2ByteLength.java
@@ -14,6 +14,8 @@ package de.rub.nds.tlsattacker.core.constants.ssl;
 public class SSL2ByteLength {
     public static final int LENGTH = 2;
 
+    public static final int LONG_LENGTH = 3;
+
     public static final int MESSAGE_TYPE = 1;
 
     public static final int VERSION = 2;
