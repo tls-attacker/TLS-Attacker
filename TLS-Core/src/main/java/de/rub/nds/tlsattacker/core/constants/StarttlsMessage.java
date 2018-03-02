05d45f823d4083f4bc73e98e9f21375577a4b9f4
Asli Yardim <asli.yardim@rub.de>
2018-03-02 18:11:23 +0100

    Updated STARTTLS Messages; works with GnuTLS now.

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/StarttlsMessage.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/StarttlsMessage.java
@@ -9,15 +9,15 @@
 package de.rub.nds.tlsattacker.core.constants;
 
 public enum StarttlsMessage {
-    FTP_S_CONNECTED("220\r\n"),
+    FTP_S_CONNECTED("211-Extensions supported\r\nAUTH TLS\r\n211 END\r\n"),
     FTP_TLS("AUTH TLS\r\n"),
-    FTP_S_READY("234\r\n"),
+    FTP_S_READY("234 AUTH command ok. Initializing TLS Connection.\r\n"),
     IMAP_S_CONNECTED(". OK IMAP4rev1 Service Ready\r\n"),
-    IMAP_C_CAP(". CAPABILITY\r\n"),
-    IMAP_S_CAP(". CAPABILITY IMAP4rev1 STARTTLS LOGINDISABLED\r\n"),
-    IMAP_TLS(". STARTTLS\r\n"),
-    IMAP_S_READY(". OK BEGIN TLS NEGOTIATION\r\n"),
-    POP3_S_CONNECTED("SERVICE READY\r\n"),
+    IMAP_C_CAP("a CAPABILITY\r\n"),
+    IMAP_S_CAP(". CAPABILITY IMAP4rev1 STARTTLS LOGINDISABLED\r\na OK CAPABILITY completed\r\n"),
+    IMAP_TLS("a STARTTLS\r\n"),
+    IMAP_S_READY("a OK BEGIN TLS NEGOTIATION\r\n"),
+    POP3_S_CONNECTED("+OK Service Ready\r\n"),
     POP3_TLS("STLS\r\n"),
     POP3_S_READY("+OK Begin TLS negotiation\r\n"),
     SMTP_S_CONNECTED("220 mail.example.com SMTP service ready\r\n"),
