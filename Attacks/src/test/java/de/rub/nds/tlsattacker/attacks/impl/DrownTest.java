daa328039425720878138cb237944e5a214018ba
ic0ns <robert.merget@rub.de>
2018-04-09 12:39:25 +0200

    Moved DrownTest to different Package

--- Attacks/src/test/java/de/rub/nds/tlsattacker/attacks/impl/DrownTest.java
+++ Attacks/src/test/java/de/rub/nds/tlsattacker/attacks/impl/DrownTest.java
@@ -6,7 +6,7 @@
  * Licensed under Apache License 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
-package de.rub.nds.tlsattacker.attacks.docker;
+package de.rub.nds.tlsattacker.attacks.impl;
 
 import de.rub.nds.tls.subject.TlsImplementationType;
 import static org.junit.Assert.assertEquals;
