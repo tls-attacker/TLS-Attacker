1c6cf9f36da816e04a5979d93e5bfdaaf2c63270
Philipp Nieting <philipp@nieting.de>
2018-04-11 00:13:46 +0200

    Improved HttpsParserTests
    - Added HttpsRequestParserTest
    - Improved HttpsResponseParserTest with valid response test case
    - Improved HttpsReposnseParser and HttpsRequestParser with consistent parsing behaviour

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/https/HttpsResponseParserTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/https/HttpsResponseParserTest.java
@@ -13,26 +13,69 @@ import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlsattacker.core.exceptions.ParserException;
 import org.junit.Before;
 import org.junit.Test;
+import java.nio.charset.Charset;
+import static org.junit.Assert.assertEquals;
 
 public class HttpsResponseParserTest {
 
-    private HttpsResponseParser parser;
 
     public HttpsResponseParserTest() {
     }
 
     @Before
     public void setUp() {
-        parser = new HttpsResponseParser(0, ArrayConverter.hexStringToByteArray("AAAAAAAAAAAAAAAAAAAAAAAA"),
-                ProtocolVersion.TLS12);
+
     }
 
     /**
-     * Test of parseMessageContent method, of class HttpsResponseParser.
+     * Test of parseMessageContent method, of class HttpsResponseParser with an invalid response.
      */
     @Test(expected = ParserException.class)
-    public void testParseMessageContent() {
+    public void testParseMessageContentFailed() {
+        HttpsResponseParser parser = new HttpsResponseParser(0, ArrayConverter.hexStringToByteArray("AAAAAAAAAAAAAAAAAAAAAAAA"),
+                ProtocolVersion.TLS12);
         parser.parse();
     }
 
+
+    /**
+     * Test of parseMessageContent method, of class HttpsResponseParser with a valid response.
+     */
+    @Test
+    public void testParseMessageContentSuccess() {
+        String message = "HTTP/1.1 200 OK\n" +
+                        "Date: Mon, 27 Jul 2009 12:28:53 GMT\n" +
+                        "Server: Apache/2.2.14 (Win32)\n" +
+                        "Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT\n" +
+                        "Content-Length: 88\n" +
+                        "Content-Type: text/html\n" +
+                        "Connection: Closed\n" +
+                        "\n" +
+                        "test";
+
+        HttpsResponseParser parser = new HttpsResponseParser(0, message.getBytes(Charset.forName("UTF-8")), ProtocolVersion.TLS12);
+        HttpsResponseMessage parsedMessage = parser.parse();
+
+        assertEquals(parsedMessage.getResponseStatusCode().getValue(), "200 OK");
+        assertEquals(parsedMessage.getResponseProtocol().getValue(), "HTTP/1.1");
+        assertEquals(parsedMessage.getResponseContent().getValue(), "test");
+
+        assertEquals(parsedMessage.getHeader().get(0).getHeaderName().getValue(), "Date");
+        assertEquals(parsedMessage.getHeader().get(0).getHeaderValue().getValue(), "Mon, 27 Jul 2009 12:28:53 GMT");
+
+        assertEquals(parsedMessage.getHeader().get(1).getHeaderName().getValue(), "Server");
+        assertEquals(parsedMessage.getHeader().get(1).getHeaderValue().getValue(), "Apache/2.2.14 (Win32)");
+
+        assertEquals(parsedMessage.getHeader().get(2).getHeaderName().getValue(), "Last-Modified");
+        assertEquals(parsedMessage.getHeader().get(2).getHeaderValue().getValue(), "Wed, 22 Jul 2009 19:15:56 GMT");
+
+        assertEquals(parsedMessage.getHeader().get(3).getHeaderName().getValue(), "Content-Length");
+        assertEquals(parsedMessage.getHeader().get(3).getHeaderValue().getValue(), "88");
+
+        assertEquals(parsedMessage.getHeader().get(4).getHeaderName().getValue(), "Content-Type");
+        assertEquals(parsedMessage.getHeader().get(4).getHeaderValue().getValue(), "text/html");
+
+        assertEquals(parsedMessage.getHeader().get(5).getHeaderName().getValue(), "Connection");
+        assertEquals(parsedMessage.getHeader().get(5).getHeaderValue().getValue(), "Closed");
+    }
 }
