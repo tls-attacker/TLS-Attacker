4ab074adaa217319ef9289b3733f482252c6ca88
Nimrod Aviram <nimrod.aviram@gmail.com>
2018-06-03 20:59:59 +0300

    Auto-formatting.

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/EarlyFinishedAttacker.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/EarlyFinishedAttacker.java
@@ -1,3 +1,11 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 package de.rub.nds.tlsattacker.attacks.impl;
 
 import java.util.LinkedList;
@@ -36,73 +44,74 @@ import de.rub.nds.tlsattacker.transport.ConnectionEndType;
 
 public class EarlyFinishedAttacker extends Attacker<EarlyFinishedCommandConfig> {
 
-	public EarlyFinishedAttacker(EarlyFinishedCommandConfig config) {
-		super(config);
-	}
+    public EarlyFinishedAttacker(EarlyFinishedCommandConfig config) {
+        super(config);
+    }
 
-	@Override
-	public void executeAttack() {
-		throw new UnsupportedOperationException("Not implemented yet");
-	}
+    @Override
+    public void executeAttack() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 
-	@Override
-	public Boolean isVulnerable() {
-		EarlyCcsVulnerabilityType earlyCcsVulnerabilityType = getEarlyCcsVulnerabilityType();
-		switch (earlyCcsVulnerabilityType) {
-		case VULN_EXPLOITABLE:
-		case VULN_NOT_EXPLOITABLE:
-			return true;
-		case NOT_VULNERABLE:
-			return false;
-		case UNKNOWN:
-			return null;
-		}
-		return null;
-	}
+    @Override
+    public Boolean isVulnerable() {
+        EarlyCcsVulnerabilityType earlyCcsVulnerabilityType = getEarlyCcsVulnerabilityType();
+        switch (earlyCcsVulnerabilityType) {
+            case VULN_EXPLOITABLE:
+            case VULN_NOT_EXPLOITABLE:
+                return true;
+            case NOT_VULNERABLE:
+                return false;
+            case UNKNOWN:
+                return null;
+        }
+        return null;
+    }
 
-	public boolean checkTargetVersion() {
-		Config tlsConfig = config.createConfig();
-		tlsConfig.setFiltersKeepUserSettings(false);
+    public boolean checkTargetVersion() {
+        Config tlsConfig = config.createConfig();
+        tlsConfig.setFiltersKeepUserSettings(false);
 
-		WorkflowConfigurationFactory workflowConfigurationFactory = new WorkflowConfigurationFactory(tlsConfig);
-		OutboundConnection connection = tlsConfig.getDefaultClientConnection();
-		WorkflowTrace workflowTrace = workflowConfigurationFactory.createHelloWorkflow(connection);
-		
-		// TODO: This shares code with WorkflowConfigurationFactory, and not in a good way :-)
-		List<ProtocolMessage> messages = new LinkedList<>();
-		workflowConfigurationFactory.addClientKeyExchangeMessage(messages);
-		messages.add(new ChangeCipherSpecMessage(tlsConfig));
-		workflowTrace.addTlsAction(MessageActionFactory.createAction(connection, ConnectionEndType.CLIENT, messages));
+        WorkflowConfigurationFactory workflowConfigurationFactory = new WorkflowConfigurationFactory(tlsConfig);
+        OutboundConnection connection = tlsConfig.getDefaultClientConnection();
+        WorkflowTrace workflowTrace = workflowConfigurationFactory.createHelloWorkflow(connection);
+
+        // TODO: This shares code with WorkflowConfigurationFactory, and not in
+        // a good way :-)
+        List<ProtocolMessage> messages = new LinkedList<>();
+        workflowConfigurationFactory.addClientKeyExchangeMessage(messages);
+        messages.add(new ChangeCipherSpecMessage(tlsConfig));
+        workflowTrace.addTlsAction(MessageActionFactory.createAction(connection, ConnectionEndType.CLIENT, messages));
 
         messages = new LinkedList<>();
         messages.add(new ChangeCipherSpecMessage(tlsConfig));
         messages.add(new FinishedMessage(tlsConfig));
-		workflowTrace.addTlsAction(MessageActionFactory.createAction(connection, ConnectionEndType.SERVER, messages));
-		
-		State state = new State(tlsConfig, workflowTrace);
-		WorkflowExecutor workflowExecutor = WorkflowExecutorFactory
-				.createWorkflowExecutor(tlsConfig.getWorkflowExecutorType(), state);
-		workflowExecutor.executeWorkflow();
+        workflowTrace.addTlsAction(MessageActionFactory.createAction(connection, ConnectionEndType.SERVER, messages));
 
-		if (WorkflowTraceUtil.didReceiveMessage(ProtocolMessageType.ALERT, workflowTrace)) {
-			LOGGER.log(LogLevel.CONSOLE_OUTPUT, "Not vulnerable (definitely), Alert message found");
-			return false;
-		} else if (WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.FINISHED, workflowTrace)) {
-			LOGGER.log(LogLevel.CONSOLE_OUTPUT, "Vulnerable (definitely), Finished message found");
-			return true;
-		} else {
-			LOGGER.log(LogLevel.CONSOLE_OUTPUT,
-					"Not vulnerable (probably), No Finished message found, yet also no alert");
-			return false;
-		}
-	}
+        State state = new State(tlsConfig, workflowTrace);
+        WorkflowExecutor workflowExecutor = WorkflowExecutorFactory.createWorkflowExecutor(
+                tlsConfig.getWorkflowExecutorType(), state);
+        workflowExecutor.executeWorkflow();
 
-	public EarlyCcsVulnerabilityType getEarlyCcsVulnerabilityType() {
-		// TODO: Changed return value
-		if (checkTargetVersion()) {
-			return EarlyCcsVulnerabilityType.VULN_EXPLOITABLE;
-		}
-		return EarlyCcsVulnerabilityType.NOT_VULNERABLE;
-	}
+        if (WorkflowTraceUtil.didReceiveMessage(ProtocolMessageType.ALERT, workflowTrace)) {
+            LOGGER.log(LogLevel.CONSOLE_OUTPUT, "Not vulnerable (definitely), Alert message found");
+            return false;
+        } else if (WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.FINISHED, workflowTrace)) {
+            LOGGER.log(LogLevel.CONSOLE_OUTPUT, "Vulnerable (definitely), Finished message found");
+            return true;
+        } else {
+            LOGGER.log(LogLevel.CONSOLE_OUTPUT,
+                    "Not vulnerable (probably), No Finished message found, yet also no alert");
+            return false;
+        }
+    }
+
+    public EarlyCcsVulnerabilityType getEarlyCcsVulnerabilityType() {
+        // TODO: Changed return value
+        if (checkTargetVersion()) {
+            return EarlyCcsVulnerabilityType.VULN_EXPLOITABLE;
+        }
+        return EarlyCcsVulnerabilityType.NOT_VULNERABLE;
+    }
 
 }
