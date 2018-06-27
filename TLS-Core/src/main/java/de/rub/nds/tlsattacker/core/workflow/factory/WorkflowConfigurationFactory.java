47c37f6494d9df8ebe951899dc32e63baaf11fe3
Nimrod Aviram <nimrod.aviram@gmail.com>
2018-06-27 20:10:59 +0300

    Early Finished.

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/workflow/factory/WorkflowConfigurationFactory.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/workflow/factory/WorkflowConfigurationFactory.java
@@ -167,14 +167,15 @@ public class WorkflowConfigurationFactory {
      *
      * @return A HelloWorkflow
      */
-    private WorkflowTrace createHelloWorkflow() {
+    public WorkflowTrace createHelloWorkflow() {
         return createHelloWorkflow(getConnection());
     }
 
     /**
      * Create a hello workflow for the given connection end.
      */
-    private WorkflowTrace createHelloWorkflow(AliasedConnection connection) {
+    // TODO: Should I have changed this method to public? :-)
+    public WorkflowTrace createHelloWorkflow(AliasedConnection connection) {
         WorkflowTrace workflowTrace = new WorkflowTrace();
 
         if (config.getStarttlsType() != StarttlsType.NONE) {
@@ -745,7 +746,8 @@ public class WorkflowConfigurationFactory {
         return null;
     }
 
-    private void addClientKeyExchangeMessage(List<ProtocolMessage> messages) {
+    // TODO: Public? :-)
+    public void addClientKeyExchangeMessage(List<ProtocolMessage> messages) {
         CipherSuite cs = config.getDefaultSelectedCipherSuite();
         ClientKeyExchangeMessage message = createClientKeyExchangeMessage(AlgorithmResolver.getKeyExchangeAlgorithm(cs));
         messages.add(message);
