5dbb5af5d80737842e65d1ae05fdd5e93cd00fca
ic0ns <robert.merget@rub.de>
2018-08-06 14:36:17 +0200

    Fixed TLS 1.0 and TLS 1.1 Signatures for ECDHE and DHE

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/crypto/SignatureCalculator.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/crypto/SignatureCalculator.java
@@ -10,6 +10,7 @@ package de.rub.nds.tlsattacker.core.crypto;
 
 import de.rub.nds.modifiablevariable.util.ArrayConverter;
 import de.rub.nds.modifiablevariable.util.BadRandom;
+import de.rub.nds.tlsattacker.core.constants.AlgorithmResolver;
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlsattacker.core.constants.SignatureAndHashAlgorithm;
 import de.rub.nds.tlsattacker.core.exceptions.CryptoException;
@@ -51,23 +52,27 @@ public class SignatureCalculator {
 
     public static byte[] generateSignature(PrivateKey key, byte[] toBeSigned, SignatureAndHashAlgorithm algorithm,
             BadRandom random, Chooser chooser) throws CryptoException {
-        try {
-            if (chooser.getSelectedProtocolVersion() == ProtocolVersion.SSL3
-                    || chooser.getSelectedProtocolVersion() == ProtocolVersion.TLS10) {
 
-                byte[] legacyToBeSigned = ArrayConverter.concatenate(MD5Utils.MD5(toBeSigned),
-                        SHA1Utils.sha1(toBeSigned));
-                Signature instance = Signature.getInstance("RSA/None/PKCS1Padding");
-                instance.update(legacyToBeSigned);
-                return instance.sign();
+        String algoName;
+        if (chooser.getSelectedProtocolVersion() == ProtocolVersion.SSL3
+                || chooser.getSelectedProtocolVersion() == ProtocolVersion.TLS10
+                || chooser.getSelectedProtocolVersion() == ProtocolVersion.TLS11) {
+            if (AlgorithmResolver.getKeyExchangeAlgorithm(chooser.getSelectedCipherSuite()).name().contains("RSA")) {
+                algoName = "NONEwithRSA";
+                toBeSigned = ArrayConverter.concatenate(MD5Utils.MD5(toBeSigned), SHA1Utils.sha1(toBeSigned));
             } else {
-                LOGGER.trace("Creating Signature with " + algorithm.getJavaName() + " over "
-                        + ArrayConverter.bytesToHexString(toBeSigned) + " with the PrivateKey:" + key.toString());
-                Signature instance = Signature.getInstance(algorithm.getJavaName());
-                instance.initSign(key, random);
-                instance.update(toBeSigned);
-                return instance.sign();
+                algoName = "SHA1withECDSA";
             }
+        } else {
+            algoName = algorithm.getJavaName();
+        }
+        try {
+            LOGGER.trace("Creating Signature with " + algoName + " over " + ArrayConverter.bytesToHexString(toBeSigned)
+                    + " with the PrivateKey:" + key.toString());
+            Signature instance = Signature.getInstance(algoName);
+            instance.initSign(key, random);
+            instance.update(toBeSigned);
+            return instance.sign();
         } catch (SignatureException | InvalidKeyException | NoSuchAlgorithmException ex) {
             throw new CryptoException("Could not sign Data", ex);
         }
