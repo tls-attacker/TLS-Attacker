3fdfa14e7e80217ac021ebdb93e1ea39aac4c168
columbkh <39829293+columbkh@users.noreply.github.com>
2018-08-24 11:24:27 +0200

    added two tests (#492)
    * added two tests
    
    * fixed the issues

---
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/workflow/action/ActivateEncryptionActionTest.java
@@ -0,0 +1,81 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.core.workflow.action;
+
+import de.rub.nds.tlsattacker.core.config.Config;
+import de.rub.nds.tlsattacker.core.constants.CipherSuite;
+import de.rub.nds.tlsattacker.core.record.layer.BlobRecordLayer;
+import de.rub.nds.tlsattacker.core.state.State;
+import de.rub.nds.tlsattacker.core.state.TlsContext;
+import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
+import de.rub.nds.tlsattacker.util.tests.SlowTests;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import de.rub.nds.tlsattacker.core.record.layer.TlsRecordLayer;
+import de.rub.nds.tlsattacker.core.record.cipher.RecordNullCipher;
+
+import static org.junit.Assert.*;
+
+public class ActivateEncryptionActionTest {
+
+    private State state;
+    private TlsContext tlsContext;
+    private ActivateEncryptionAction action;
+
+    @Before
+    public void setUp() {
+        Config config = Config.createConfig();
+        action = new ActivateEncryptionAction();
+        WorkflowTrace trace = new WorkflowTrace();
+        trace.addTlsAction(action);
+        state = new State(config, trace);
+        tlsContext = state.getTlsContext();
+        tlsContext.setSelectedCipherSuite(CipherSuite.TLS_DHE_DSS_WITH_AES_128_CBC_SHA);
+        tlsContext.setRecordLayer(new TlsRecordLayer(tlsContext));
+    }
+
+    @Test
+    public void testExecute() throws Exception {
+        action.execute(state);
+        assertTrue(action.isExecuted());
+        TlsRecordLayer layer = TlsRecordLayer.class.cast(tlsContext.getRecordLayer());
+        assertFalse(layer.getRecordCipher() instanceof RecordNullCipher);
+        assertFalse(layer.getEncryptor() instanceof RecordNullCipher);
+        assertFalse(layer.getDecryptor() instanceof RecordNullCipher);
+    }
+
+    @Test
+    public void testReset() throws Exception {
+        assertFalse(action.isExecuted());
+        action.execute(state);
+        assertTrue(action.isExecuted());
+        action.reset();
+        assertFalse(action.isExecuted());
+    }
+
+    @Test
+    @Category(SlowTests.class)
+    public void marshalingEmptyActionYieldsMinimalOutput() {
+        ActionTestUtils.marshalingEmptyActionYieldsMinimalOutput(ActivateEncryptionAction.class);
+    }
+
+    @Test
+    @Category(SlowTests.class)
+    public void marshalingAndUnmarshalingEmptyObjectYieldsEqualObject() {
+        ActionTestUtils.marshalingAndUnmarshalingEmptyObjectYieldsEqualObject(ActivateEncryptionAction.class);
+    }
+
+    @Test
+    @Category(SlowTests.class)
+    public void marshalingAndUnmarshalingFilledObjectYieldsEqualObject() {
+        ActionTestUtils.marshalingAndUnmarshalingFilledObjectYieldsEqualObject(action);
+    }
+
+}
