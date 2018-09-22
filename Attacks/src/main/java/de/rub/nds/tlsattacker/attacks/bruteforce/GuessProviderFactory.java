eec826e5ee4a8ac713dbdb59e9e0f07bb8686eb8
ic0ns <robert.merget@rub.de>
2018-09-22 21:37:14 +0000

    Added Javadoc for the GuessProviderFactory

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/bruteforce/GuessProviderFactory.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/bruteforce/GuessProviderFactory.java
@@ -11,16 +11,21 @@ package de.rub.nds.tlsattacker.attacks.bruteforce;
 import java.io.InputStream;
 
 /**
- *
- * @author robert
+ * A Factory class for GuessProvider instances, which creates GuessProvider
+ * objects based on a GuessProviderType.
  */
 public class GuessProviderFactory {
 
     /**
+     * Creates GuessProvider objects based on a GuessProviderType. Some
+     * GuessProvider require an InputSource to create their guesses. If the
+     * GuessProvider does not use an InputStream, the InputStream is ignored
      *
-     * @param type
-     * @param guessSource
-     * @return
+     * @param type Type of the GuessProvider which should be created
+     * @param guessSource An InputStream as an input source for the
+     * GuessProvider. If the GuessProvider does not use an InputStream the
+     * guessSource is ignored.
+     * @return A new GuessProvider object.
      */
     public static GuessProvider createGuessProvider(GuessProviderType type, InputStream guessSource) {
         switch (type) {
