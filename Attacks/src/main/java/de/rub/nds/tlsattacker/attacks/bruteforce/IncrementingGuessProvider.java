11ae5ffc87ba8948279f097aa1cf0df135d8b779
ic0ns <robert.merget@rub.de>
2018-09-22 21:47:14 +0000

    Added Javadoc for the IncrementingGuessProvider

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/bruteforce/IncrementingGuessProvider.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/bruteforce/IncrementingGuessProvider.java
@@ -9,8 +9,10 @@
 package de.rub.nds.tlsattacker.attacks.bruteforce;
 
 /**
- *
- * @author robert
+ * An IncrementingGuessProvider is a GuessProvider which tries all byte[]
+ * sequences in a growing order. Starting by an empty byte[] and then continuing
+ * the sequence with th byte[] size increased by 1. It would then try 00, 01,
+ * 02, ..., FF and continue with 00 00, 00 01, 00 02 , ... to FF, FF and so on.
  */
 public class IncrementingGuessProvider extends GuessProvider {
 
@@ -19,14 +21,15 @@ public class IncrementingGuessProvider extends GuessProvider {
     private int size = 0;
 
     /**
-     *
+     * Default Constructor
      */
     public IncrementingGuessProvider() {
         super(GuessProviderType.INCREMENTING);
     }
 
     /**
-     *
+     * Returns the last Guess incremented by 1.
+     * 
      * @return
      */
     @Override
@@ -35,11 +38,7 @@ public class IncrementingGuessProvider extends GuessProvider {
         return guess;
     }
 
-    /**
-     *
-     * @return
-     */
-    public byte[] getIncrementedGuess() {
+    private byte[] getIncrementedGuess() {
         if (lastGuess == null) {
             lastGuess = new byte[size];
         } else {
@@ -52,13 +51,7 @@ public class IncrementingGuessProvider extends GuessProvider {
         return lastGuess;
     }
 
-    /**
-     *
-     * @param array
-     * @param position
-     * @return
-     */
-    public byte[] createdIncrementedAtPosition(byte[] array, int position) {
+    private byte[] createdIncrementedAtPosition(byte[] array, int position) {
         if (array.length > position) {
             array[position] = (byte) (array[position] + 1);
             if (array[position] == 0) {
