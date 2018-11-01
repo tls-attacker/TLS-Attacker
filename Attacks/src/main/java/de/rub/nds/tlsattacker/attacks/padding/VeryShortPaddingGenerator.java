82f2e57b8beb8b5506a7fe30914359e68d6a6d39
ic0ns <robert.merget@rub.de>
2018-11-01 18:19:45 +0100

    Adjusted veryshort padding generator

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/padding/VeryShortPaddingGenerator.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/padding/VeryShortPaddingGenerator.java
@@ -8,14 +8,15 @@
  */
 package de.rub.nds.tlsattacker.attacks.padding;
 
+import de.rub.nds.modifiablevariable.bytearray.ByteArrayDeleteModification;
 import de.rub.nds.modifiablevariable.bytearray.ByteArrayExplicitValueModification;
 import de.rub.nds.modifiablevariable.bytearray.ByteArrayModificationFactory;
 import de.rub.nds.modifiablevariable.bytearray.ByteArrayXorModification;
 import de.rub.nds.modifiablevariable.util.ArrayConverter;
-import de.rub.nds.tlsattacker.attacks.padding.vector.ModifiedMacVector;
-import de.rub.nds.tlsattacker.attacks.padding.vector.ModifiedPaddingVector;
 import de.rub.nds.tlsattacker.attacks.padding.vector.PaddingVector;
 import de.rub.nds.tlsattacker.attacks.padding.vector.PlainPaddingVector;
+import de.rub.nds.tlsattacker.attacks.padding.vector.TrippleVector;
+import de.rub.nds.tlsattacker.core.constants.AlgorithmResolver;
 import de.rub.nds.tlsattacker.core.constants.CipherSuite;
 import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import java.util.LinkedList;
@@ -28,6 +29,16 @@ import java.util.List;
 public class VeryShortPaddingGenerator extends PaddingVectorGenerator {
 
     /**
+     * Default length of the encrypted data (app + mac + padding)
+     */
+    static final int DEFAULT_CIPHERTEXT_LENGTH = 64;
+    /**
+     * Default padding length for the construction of modified encrypted
+     * plaintexts
+     */
+    static final int DEFAULT_PADDING_LENGTH = 4;
+
+    /**
      *
      * @param suite
      * @param version
@@ -36,44 +47,72 @@ public class VeryShortPaddingGenerator extends PaddingVectorGenerator {
     @Override
     public List<PaddingVector> getVectors(CipherSuite suite, ProtocolVersion version) {
         List<PaddingVector> vectorList = new LinkedList<>();
-        vectorList.addAll(createVectorWithModifiedMac());
-        vectorList.addAll(createVectorWithModifiedPadding());
-        vectorList.addAll(createVectorWithPlainData());
+        vectorList.addAll(createOnlyPaddingVectors(suite, version));
+        vectorList.addAll(createClassicModifiedPadding(suite, version));
         return vectorList;
     }
 
-    private List<PaddingVector> createVectorWithPlainData() {
+    List<PaddingVector> createOnlyPaddingVectors(CipherSuite suite, ProtocolVersion version) {
         List<PaddingVector> vectorList = new LinkedList<>();
-        byte[] plain = createPaddingBytes(63);
-        vectorList.add(createVectorWithPlainData("Plain 3F", plain));
-        plain = new byte[] { (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
-                (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
-                (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
-                (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
-                (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
-                (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
-                (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
-                (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
-                (byte) 255, };
+        byte[] plain = createPaddingBytes(DEFAULT_CIPHERTEXT_LENGTH - 1);
         vectorList.add(createVectorWithPlainData("Plain FF", plain));
+        plain = new byte[]{(byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
+            (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
+            (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
+            (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
+            (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
+            (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
+            (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
+            (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255, (byte) 255,
+            (byte) 255,};
+        vectorList.add(createVectorWithPlainData("Plain 3F", plain));
         return vectorList;
+    }
+
+    List<PaddingVector> createClassicModifiedPadding(CipherSuite suite, ProtocolVersion version) {
+        int macSize = AlgorithmResolver.getMacAlgorithm(version, suite).getSize();
+        int paddingValue = DEFAULT_CIPHERTEXT_LENGTH - macSize - 1;
+        int applicationLength = 0;
+        List<PaddingVector> vectorList = createClassicModifiedPaddingWithValidMAC(applicationLength, paddingValue);
+        vectorList.addAll(createClassicModifiedPaddingWithInvalidMAC(applicationLength, paddingValue));
+
+        return vectorList;
+    }
+
+    private List<PaddingVector> createClassicModifiedPaddingWithValidMAC(int applicationLength, int paddingValue) {
+        List<PaddingVector> vectorList = new LinkedList<>();
+        // valid mac
+        byte[] padding = createPaddingBytes(paddingValue);
+        padding[0] ^= 0x80; // flip first padding byte highest bit
+        vectorList.add(new TrippleVector("InvPadValMac-[0]-" + applicationLength + "-" + paddingValue,
+                new ByteArrayExplicitValueModification(new byte[applicationLength]), null,
+                new ByteArrayExplicitValueModification(padding)));
+        return vectorList;
+    }
+
+    private List<PaddingVector> createClassicModifiedPaddingWithInvalidMAC(int applicationLength, int paddingValue) {
+        List<PaddingVector> vectorList = new LinkedList<>();
+        // invalid mac
+        byte[] padding = createPaddingBytes(paddingValue);
+        vectorList.add(new TrippleVector("ValPadInvMac-[0]-" + applicationLength + "-" + paddingValue,
+                new ByteArrayExplicitValueModification(new byte[applicationLength]), new ByteArrayXorModification(
+                        new byte[]{0x01}, 0), new ByteArrayExplicitValueModification(padding)));
+        return vectorList;
+    }
+
+    List<ByteArrayXorModification> createFlippedModifications(int byteLength) {
+        List<ByteArrayXorModification> modificationList = new LinkedList<>();
+        modificationList.add(new ByteArrayXorModification(new byte[]{0x01}, byteLength - 1)); // Last
+        // Byte / lowest bit
+        modificationList.add(new ByteArrayXorModification(new byte[]{0x08}, byteLength / 2)); // Some
+        // Byte / middle bit
+        modificationList.add(new ByteArrayXorModification(new byte[]{(byte) 0x80}, 0)); // first
+        // Byte / highest bit
+        return modificationList;
     }
 
     private PaddingVector createVectorWithPlainData(String name, byte[] plain) {
         return new PlainPaddingVector(name,
                 (ByteArrayExplicitValueModification) ByteArrayModificationFactory.explicitValue(plain));
-    }
-
-    private List<PaddingVector> createVectorWithModifiedPadding() {
-        List<PaddingVector> records = new LinkedList<>();
-        records.add(new ModifiedPaddingVector("ModifiedPadding", ByteArrayModificationFactory.xor(new byte[] { 1 }, 0)));
-        return records;
-    }
-
-    private List<PaddingVector> createVectorWithModifiedMac() {
-        List<PaddingVector> vectors = new LinkedList<>();
-        vectors.add(new ModifiedMacVector("ModifiedMac", (ByteArrayXorModification) ByteArrayModificationFactory.xor(
-                new byte[] { 1, 1, 1 }, 0)));
-        return vectors;
     }
 }
