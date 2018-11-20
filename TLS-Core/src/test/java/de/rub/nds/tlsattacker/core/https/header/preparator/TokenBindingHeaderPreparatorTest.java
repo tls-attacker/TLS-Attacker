d0bf1a0d70afe3927cf3bb122ea83764be0ad710
ic0ns <robert.merget@rub.de>
2018-11-20 14:42:19 +0100

    formatted

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/https/header/preparator/TokenBindingHeaderPreparatorTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/https/header/preparator/TokenBindingHeaderPreparatorTest.java
@@ -48,7 +48,8 @@ public class TokenBindingHeaderPreparatorTest {
         preparator.prepare();
 
         assertEquals(header.getHeaderName().getValue(), "Sec-Token-Binding");
-        assertEquals(header.getHeaderValue().getValue(),
+        assertEquals(
+                header.getHeaderValue().getValue(),
                 "AIkAAgBBQF7L5NGmMwpEyPfvlR1L8WXmxrch762phftBZhvG5_1shzRkDEmY_343SwbOGmSi7NgqsDY4T7g9mnmxJ6J9UDIAQBiMGdH7awDozrs8wPI2pfRAqtPX2vx3LTNCmY-9ngpdWHu9GFxflmo9jN0yKR1IxnVNtU-85XOUEwjlYaPYUJMAAA");
     }
 
