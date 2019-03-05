939146d1cd2ae845291531d05415103df043f81a
ic0ns <robert.merget@rub.de>
2019-03-05 23:31:40 +0000

    Updated padding oracle attacker rescans and added config options

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/PaddingOracleAttacker.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/PaddingOracleAttacker.java
@@ -52,9 +52,7 @@ public class PaddingOracleAttacker extends Attacker<PaddingOracleCommandConfig> 
 
     private long additionalTimeout = 1000;
 
-    private List<VectorResponse> vectorResponseList;
-    private List<VectorResponse> vectorResponseListTwo;
-    private List<VectorResponse> vectorResponseListThree;
+    private List<List<VectorResponse>> responseMapList;
 
     private CipherSuite testedSuite;
 
@@ -103,47 +101,56 @@ public class PaddingOracleAttacker extends Attacker<PaddingOracleCommandConfig> 
         groupRecords = false;
         CONSOLE.info("A server is considered vulnerable to this attack if it responds differently to the test vectors.");
         CONSOLE.info("A server is considered secure if it always responds the same way.");
-        EqualityError error;
-
+        EqualityError referenceError = null;
+        List<VectorResponse> referenceResponseMap = null;
+        List<List<VectorResponse>> responseMapList = new LinkedList<>();
         try {
-            vectorResponseList = createVectorResponseList();
-            error = getEqualityError(vectorResponseList);
-            if (error != EqualityError.NONE && error != null) {
-                CONSOLE.info("Found a side channel. Rescanning to confirm.");
-                vectorResponseListTwo = createVectorResponseList();
-                EqualityError errorTwo = getEqualityError(vectorResponseListTwo);
-                if (error == errorTwo && lookEqual(vectorResponseList, vectorResponseListTwo)) {
-                    vectorResponseListThree = createVectorResponseList();
-                    EqualityError errorThree = getEqualityError(vectorResponseListThree);
-                    if (error == errorThree && lookEqual(vectorResponseList, vectorResponseListThree)) {
-                        CONSOLE.info("Found an equality Error.");
-                        CONSOLE.info("The Server is very likely vulnerable");
-                    } else {
-                        LOGGER.error("2nd rescan false positive");
-                        CONSOLE.info("Rescan revealed a false positive");
-                        shakyScans = true;
-                        return false;
+            for (int i = 0; i < config.getMapListDepth(); i++) {
+
+                List<VectorResponse> responseMap = createVectorResponseList();
+                responseMapList.add(responseMap);
+                if (i == 0) {
+                    referenceResponseMap = responseMap;
+                    referenceError = getEqualityError(responseMap);
+                    if (referenceError == EqualityError.NONE && !config.isRescanNotVulnerable()) {
+                        LOGGER.debug("Server appears not vulnerable an RescanNotVulnerable is not active");
+                        break;
                     }
                 } else {
-                    LOGGER.error("Rescan false positive");
-                    CONSOLE.info("Rescan revealed a false positive");
-                    shakyScans = true;
-                    return false;
+                    EqualityError error = getEqualityError(responseMap);
+                    if (error == referenceError && lookEqual(referenceResponseMap, responseMap)) {
+                        CONSOLE.info("Rescan[" + i + "] shows same results");
+
+                    } else {
+                        shakyScans = true;
+                        CONSOLE.info("Rescan[" + i + "] shows different results");
+                        if (config.isAbortRescansOnFailure()) {
+                            CONSOLE.info("Abort Rescans on failure is active. Stopping.");
+                            return false;
+                        }
+
+                    }
                 }
             }
         } catch (AttackFailedException E) {
             CONSOLE.info(E.getMessage());
             return null;
         }
-        CONSOLE.info(EqualityErrorTranslator.translation(error, null, null));
-        if (error != EqualityError.NONE || LOGGER.getLevel().isMoreSpecificThan(Level.INFO)) {
+
+        CONSOLE.info(EqualityErrorTranslator.translation(referenceError, null, null));
+        if (referenceError != EqualityError.NONE
+                || LOGGER.getLevel()
+                        .isMoreSpecificThan(Level.INFO)) {
             LOGGER.debug("-------------(Not Grouped)-----------------");
-            for (VectorResponse vectorResponse : vectorResponseList) {
+            for (VectorResponse vectorResponse : referenceResponseMap) {
                 LOGGER.debug(vectorResponse.toString());
             }
         }
 
-        return error != EqualityError.NONE;
+        if (shakyScans) {
+            return null;
+        }
+        return referenceError != EqualityError.NONE;
     }
 
     /**
@@ -283,30 +290,8 @@ public class PaddingOracleAttacker extends Attacker<PaddingOracleCommandConfig> 
         return EqualityError.NONE;
     }
 
-    /**
-     *
-     * @return
-     */
-    public List<VectorResponse> getVectorResponseList() {
-        return vectorResponseList;
-    }
-
-    /**
-     * The responseVector list of the first rescan
-     *
-     * @return
-     */
-    public List<VectorResponse> getVectorResponseListTwo() {
-        return vectorResponseListTwo;
-    }
-
-    /**
-     * The responseVector list of the second rescan
-     *
-     * @return
-     */
-    public List<VectorResponse> getVectorResponseListThree() {
-        return vectorResponseListThree;
+    public List<List<VectorResponse>> getResponseMapList() {
+        return responseMapList;
     }
 
     /**
