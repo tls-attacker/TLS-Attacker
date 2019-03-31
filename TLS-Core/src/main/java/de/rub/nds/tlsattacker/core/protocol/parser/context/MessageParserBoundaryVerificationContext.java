798b15452326ebd1b96dfe392a1980ee022a09bb
mwhesse <mwhesse@gmail.com>
2019-03-31 18:16:30 +0700

    Adding ParserContext to verify double length parsing

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/parser/context/MessageParserBoundaryVerificationContext.java
@@ -0,0 +1,61 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.core.protocol.parser.context;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import de.rub.nds.tlsattacker.core.exceptions.ParserException;
+import de.rub.nds.tlsattacker.core.protocol.parser.Parser;
+
+public class MessageParserBoundaryVerificationContext implements ParserContext {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private final int messageBoundary;
+
+    private final String boundaryQualifier;
+
+    private final int pointerOffset;
+
+    public MessageParserBoundaryVerificationContext(int boundary, String boundaryQualifier, int pointerOffset) {
+        super();
+        this.messageBoundary = boundary;
+        this.boundaryQualifier = boundaryQualifier;
+        this.pointerOffset = pointerOffset;
+    }
+
+    @Override
+    public ParserContextResult beforeParse(final Parser p, final int requestedLength) {
+        int requestedBoundary = (p.getPointer() - pointerOffset) + requestedLength;
+        LOGGER.trace("verify requested boundary {} against boundary {} {}", requestedBoundary, boundaryQualifier,
+                messageBoundary);
+        if (requestedBoundary <= this.messageBoundary) {
+            return ParserContextResult.NULL_RESULT;
+        } else {
+            return new ParserContextResult() {
+                @Override
+                public void evaluate() {
+                    throw new ParserException(String.format("Attempt to parse over boundary %s of current context, "
+                            + "boundary only has %d bytes left, but parse request was for %d bytes in %s",
+                            boundaryQualifier,
+                            MessageParserBoundaryVerificationContext.this.messageBoundary
+                                    - (p.getPointer() - pointerOffset), requestedLength,
+                            MessageParserBoundaryVerificationContext.this));
+                }
+            };
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "MessageParserBoundaryContext [boundary=" + messageBoundary + ", boundaryQualifier=" + boundaryQualifier
+                + ", pointerOffset=" + pointerOffset + "]";
+    }
+}
---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/parser/context/ParserContext.java
@@ -0,0 +1,17 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.core.protocol.parser.context;
+
+import de.rub.nds.tlsattacker.core.protocol.parser.Parser;
+
+public interface ParserContext {
+
+    ParserContextResult beforeParse(Parser p, int length);
+
+}
