4a767becc0157e1c05f6efb8069a628a7a1bed3a
Pierre Tilhaus <pierre.tilhaus@rub.de>
2019-04-03 15:57:02 +0200

    Added check if BouncyCastleProvider was loaded in WorkflowExecutor Issue #355

---
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/workflow/BouncyCastleProviderCheckerTest.java
@@ -0,0 +1,37 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.core.workflow;
+
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pierre Tilhaus <Pierre.TIlhaus@rub.de>
+ */
+public class BouncyCastleProviderCheckerTest {
+    @Before
+    public void setUp() {
+        Security.removeProvider("BC");
+    }
+
+    /**
+     * Test of isLoaded method, of class BouncyCastleProviderChecker.
+     */
+    @Test
+    public void testIsLoaded() {
+        assertFalse(BouncyCastleProviderChecker.isLoaded());
+        Security.addProvider(new BouncyCastleProvider());
+        assertTrue(BouncyCastleProviderChecker.isLoaded());
+    }
+
+}
