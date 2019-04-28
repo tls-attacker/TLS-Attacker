a6192004e7353dfbe0726418406010ae00dbc721
mwhesse <mwhesse@gmail.com>
2019-04-28 09:23:51 +0700

    Adding schema generator for WorkflowTrace type (WIP)

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/workflow/package-info.java
@@ -0,0 +1,7 @@
+@javax.xml.bind.annotation.XmlSchema(
+		elementFormDefault=XmlNsForm.UNSET, 
+		attributeFormDefault=XmlNsForm.UNSET, 
+		namespace="http://nds.rub.de/tlsattacker")
+package de.rub.nds.tlsattacker.core.workflow;
+
+import javax.xml.bind.annotation.XmlNsForm;
