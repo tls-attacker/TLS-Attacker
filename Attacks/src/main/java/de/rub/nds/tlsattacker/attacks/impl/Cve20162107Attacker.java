0e910b19f4248ab4fd9069a83c1a0d89428296bb
Robert Merget <robert.merget@rub.de>
2019-05-19 18:04:19 +0200

    Deprecationcve (#564)
    * deprecated cve20162107

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/Cve20162107Attacker.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/Cve20162107Attacker.java
@@ -44,6 +44,11 @@ import org.apache.logging.log4j.Logger;
 /**
  * Tests for the availability of the OpenSSL padding oracle (CVE-2016-2107).
  */
+/**
+ * Deprecated 18.4.2019 - There is no need for this class. it is completly
+ * integrated into the padding oracle attacker
+ */
+@Deprecated
 public class Cve20162107Attacker extends Attacker<Cve20162107CommandConfig> {
 
     private static final Logger LOGGER = LogManager.getLogger();
