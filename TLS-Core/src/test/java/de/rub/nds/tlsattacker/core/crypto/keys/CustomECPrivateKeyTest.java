407272582c939a691e13029da8401565ca22bd3f
ic0ns <robert.merget@rub.de>
2019-06-28 11:05:03 +0200

    Added temporary exception for brainpool curves (since ecdsa is currently not supported with them)

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/crypto/keys/CustomECPrivateKeyTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/crypto/keys/CustomECPrivateKeyTest.java
@@ -10,9 +10,11 @@ package de.rub.nds.tlsattacker.core.crypto.keys;
 
 import de.rub.nds.tlsattacker.core.constants.NamedGroup;
 import java.math.BigInteger;
+import java.security.Security;
 import java.security.spec.ECParameterSpec;
 import java.util.ArrayList;
 import java.util.List;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Rule;
 import org.junit.Test;
@@ -24,12 +26,14 @@ import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class CustomECPrivateKeyTest {
+
     @Parameters
     public static Iterable<Object[]> createParameters() {
         List<Object[]> testValues = new ArrayList<>();
         for (NamedGroup curve : NamedGroup.getImplemented()) {
-            if (curve.isStandardCurve()) {
-                testValues.add(new Object[] { curve });
+            if (curve.isStandardCurve() && curve.name().contains("BRAIN")) {
+                //TODO do not skip brainpool once asn1 tool is integrated
+                testValues.add(new Object[]{curve});
             }
         }
         return testValues;
@@ -46,6 +50,7 @@ public class CustomECPrivateKeyTest {
      */
     @Test
     public void testGetParams() {
+        Security.addProvider(new BouncyCastleProvider());
         CustomECPrivateKey key = new CustomECPrivateKey(BigInteger.TEN, curve);
         ECParameterSpec params = key.getParams();
         assertNotNull(params);
