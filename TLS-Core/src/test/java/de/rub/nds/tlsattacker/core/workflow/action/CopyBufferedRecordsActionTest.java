51220e55180fd9a298ec2a83c16c811d7b45daae
TanerBoecek <taner.boecek@hotmail.de>
2019-10-02 13:17:41 +0200

    removed wrong tags

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/workflow/action/CopyBufferedRecordsActionTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/workflow/action/CopyBufferedRecordsActionTest.java
@@ -1,8 +1,8 @@
 /**
  * TLS-Attacker - A Modular Penetration Testing Framework for TLS
- * <p>
+ *
  * Copyright 2014-2017 Ruhr University Bochum / Hackmanit GmbH
- * <p>
+ * 
  * Licensed under Apache License 2.0
  * http://www.apache.org/licenses/LICENSE-2.0
  */
