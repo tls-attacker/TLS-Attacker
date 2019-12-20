e46c0ef983d1f20d689211cd6258f970b20d4ad4
Mario <mario.korth@rub.de>
2019-12-20 14:13:59 +0100

    CcaCertificateGenerator doc update, parsing of xml files started, missing adding to CertificateMessage

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/cca/CcaCertificateGenerator.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/cca/CcaCertificateGenerator.java
@@ -9,15 +9,22 @@
 package de.rub.nds.tlsattacker.attacks.cca;
 
 import de.rub.nds.asn1.Asn1Encodable;
+import de.rub.nds.asn1.encoder.Asn1EncoderForX509;
 import de.rub.nds.asn1.model.*;
 import de.rub.nds.asn1.parser.Asn1Parser;
 import de.rub.nds.asn1.parser.ParserException;
 import de.rub.nds.asn1.translator.ParseNativeTypesContext;
 import de.rub.nds.asn1tool.filesystem.TextFileReader;
+import de.rub.nds.asn1tool.xmlparser.Asn1XmlContent;
+import de.rub.nds.asn1tool.xmlparser.XmlParser;
 import de.rub.nds.modifiablevariable.util.Modifiable;
 import de.rub.nds.tlsattacker.core.config.delegate.CcaDelegate;
 import de.rub.nds.tlsattacker.core.protocol.message.CertificateMessage;
 import de.rub.nds.tlsattacker.core.protocol.message.cert.CertificatePair;
+import de.rub.nds.x509attacker.keyfilemanager.KeyFileManager;
+import de.rub.nds.x509attacker.keyfilemanager.KeyFileManagerException;
+import de.rub.nds.x509attacker.linker.Linker;
+import de.rub.nds.x509attacker.xmlsignatureengine.XmlSignatureEngine;
 
 import javax.security.auth.x500.X500Principal;
 import java.io.FileInputStream;
@@ -27,6 +34,7 @@ import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import static de.rub.nds.x509attacker.X509Attacker.*;
 
@@ -42,7 +50,7 @@ public class CcaCertificateGenerator {
      *         be implemented with x509 attacker.
      */
     public static CertificateMessage generateCertificate(CcaDelegate ccaDelegate, CcaCertificateType type)
-            throws CertificateException, IOException, ParserException {
+            throws CertificateException, IOException, ParserException, KeyFileManagerException {
         CertificateMessage certificateMessage = new CertificateMessage();
         if (type != null) {
             switch (type) {
@@ -76,7 +84,10 @@ public class CcaCertificateGenerator {
      * processed by X509Attacker with the sole exception of two placeholders which will be replaced. Those placeholders
      * are '<asn1Sequence identifier="issuer" type="Name" placeholder="replace_me"/>' and 'replace_me_im_a_dummy_key'.
      * The former is replaced with the subject of the root certificate used and the latter with the path to the key
-     * of the root certificate. Note that not the attribute is the placeholder but the whole string.
+     * of the root certificate. Note that not the attribute is the placeholder but the whole string. Last but not least
+     * certificates have to be created in a certain order in the XML file because the code uses the key corresponding
+     * to the last certificate for the connection, aka it's supposed to be the leaf certificate. All previous certificates
+     * should be should be in descending order, starting at the top with the highest level CA.
      *
      *
      * @param rootCertificate
@@ -94,12 +105,45 @@ public class CcaCertificateGenerator {
                                                                         String keyDirectory, String xmlDirectory,
                                                                         String certificateInputDirectory,
                                                                         String certificateOutputDirectory)
-            throws CertificateException, IOException, ParserException {
+            throws CertificateException, IOException, ParserException, KeyFileManagerException {
 
         String xmlSubject = extractXMLCertificateSubject(rootCertificate);
 
         TextFileReader textFileReader = new TextFileReader(certificateChain);
         String xmlString = textFileReader.read();
+
+        xmlString = xmlString.replace("<asn1Sequence identifier=\"issuer\" type=\"Name\" placeholder=\"replace_me\"/>", xmlSubject);
+        xmlString = xmlString.replace("replace_me_im_a_dummy_key", keyDirectory + rootCertificate);
+
+        // Parse XML
+        XmlParser xmlParser = new XmlParser(xmlString);
+        Asn1XmlContent asn1XmlContent = xmlParser.getAsn1XmlContent();
+        Map<String, Asn1Encodable> identifierMap = xmlParser.getIdentifierMap();
+
+        // Create links
+        Linker linker = new Linker(identifierMap);
+
+        // Load key files
+        KeyFileManager keyFileManager = KeyFileManager.getReference();
+        keyFileManager.init(keyDirectory);
+
+        // Create signatures
+        XmlSignatureEngine xmlSignatureEngine = new XmlSignatureEngine(linker, identifierMap);
+        xmlSignatureEngine.computeSignatures();
+
+        // Encode XML for certificate
+        List<Asn1Encodable> certificates = asn1XmlContent.getAsn1Encodables();
+        byte[][] encodedCertificates = new byte[certificates.size()][];
+        for (int i = 0; i < certificates.size(); i++) {
+            encodedCertificates[i] = Asn1EncoderForX509.encodeForCertificate(linker, certificates.get(i));
+        }
+
+        // Write certificate files such that test cases can be reproduced without TLS-Scanner with exactly the same certificates
+        writeCertificates(certificateOutputDirectory, certificates, encodedCertificates);
+
+        /**
+         * TODO: add certificates in order to CertificateMessage and set keyPair according to leaf certs key
+         */
 
         return new CertificateMessage();
     }
