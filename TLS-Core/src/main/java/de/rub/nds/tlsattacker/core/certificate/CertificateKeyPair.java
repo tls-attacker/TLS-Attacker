4ee3be2ad860edeb9052a9caf3aa68557c26164d
Mario Korth <mario.korth@rub.de>
2020-01-24 11:33:50 +0100

    Removed usage of deprecated API to obtain the name of the NamedGroup in an EC-Cert

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/certificate/CertificateKeyPair.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/certificate/CertificateKeyPair.java
@@ -48,6 +48,7 @@ import org.apache.logging.log4j.Logger;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x9.ECNamedCurveTable;
 import org.bouncycastle.crypto.tls.Certificate;
 import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 import org.bouncycastle.jcajce.provider.asymmetric.ecgost.BCECGOST3410PublicKey;
@@ -263,23 +264,9 @@ public class CertificateKeyPair implements Serializable {
         // the signature group is
         // the same as for the public key
         try {
-            X509CertificateObject obj = new X509CertificateObject(cert.getCertificateAt(0));
-            if (obj.getPublicKey() instanceof BCECGOST3410PublicKey) {
-                LOGGER.debug("SignatureKey is not part of a NamedGroup (GOST 2001)");
-                return null;
-            }
-            if (obj.getPublicKey() instanceof BCECGOST3410_2012PublicKey) {
-                LOGGER.debug("SignatureKey is not part of a NamedGroup (GOST 2012)");
-                return null;
-            }
-            BCECPublicKey ecKey = (BCECPublicKey) obj.getPublicKey();
-            ECNamedCurveSpec spec = (ECNamedCurveSpec) ecKey.getParams();
-            NamedGroup group = NamedGroup.fromJavaName(spec.getName());
-            if (group == null) {
-                return null;
-            } else {
-                return group;
-            }
+            ASN1ObjectIdentifier publicKeyParameters = (ASN1ObjectIdentifier) cert.getCertificateAt(0).
+                    getSubjectPublicKeyInfo().getAlgorithm().getParameters();
+            return NamedGroup.fromJavaName(ECNamedCurveTable.getName(publicKeyParameters));
         } catch (Exception ex) {
             LOGGER.warn("Could not determine EC public key group", ex);
             return null;
@@ -294,18 +281,9 @@ public class CertificateKeyPair implements Serializable {
             return null;
         }
         try {
-            X509CertificateObject obj = new X509CertificateObject(cert.getCertificateAt(0));
-            if (obj.getPublicKey() instanceof BCECGOST3410PublicKey) {
-                LOGGER.debug("PublicKey is not part of a NamedGroup (GOST 2001)");
-                return null;
-            }
-            if (obj.getPublicKey() instanceof BCECGOST3410_2012PublicKey) {
-                LOGGER.debug("PublicKey is not part of a NamedGroup (GOST 2012)");
-                return null;
-            }
-            BCECPublicKey ecKey = (BCECPublicKey) obj.getPublicKey();
-            ECNamedCurveSpec spec = (ECNamedCurveSpec) ecKey.getParams();
-            return NamedGroup.fromJavaName(spec.getName());
+            ASN1ObjectIdentifier publicKeyParameters = (ASN1ObjectIdentifier) cert.getCertificateAt(0).
+                    getSubjectPublicKeyInfo().getAlgorithm().getParameters();
+            return NamedGroup.fromJavaName(ECNamedCurveTable.getName(publicKeyParameters));
         } catch (Exception ex) {
             LOGGER.warn("Could not determine EC public key group", ex);
             return null;
