8c6db72043443f8875a0760c8b3674fd7a631360
Nimrod Aviram <nimrod.aviram@gmail.com>
2020-02-26 19:17:09 +0200

    First draft of Fisher Exact Test in Padding Oracle Attacker.

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/PaddingOracleAttacker.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/PaddingOracleAttacker.java
@@ -11,6 +11,7 @@ package de.rub.nds.tlsattacker.attacks.impl;
 import de.rub.nds.tlsattacker.attacks.config.PaddingOracleCommandConfig;
 import de.rub.nds.tlsattacker.attacks.exception.AttackFailedException;
 import de.rub.nds.tlsattacker.attacks.exception.OracleUnstableException;
+import de.rub.nds.tlsattacker.attacks.general.Vector;
 import de.rub.nds.tlsattacker.attacks.padding.PaddingTraceGenerator;
 import de.rub.nds.tlsattacker.attacks.padding.PaddingTraceGeneratorFactory;
 import de.rub.nds.tlsattacker.attacks.padding.PaddingVectorGenerator;
@@ -29,8 +30,14 @@ import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.workflow.ParallelExecutor;
 import de.rub.nds.tlsattacker.core.workflow.task.TlsTask;
 import static de.rub.nds.tlsattacker.util.ConsoleLogger.CONSOLE;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map.Entry;
+
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -245,6 +252,38 @@ public class PaddingOracleAttacker extends Attacker<PaddingOracleCommandConfig> 
             }
         }
         return EqualityError.NONE;
+    }
+    
+    private double getFisherTestPValue(List<VectorResponse> responseVectorList) {
+    	// TODO: Make sure Vector and ResponseFingerprint implement hashCode(). 
+    	HashSet<Vector> vectors = new HashSet<Vector>();
+    	HashSet<ResponseFingerprint> responseFingerprints = new HashSet<ResponseFingerprint>();
+		HashMap<Entry<Vector, ResponseFingerprint>, Integer> contingencyTable = new HashMap<Entry<Vector, ResponseFingerprint>, Integer>();
+    	for (VectorResponse vectorResponse : responseVectorList) {
+			Vector vector = vectorResponse.getVector();
+			ResponseFingerprint fingerprint = vectorResponse.getFingerprint();
+			vectors.add(vector);
+			responseFingerprints.add(fingerprint);
+			SimpleEntry<Vector,ResponseFingerprint> entry = new java.util.AbstractMap.SimpleEntry<>(vector, fingerprint);
+			contingencyTable.put(entry, 1 + contingencyTable.getOrDefault(entry, 0));
+		}
+    	if (vectors.size() != 2) {
+    		LOGGER.error("More than 2 vectors in Fisher test.");
+    		return 0;
+    	}
+    	if (responseFingerprints.size() != 2) {
+    		LOGGER.error("More than 2 responses in Fisher test.");
+    		return 0;
+    	}
+    	Vector vectorA = (Vector)vectors.toArray()[0];
+    	Vector vectorB = (Vector)vectors.toArray()[1];
+    	ResponseFingerprint response1 = (ResponseFingerprint) responseFingerprints.toArray()[0];
+    	ResponseFingerprint response2 = (ResponseFingerprint) responseFingerprints.toArray()[1];
+    	int inputAOutput1 = contingencyTable.get(new java.util.AbstractMap.SimpleEntry<>(vectorA, response1));
+    	int inputAOutput2 = contingencyTable.get(new java.util.AbstractMap.SimpleEntry<>(vectorA, response2));
+    	int inputBOutput1 = contingencyTable.get(new java.util.AbstractMap.SimpleEntry<>(vectorB, response1));
+    	int inputBOutput2 = contingencyTable.get(new java.util.AbstractMap.SimpleEntry<>(vectorB, response2));
+    	return FisherExactTest.getLog2PValue(inputAOutput1, inputBOutput1, inputAOutput2, inputBOutput2);
     }
 
     public EqualityError getResultError() {
