0c7edd36ec756624652f2ec2ea2929ba298b1153
Robert Merget <robert.merget@rub.de>
2020-03-04 11:20:49 +0100

    Removed statistic eval from attacks project and moved it to TLS-Scanner

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/PaddingOracleAttacker.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/impl/PaddingOracleAttacker.java
@@ -254,38 +254,6 @@ public class PaddingOracleAttacker extends Attacker<PaddingOracleCommandConfig> 
         return EqualityError.NONE;
     }
 
-    private double getFisherTestPValue(List<VectorResponse> responseVectorList) {
-        // TODO: Make sure Vector and ResponseFingerprint implement hashCode().
-        HashSet<Vector> vectors = new HashSet<Vector>();
-        HashSet<ResponseFingerprint> responseFingerprints = new HashSet<ResponseFingerprint>();
-        HashMap<Entry<Vector, ResponseFingerprint>, Integer> contingencyTable = new HashMap<Entry<Vector, ResponseFingerprint>, Integer>();
-        for (VectorResponse vectorResponse : responseVectorList) {
-            Vector vector = vectorResponse.getVector();
-            ResponseFingerprint fingerprint = vectorResponse.getFingerprint();
-            vectors.add(vector);
-            responseFingerprints.add(fingerprint);
-            SimpleEntry<Vector, ResponseFingerprint> entry = new java.util.AbstractMap.SimpleEntry<>(vector,
-                    fingerprint);
-            contingencyTable.put(entry, 1 + contingencyTable.getOrDefault(entry, 0));
-        }
-        if (vectors.size() != 2) {
-            LOGGER.error("More than 2 vectors in Fisher test.");
-            return 0;
-        }
-        if (responseFingerprints.size() != 2) {
-            LOGGER.error("More than 2 responses in Fisher test.");
-            return 0;
-        }
-        Vector vectorA = (Vector) vectors.toArray()[0];
-        Vector vectorB = (Vector) vectors.toArray()[1];
-        ResponseFingerprint response1 = (ResponseFingerprint) responseFingerprints.toArray()[0];
-        ResponseFingerprint response2 = (ResponseFingerprint) responseFingerprints.toArray()[1];
-        int inputAOutput1 = contingencyTable.get(new java.util.AbstractMap.SimpleEntry<>(vectorA, response1));
-        int inputAOutput2 = contingencyTable.get(new java.util.AbstractMap.SimpleEntry<>(vectorA, response2));
-        int inputBOutput1 = contingencyTable.get(new java.util.AbstractMap.SimpleEntry<>(vectorB, response1));
-        int inputBOutput2 = contingencyTable.get(new java.util.AbstractMap.SimpleEntry<>(vectorB, response2));
-        return FisherExactTest.getLog2PValue(inputAOutput1, inputBOutput1, inputAOutput2, inputBOutput2);
-    }
 
     public EqualityError getResultError() {
         return resultError;
