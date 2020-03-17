7cfd54791073d340a945618c18e9c4e2c0d337e1
ic0ns <robert.merget@rub.de>
2020-03-17 15:57:06 +0100

    Added reverse aead tests to also test the other key direction

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/record/cipher/RecordAEADCipherTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/record/cipher/RecordAEADCipherTest.java
@@ -49,7 +49,7 @@ public class RecordAEADCipherTest {
     }
 
     @Test
-    public void testEncryptTls12Server() throws NoSuchAlgorithmException, CryptoException {
+    public void testEncryptGcmServer() throws NoSuchAlgorithmException, CryptoException {
         context.setConnection(new OutboundConnection());
         context.setTalkingConnectionEndType(ConnectionEndType.SERVER);
         context.setSelectedCipherSuite(CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256);
@@ -114,7 +114,7 @@ public class RecordAEADCipherTest {
     }
 
     @Test
-    public void testDecryptTls12Server() throws NoSuchAlgorithmException, CryptoException {
+    public void testDecryptGcm12Server() throws NoSuchAlgorithmException, CryptoException {
         context.setConnection(new InboundConnection());
         context.setSelectedCipherSuite(CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256);
         context.setSelectedProtocolVersion(ProtocolVersion.TLS12);
@@ -127,6 +127,137 @@ public class RecordAEADCipherTest {
         keySet.setServerWriteIv(new byte[12]); // ServerSide is not used
         keySet.setServerWriteKey(new byte[16]); // ServerSide is not used
         keySet.setServerWriteMacSecret(new byte[0]); // ServerSide is not used
+        context.setRandom(new TestRandomData(ArrayConverter.hexStringToByteArray("FFEEDDCC"))); // ExplicitIV
+        byte[] data = ArrayConverter
+                .hexStringToByteArray("000000000000000077D85417660273BBA5F220778CC117ECB7AAC7F46B0E07A8679215363031E912DA4494F0E8BEA216DEA10FBB5AF87DF49E75EA206892A1A0");
+        cipher = new RecordAEADCipher(context, keySet);
+        Record record = new Record();
+        record.setContentType(ProtocolMessageType.HANDSHAKE.getValue());
+        record.prepareComputations();
+        record.setSequenceNumber(new BigInteger("0"));
+        record.setProtocolMessageBytes(data);
+        record.setProtocolVersion(ProtocolVersion.TLS12.getValue());
+        cipher.decrypt(record);
+
+        // These fields are not used within blockciphers
+        assertNull(record.getComputations().getCbcInitialisationVector());
+        assertNull(record.getComputations().getMacKey());
+        assertNull(record.getComputations().getMac());
+        assertNull(record.getComputations().getMacValid());
+        assertNull(record.getComputations().getPaddingValid());
+        assertNull(record.getComputations().getPadding());
+
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("0000000000000000"), record.getComputations()
+                .getExplicitNonce().getValue());
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("11223344556677889900AABB"), record.getComputations()
+                .getAeadSalt().getValue());
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("11223344556677889900AABB0000000000000000"), record
+                .getComputations().getGcmNonce().getValue());
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("DEA10FBB5AF87DF49E75EA206892A1A0"), record
+                .getComputations().getAuthenticationTag().getValue());
+        assertTrue(record.getComputations().getAuthenticationTagValid());
+
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("00000000000000001603030028"), record.getComputations()
+                .getAuthenticatedMetaData().getValue());
+        assertArrayEquals(
+                ArrayConverter
+                        .hexStringToByteArray("77D85417660273BBA5F220778CC117ECB7AAC7F46B0E07A8679215363031E912DA4494F0E8BEA216"),
+                record.getComputations().getAuthenticatedNonMetaData().getValue());
+
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("65B7DA726864D4184D75A549BF5C06AB20867846AF4434CC"),
+                record.getComputations().getCipherKey().getValue());
+        assertArrayEquals(
+                ArrayConverter
+                        .hexStringToByteArray("1400000CCE92FBEC9131F48A63FED31F71573F726479AA9108FB86A4FA16BC1D5CB5753003030303"),
+                record.getComputations().getPlainRecordBytes().getValue());
+        assertArrayEquals(
+                ArrayConverter
+                        .hexStringToByteArray("77D85417660273BBA5F220778CC117ECB7AAC7F46B0E07A8679215363031E912DA4494F0E8BEA216"),
+                record.getComputations().getCiphertext().getValue());
+        assertArrayEquals(
+                ArrayConverter
+                        .hexStringToByteArray("1400000CCE92FBEC9131F48A63FED31F71573F726479AA9108FB86A4FA16BC1D5CB5753003030303"),
+                record.getCleanProtocolMessageBytes().getValue());
+    }
+
+    @Test
+    public void testEncryptGcmClient() throws NoSuchAlgorithmException, CryptoException {
+        context.setConnection(new InboundConnection());
+        context.setSelectedCipherSuite(CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256);
+        context.setSelectedProtocolVersion(ProtocolVersion.TLS12);
+        KeySet keySet = new KeySet();
+        keySet.setServerWriteKey(ArrayConverter
+                .hexStringToByteArray("65B7DA726864D4184D75A549BF5C06AB20867846AF4434CC"));
+        keySet.setServerWriteMacSecret(new byte[0]);
+        keySet.setServerWriteIv(ArrayConverter.hexStringToByteArray("11223344556677889900AABB"));
+        // IV is not from keyblock
+        keySet.setClientWriteIv(new byte[12]); // ClientSide is not used
+        keySet.setClientWriteKey(new byte[16]); // ClientSide is not used
+        keySet.setClientWriteMacSecret(new byte[0]); // ClientSide is not used
+        context.setRandom(new TestRandomData(ArrayConverter.hexStringToByteArray("FFEEDDCC"))); // ExplicitIV
+        byte[] data = ArrayConverter
+                .hexStringToByteArray("1400000CCE92FBEC9131F48A63FED31F71573F726479AA9108FB86A4FA16BC1D5CB5753003030303");
+        cipher = new RecordAEADCipher(context, keySet);
+        Record record = new Record();
+        record.setContentType(ProtocolMessageType.HANDSHAKE.getValue());
+        record.prepareComputations();
+        record.setSequenceNumber(new BigInteger("0"));
+        record.setCleanProtocolMessageBytes(data);
+        record.setProtocolVersion(ProtocolVersion.TLS12.getValue());
+        cipher.encrypt(record);
+
+        // These fields are not used within blockciphers
+        assertNull(record.getComputations().getCbcInitialisationVector());
+        assertNull(record.getComputations().getMacKey());
+        assertNull(record.getComputations().getMac());
+        assertNull(record.getComputations().getMacValid());
+        assertNull(record.getComputations().getPaddingValid());
+        assertNull(record.getComputations().getPadding());
+
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("0000000000000000"), record.getComputations()
+                .getExplicitNonce().getValue());
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("11223344556677889900AABB"), record.getComputations()
+                .getAeadSalt().getValue());
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("11223344556677889900AABB0000000000000000"), record
+                .getComputations().getGcmNonce().getValue());
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("DEA10FBB5AF87DF49E75EA206892A1A0"), record
+                .getComputations().getAuthenticationTag().getValue());
+        assertTrue(record.getComputations().getAuthenticationTagValid());
+
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("00000000000000001603030028"), record.getComputations()
+                .getAuthenticatedMetaData().getValue());
+        assertArrayEquals(data, record.getComputations().getAuthenticatedNonMetaData().getValue());
+
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("65B7DA726864D4184D75A549BF5C06AB20867846AF4434CC"),
+                record.getComputations().getCipherKey().getValue());
+        assertArrayEquals(
+                ArrayConverter
+                        .hexStringToByteArray("1400000CCE92FBEC9131F48A63FED31F71573F726479AA9108FB86A4FA16BC1D5CB5753003030303"),
+                record.getComputations().getPlainRecordBytes().getValue());
+        assertArrayEquals(
+                ArrayConverter
+                        .hexStringToByteArray("77D85417660273BBA5F220778CC117ECB7AAC7F46B0E07A8679215363031E912DA4494F0E8BEA216"),
+                record.getComputations().getCiphertext().getValue());
+        assertArrayEquals(
+                ArrayConverter
+                        .hexStringToByteArray("000000000000000077D85417660273BBA5F220778CC117ECB7AAC7F46B0E07A8679215363031E912DA4494F0E8BEA216DEA10FBB5AF87DF49E75EA206892A1A0"),
+                record.getProtocolMessageBytes().getValue());
+    }
+
+    @Test
+    public void testDecryptGcm12Client() throws NoSuchAlgorithmException, CryptoException {
+        context.setConnection(new OutboundConnection());
+        context.setSelectedCipherSuite(CipherSuite.TLS_RSA_WITH_AES_128_GCM_SHA256);
+        context.setSelectedProtocolVersion(ProtocolVersion.TLS12);
+        KeySet keySet = new KeySet();
+        keySet.setServerWriteKey(ArrayConverter
+                .hexStringToByteArray("65B7DA726864D4184D75A549BF5C06AB20867846AF4434CC"));
+        keySet.setServerWriteMacSecret(new byte[0]);
+        keySet.setServerWriteIv(ArrayConverter.hexStringToByteArray("11223344556677889900AABB"));
+        // IV is not from KeyBlock
+        keySet.setClientWriteIv(new byte[12]); // ClientSide is not used
+        keySet.setClientWriteKey(new byte[16]); // ClientSide is not used
+        keySet.setClientWriteMacSecret(new byte[0]); // ClientSide is not used
         context.setRandom(new TestRandomData(ArrayConverter.hexStringToByteArray("FFEEDDCC"))); // ExplicitIV
         byte[] data = ArrayConverter
                 .hexStringToByteArray("000000000000000077D85417660273BBA5F220778CC117ECB7AAC7F46B0E07A8679215363031E912DA4494F0E8BEA216DEA10FBB5AF87DF49E75EA206892A1A0");
