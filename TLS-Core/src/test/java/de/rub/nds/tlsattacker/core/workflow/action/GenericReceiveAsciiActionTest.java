552fba5b6acfec9316d2e3be1cb7f96e41c7df8b
Nirusu <Nirusu@users.noreply.github.com>
2020-03-30 22:29:47 +0200

    Add test for GenericReceiveAsciiActionTest

---
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/workflow/action/GenericReceiveAsciiActionTest.java
@@ -0,0 +1,88 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2020 Ruhr University Bochum, Paderborn University,
+ * and Hackmanit GmbH
+ *
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package de.rub.nds.tlsattacker.core.workflow.action;
+
+import de.rub.nds.tlsattacker.core.exceptions.WorkflowExecutionException;
+import de.rub.nds.tlsattacker.core.state.State;
+import de.rub.nds.tlsattacker.core.state.TlsContext;
+import de.rub.nds.tlsattacker.core.unittest.helper.FakeTransportHandler;
+import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
+import de.rub.nds.tlsattacker.transport.ConnectionEndType;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GenericReceiveAsciiActionTest {
+
+    private State state;
+    private TlsContext tlsContext;
+
+    private GenericReceiveAsciiAction action;
+    private GenericReceiveAsciiAction actionException;
+
+    @Before
+    public void setUp() {
+        action = new GenericReceiveAsciiAction("US-ASCII");
+        actionException = new GenericReceiveAsciiAction("IOExceptionTest");
+        WorkflowTrace trace = new WorkflowTrace();
+        trace.addTlsAction(action);
+        state = new State(trace);
+
+        tlsContext = state.getTlsContext();
+        tlsContext.setTransportHandler(new FakeTransportHandler(ConnectionEndType.CLIENT));
+    }
+
+    /**
+     * Test of execute method, of class GenericReceiveAsciiAction.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testExecute() throws Exception {
+        ((FakeTransportHandler) tlsContext.getTransportHandler())
+                .setFetchableByte(new byte[] { 0x15, 0x03, 0x02, 0x01 });
+
+        action.execute(state);
+        assertTrue(action.isExecuted());
+
+        actionException.execute(state);
+        assertFalse(actionException.isExecuted());
+    }
+
+    /**
+     * Test of WorkflowExecutionException of execute method, of class
+     * GenericReceiveAsciiAction.
+     */
+    @Test(expected = WorkflowExecutionException.class)
+    public void testExecuteWorkflowExecutionException() {
+        action.execute(state);
+        action.execute(state);
+    }
+
+    /**
+     * Test of reset method, of class GenericReceiveAsciiAction.
+     */
+    @Test
+    public void testReset() {
+        action.reset();
+        assertFalse(action.isExecuted());
+    }
+
+    /**
+     * Test of executedAsPlanned method, of class GenericReceiveAsciiAction.
+     */
+    @Test
+    public void testExecutedAsPlanned() {
+        assertFalse(action.executedAsPlanned());
+        action.execute(state);
+        assertTrue(action.executedAsPlanned());
+    }
+}
