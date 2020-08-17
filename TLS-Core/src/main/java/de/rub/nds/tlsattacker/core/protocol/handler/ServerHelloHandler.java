3605b7e6fd4dc98055fc6a0700d37674acd0a713
ic0ns <robert.merget@rub.de>
2020-08-17 17:16:09 +0200

    server hello handler now activates recordlayer independent of backwards compatiblity

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/handler/ServerHelloHandler.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/handler/ServerHelloHandler.java
@@ -198,9 +198,7 @@ public class ServerHelloHandler extends HandshakeMessageHandler<ServerHelloMessa
     @Override
     public void adjustTlsContextAfterSerialize(ServerHelloMessage message) {
         if (tlsContext.getChooser().getSelectedProtocolVersion().isTLS13()) {
-            if (!tlsContext.getConfig().getTls13BackwardsCompatibilityMode()) {
-                setServerRecordCipher();
-            }
+            setServerRecordCipher();
         }
     }
 
