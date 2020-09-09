5f0338ee3f9e9dc034ab793056997935d1d9ac36
ic0ns <robert.merget@rub.de>
2020-09-09 10:41:46 +0200

    fixed and updated unit test

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/handler/DHClientKeyExchangeHandlerTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/protocol/handler/DHClientKeyExchangeHandlerTest.java
@@ -17,6 +17,7 @@ import de.rub.nds.tlsattacker.core.protocol.preparator.DHClientKeyExchangePrepar
 import de.rub.nds.tlsattacker.core.protocol.serializer.DHClientKeyExchangeSerializer;
 import de.rub.nds.tlsattacker.core.record.layer.TlsRecordLayer;
 import de.rub.nds.tlsattacker.core.state.TlsContext;
+import java.math.BigInteger;
 import org.junit.After;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -68,6 +69,7 @@ public class DHClientKeyExchangeHandlerTest {
     @Test
     public void testAdjustTLSContext() {
         DHClientKeyExchangeMessage message = new DHClientKeyExchangeMessage();
+        message.setPublicKey(new byte[] { 1 });
         message.prepareComputations();
         message.getComputations()
                 .setPremasterSecret(
@@ -89,5 +91,6 @@ public class DHClientKeyExchangeHandlerTest {
                 ArrayConverter
                         .hexStringToByteArray("4a0a7f6a0598acb36684359e1a19d848ab03b3ba1167430471166d94dcf8315d1c4290c9d9e40c50ae834df7b4f4bdef"),
                 context.getMasterSecret());
+        assertTrue(context.getClientDhPublicKey().equals(BigInteger.ONE));
     }
 }
