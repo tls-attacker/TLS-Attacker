a895cfb297b30a446e88a693b36d7bca63821891
mmaehren <32199075+mmaehren@users.noreply.github.com>
2020-09-15 08:31:36 +0200

    added second set of points

--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/ec/InvalidCurvePoint.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/ec/InvalidCurvePoint.java
@@ -20,35 +20,75 @@ import java.math.BigInteger;
 public enum InvalidCurvePoint {
 
     SECP160K1(new BigInteger("6F6118AE7199611C0B4F95CFE3B7DEDA68301E69", 16), new BigInteger(
-            "F6F9D0E04364C716C25263D7E44CA6C571D22597", 16), new BigInteger("5"), NamedGroup.SECP160K1),
+            "F6F9D0E04364C716C25263D7E44CA6C571D22597", 16), new BigInteger("5"), NamedGroup.SECP160K1, new BigInteger(
+            "1EB1255E9C4C24DDEDDE4E0F7756123D7A823027", 16), new BigInteger("3D7F3F573E9BA07A843B3ADE7A12C88AD5DCC9FC",
+            16), new BigInteger("7")),
     SECP160R1(new BigInteger("D465C0476AE02C499B0561B9C752C5CFEE8501ED", 16), new BigInteger(
-            "5B6394C2C94D9214417E722792D0C07617CC31A6", 16), new BigInteger("5"), NamedGroup.SECP160R1),
+            "5B6394C2C94D9214417E722792D0C07617CC31A6", 16), new BigInteger("5"), NamedGroup.SECP160R1, new BigInteger(
+            "B65C8C2E4DAC05379029F9A01C110E7F470438EB", 16), new BigInteger("4DCECC50B0A5F478DEA08B9C95ED1B8D9CCA85D4",
+            16), new BigInteger("7")),
     SECP160R2(new BigInteger("2790AABFE83C792584D45D5259ECCA28843D56AA", 16), new BigInteger(
-            "5DE5B6B1EC7BDA3940ABA6AD9AE01008040D5949", 16), new BigInteger("5"), NamedGroup.SECP160R2),
-    SECP192K1(new BigInteger("7E89D82546F6EDC79CB91F2646E8D7E7AB3FC2F971F1713C", 16), new BigInteger(
-            "8A62DA9766C50A90A776C599C421632B46CA9878AB55AF19", 16), new BigInteger("7"), NamedGroup.SECP192K1),
-    SECP192R1(new BigInteger("F6DA5E72B35D837EDCDD6E8D211BDBB6565B9708D0447400", 16), new BigInteger(
-            "ED15E29256077E3D25C26753FEE705C02FFC0DC8EFDA443A", 16), new BigInteger("5"), NamedGroup.SECP192R1),
-    SECP224K1(new BigInteger("54510A6A85EF6144CA057E159DD83C240E3A69B06EE2CAC06BD25AC7", 16), new BigInteger(
-            "D2799F20E14C33AB704203F75EBDB38471919531970090DE8D12BC95", 16), new BigInteger("7"), NamedGroup.SECP224K1),
-    SECP224R1(new BigInteger("A02F6D2FEBD6C53F11737C43EDDAF9A5026A21245DACA9342CFF7247", 16), new BigInteger(
-            "3B0781466C19DCCCAD13A2591A4DFAB7DADF210E9A150CE0C00137D9", 16), new BigInteger("5"), NamedGroup.SECP224R1),
+            "5DE5B6B1EC7BDA3940ABA6AD9AE01008040D5949", 16), new BigInteger("5"), NamedGroup.SECP160R2, new BigInteger(
+            "1263F47E04F675EF943F25EC205BBF1E96ABBCC7", 16), new BigInteger("C0363F5C199CC391A8CA102AA46736A99C59C4FE",
+            16), new BigInteger("7")),
+    SECP192K1(
+            new BigInteger("7E89D82546F6EDC79CB91F2646E8D7E7AB3FC2F971F1713C", 16),
+            new BigInteger("8A62DA9766C50A90A776C599C421632B46CA9878AB55AF19", 16),
+            new BigInteger("7"),
+            NamedGroup.SECP192K1,
+            new BigInteger("AE33F2175E2DC0D0F1F553164E4DAAB998AF0BD83EE09438", 16),
+            new BigInteger("B52C65D5C198450DB0B2FD239DE58E34BCD3F304936697A5", 16),
+            new BigInteger("13")),
+    SECP192R1(
+            new BigInteger("F6DA5E72B35D837EDCDD6E8D211BDBB6565B9708D0447400", 16),
+            new BigInteger("ED15E29256077E3D25C26753FEE705C02FFC0DC8EFDA443A", 16),
+            new BigInteger("5"),
+            NamedGroup.SECP192R1,
+            new BigInteger("83121F7CA2E4E6B248A868F240B632AC6E9601936CBA0D5D", 16),
+            new BigInteger("D15CA4A5728C75A72C152A3927474CE91A9216F548276EFD", 16),
+            new BigInteger("7")),
+    SECP224K1(
+            new BigInteger("54510A6A85EF6144CA057E159DD83C240E3A69B06EE2CAC06BD25AC7", 16),
+            new BigInteger("D2799F20E14C33AB704203F75EBDB38471919531970090DE8D12BC95", 16),
+            new BigInteger("7"),
+            NamedGroup.SECP224K1,
+            new BigInteger("674858B9E39B1E4359DDD8AD714120C984888ED8073BF8B650AF7B96", 16),
+            new BigInteger("1902C36C50FFE6BA2DEB89C7649B8A48B80AF4DDDCC849D8EBE91A9C", 16),
+            new BigInteger("31")),
+    SECP224R1(
+            new BigInteger("A02F6D2FEBD6C53F11737C43EDDAF9A5026A21245DACA9342CFF7247", 16),
+            new BigInteger("3B0781466C19DCCCAD13A2591A4DFAB7DADF210E9A150CE0C00137D9", 16),
+            new BigInteger("5"),
+            NamedGroup.SECP224R1,
+            new BigInteger("E04C41F0E37355144656277C17E9439D779DBE28BA48F2E61A75EEFF", 16),
+            new BigInteger("D5AAA43E42A0B640CF27C4AFF7C8508CD017207338B164F074BF0143", 16),
+            new BigInteger("7")),
     SECP256K1(
             new BigInteger("5748979A06D28004D165F01FCA69C80DECAFB0119BA2A7C4C7F84C7AF2DCA311", 16),
             new BigInteger("D9625DF3DC92015DEB22AC7242ABEBE512B195E973BA657203F1BDEE8662B45A", 16),
             new BigInteger("7"),
-            NamedGroup.SECP256K1),
+            NamedGroup.SECP256K1,
+            new BigInteger("1736E34CAA747C1B1EC4160765E9C0D50882138D13E13E5F2E34B24778433548", 16),
+            new BigInteger("24A1D38F809C23EBC750A68D7EC3727AF19249E5A4F4ABAA14CD160C739B8C49", 16),
+            new BigInteger("13")),
     SECP256R1(
             new BigInteger("21D2EFDDCFDF5C96268A16A8D5B8CB49EAD2DDE206259FE98686188A30CF0339", 16),
             new BigInteger("D440D09110D30D6CC3CDBBC38284109DB3ACA31F3C6717E29F1CE9D4088D4B1C", 16),
             new BigInteger("5"),
-            NamedGroup.SECP256R1),
+            NamedGroup.SECP256R1,
+            new BigInteger("54A3B03165F9DF43B0D54251BE429D09AC599686639CC567D790736BC8DE7308", 16),
+            new BigInteger("BBCC0B64E854B52E49D819BB03E4472ACBA7BFC9F090A8BF602F6ACD4719060D", 16),
+            new BigInteger("7")),
     SECP384R1(
             new BigInteger(
                     "B68083A3FE4F9E46B78D7EDA7DD98FBB712EF7C9899F728D9633A3688B6DE446366668EA1E6CF80996B046719DAD63FF",
                     16), new BigInteger(
                     "FC00B0AFDC553D8A01336C78527231BF2D7C8BAD862225A07761BD0975E968E72204EBF877D9F67A22883512884BA870",
-                    16), new BigInteger("5"), NamedGroup.SECP384R1),
+                    16), new BigInteger("5"), NamedGroup.SECP384R1, new BigInteger(
+                    "78C45BF6A62393F086C74C9EE999911A1149F8328E9B8D385B2AED93DFD65D72962A745D4A2A24817E4EABB0666639B0",
+                    16), new BigInteger(
+                    "BF807F465329EF7753C64E4834C4EF68F1732776DB1FAEC725D0172F20A4988D90FE728FE16C0A54F621F378AFF3001D",
+                    16), new BigInteger("7")),
     SECP521R1(
             new BigInteger(
                     "E04ED20B3289E72B4916D3C9095785488D309571BA7E39E0033DB72B471976133EE387F812A0DC2DE796A2C65ACCC220C2E11805FCADAF7F2D29826DF83C0B487F",
@@ -57,18 +97,32 @@ public enum InvalidCurvePoint {
                     "7555B523F2A83D26CF76E8BF6F3BD55A6BD7307D617D10F7228ED84920C2832F5AB78472FB1E54E572703E70FB84F4F956F2AA2027F0156DDE1CCE729BA135B02C",
                     16),
             new BigInteger("5"),
-            NamedGroup.SECP521R1),
+            NamedGroup.SECP521R1,
+            new BigInteger(
+                    "9470BBD33D650A1CFE779BE2B55E2312C0972129267B843387B09426990FC9CFE06527DE124F07E65652F16E76D61079A166FE9B9F435B05FE89C56ECD425CB851",
+                    16),
+            new BigInteger(
+                    "CEB883A8917D368E776877BCBAE2B2B57084631CA0FC406B3F147E9F2F2843D5C26C7885F2C38E47D3549C7E3E5D49DBC410D6484D7DA1792AFCB940825048DC6C",
+                    16),
+            new BigInteger("7")),
     BRAINPOOLP256R1(
             new BigInteger("475638180469F3128FCEACFF3D1B2A7052021FABE168456E724C82CE647A0B38", 16),
             new BigInteger("24392E4B249529608415683ABF8DF8017A577A447B791233BFF1F8D50003C3DA", 16),
             new BigInteger("5"),
-            NamedGroup.BRAINPOOLP256R1),
+            NamedGroup.BRAINPOOLP256R1,
+            new BigInteger("7D26DAD103711071F75690F00EBD8C5D8CA2F0DD89FD7FB5938A9CA557B10022", 16),
+            new BigInteger("21785CE72944C16797CD1014CBE23E5ED022206A679FCAA5832723C984124B4C", 16),
+            new BigInteger("7")),
     BRAINPOOLP384R1(
             new BigInteger(
                     "7A15487AF637530E2BECC85585C2E36C21447AB4C786F08EF75A1EFBE7785016855AB3B6EFBB9F80517C23C1438A3F18",
                     16), new BigInteger(
                     "1C8AC00FBE2E3CD0994704AC81F8210A283F34D4F351F19525876A14719B8DDAC45315782BB7BBEAB47B0B6061788A9D",
-                    16), new BigInteger("5"), NamedGroup.BRAINPOOLP384R1),
+                    16), new BigInteger("5"), NamedGroup.BRAINPOOLP384R1, new BigInteger(
+                    "AA5BC981A7C36628E472AD4BA458CDC9BF895A0ED26399A17485A40B62376B0C1EA274F1296D308AD87BB77D76C83B6",
+                    16), new BigInteger(
+                    "60D9486AD043F67CA934338328C1C1EBA8600C587E9E435ABEA305D7B5954DE506B26708D6F1026F06A79CC7B3AD044F",
+                    16), new BigInteger("7")),
     BRAINPOOLP512R1(
             new BigInteger(
                     "3A52E57C2D5BE39BB3F97C4CF90D81BEE7123CACBC6B7FF6EB03A164CCF0253FDF1AACF7C4AC6B820E6D48145D7854C67DEF4CADAB555D4609E279956450A610",
@@ -77,19 +131,33 @@ public enum InvalidCurvePoint {
                     "1C41E102D5E9EF09CA132E808D87D1C0944951572E82C4F9FECACC80714C0C926E5DA09BD775F5C7E2BE54878EE2AC1A091A8653AE9961789202FD2BA21E7999",
                     16),
             new BigInteger("5"),
-            NamedGroup.BRAINPOOLP512R1);
+            NamedGroup.BRAINPOOLP512R1,
+            new BigInteger(
+                    "1ADF96CEF08D1587B145CEA629E7452240B5851C9DB46406FC75FA550BF38687D33AA0081490A8FAB73C59448CBF50A5623FA7C641A5D8A5FE372BA3CFE16133",
+                    16),
+            new BigInteger(
+                    "2673C1B41A4A42697C771192DF770B787E1882F9AF6A965FAAF9A4262AD29D95BB9F6C87DF01AA29BC7646B901DF390317A97513728F5DA37E99A1C6A7575082",
+                    16),
+            new BigInteger("7"));
 
     private BigInteger publicPointBaseX;
     private BigInteger publicPointBaseY;
     private final NamedGroup namedGroup;
     private BigInteger order;
 
+    private BigInteger redundantBaseX;
+    private BigInteger redundantBaseY;
+    private BigInteger redundantOrder;
+
     private InvalidCurvePoint(BigInteger publicPointBaseX, BigInteger publicPointBaseY, BigInteger order,
-            NamedGroup namedGroup) {
+            NamedGroup namedGroup, BigInteger redundantBaseX, BigInteger redundantBaseY, BigInteger redundantOrder) {
         this.publicPointBaseX = publicPointBaseX;
         this.publicPointBaseY = publicPointBaseY;
         this.order = order;
         this.namedGroup = namedGroup;
+        this.redundantBaseX = redundantBaseX;
+        this.redundantBaseY = redundantBaseY;
+        this.redundantOrder = redundantOrder;
     }
 
     public static InvalidCurvePoint fromNamedGroup(NamedGroup group) {
@@ -145,6 +213,30 @@ public enum InvalidCurvePoint {
      */
     public void setOrder(BigInteger order) {
         this.order = order;
+    }
+
+    public BigInteger getRedundantBaseX() {
+        return redundantBaseX;
+    }
+
+    public void setRedundantBaseX(BigInteger redundantBaseX) {
+        this.redundantBaseX = redundantBaseX;
+    }
+
+    public BigInteger getRedundantBaseY() {
+        return redundantBaseY;
+    }
+
+    public void setRedundantBaseY(BigInteger redundantBaseY) {
+        this.redundantBaseY = redundantBaseY;
+    }
+
+    public BigInteger getRedundantOrder() {
+        return redundantOrder;
+    }
+
+    public void setRedundantOrder(BigInteger redundantOrder) {
+        this.redundantOrder = redundantOrder;
     }
 
 }
--- Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/ec/TwistedCurvePoint.java
+++ Attacks/src/main/java/de/rub/nds/tlsattacker/attacks/ec/TwistedCurvePoint.java
@@ -20,52 +20,84 @@ import java.math.BigInteger;
 public enum TwistedCurvePoint {
     SECP160K1Twist(new BigInteger("CAA163F84C55E308840926EB7347951F5BBB937B", 16), new BigInteger(
             "804BC5BCB00D3C0A47571FFCC9275755F506680C", 16), new BigInteger("7"), NamedGroup.SECP160K1, new BigInteger(
-            "C59136F5F837CEEE4C4071B911125BF127E89260", 16)),
+            "C59136F5F837CEEE4C4071B911125BF127E89260", 16), new BigInteger("9C84267D3E6407C5964E752D0CE7A6B7DD9AE110",
+            16), new BigInteger("E5A411F8B958C8CF70A312E4C01F2C0962F88290", 16), new BigInteger("13"), new BigInteger(
+            "BF9082E7409B2DDAA99B8BC0618282890C4121AC", 16)),
     SECP160R1Twist(
             new BigInteger("3791A82ED128406D89E44E508CC98BCB60D09E67", 16),
             new BigInteger("B8F5B2AD14E4498F71BBDF7505E21A0C3257FB68", 16),
             new BigInteger("523"),
             NamedGroup.SECP160R1,
-            new BigInteger("E708B3C59377C001AAA87F4743B64830AC27891B", 16)),
+            new BigInteger("E708B3C59377C001AAA87F4743B64830AC27891B", 16),
+            null,
+            null,
+            null,
+            null),
     SECP160R2Twist(new BigInteger("B1A722C8E8C916E4B63562C0429B36491187756", 16), new BigInteger(
             "E9DDA1D11EB136D574EAAFB70281E902E696F0", 16), new BigInteger("163"), NamedGroup.SECP160R2, new BigInteger(
-            "3444EB0B52787F04B7807B26D57249F0FBBF597", 16)),
+            "3444EB0B52787F04B7807B26D57249F0FBBF597", 16), new BigInteger("4E5E029E4E468289B5DA29B3EE8600697393C7A6",
+            16), new BigInteger("BAD18050A6A226794C51AD943D15D3721F633CF8", 16), new BigInteger("457"), new BigInteger(
+            "B5D70A87E90DB538403EB2953D4EECB202FE8361", 16)),
     SECP192K1Twist(
             new BigInteger("68EEA3E7F1C1504377C695B4F10F214CC71DB992366CFFAF", 16),
             new BigInteger("95DDFAAD265EC4B383C1F0679EE2EF94282FBA1570190F0B", 16),
             new BigInteger("373"),
             NamedGroup.SECP192K1,
-            new BigInteger("884067DD2B6A474C4F8138A9C735B567C3F624FBB5A89253", 16)),
+            new BigInteger("884067DD2B6A474C4F8138A9C735B567C3F624FBB5A89253", 16),
+            new BigInteger("90EA09CF46028698AD6D705CB550F505E2DB1F9E6533D07", 16),
+            new BigInteger("8E98BD9BCA2CDEC91022FED09C66D8C38FD9460D3BE2ABE9", 16),
+            new BigInteger("56383"),
+            new BigInteger("9DAF16AF0679BAE9E4726AC603BBD98708CF0A088ED98975", 16)),
     SECP192R1Twist(
             new BigInteger("C62B835DE26E223FB2CEE974624C14C1320A462FCF75156", 16),
             new BigInteger("BD031D48BB024F6942BAED45F9B140637A708F59AEAE09E7", 16),
             new BigInteger("23"),
             NamedGroup.SECP192R1,
-            new BigInteger("3DBF0D90776B937B86D57A65496AD0DD9F49D1D35CD489C9", 16)),
+            new BigInteger("3DBF0D90776B937B86D57A65496AD0DD9F49D1D35CD489C9", 16),
+            null,
+            null,
+            null,
+            null),
     SECP224K1Twist(
             new BigInteger("332C4168D13C82495CD5216012EAF9B2A6FF73EB80DBE470BAB28D32", 16),
             new BigInteger("21A117E36A4D8C047BAD31C5EDF8B4BF8308968DFFAF2425C33B5AE2", 16),
             new BigInteger("2161"),
             NamedGroup.SECP224K1,
-            new BigInteger("73DDCEEA75D52BA94BB278BF4D339D40F467F6451EF26756A51E1F3", 16)),
+            new BigInteger("73DDCEEA75D52BA94BB278BF4D339D40F467F6451EF26756A51E1F3", 16),
+            null,
+            null,
+            null,
+            null),
     SECP224R1Twist(
             new BigInteger("CAE5F9CCFA939BA4DA1B171660E4E6225AFECBC54CB5A07670EF4FB7", 16),
             new BigInteger("AEB87151E7A3370301B7C984E52DFDB45AE6CF800143A50C4F2750EE", 16),
             new BigInteger("11"),
             NamedGroup.SECP224R1,
-            new BigInteger("F517A55DD490FCD53A83392176BB9B1C6DD4E2F5EFDEE2F6454367BB", 16)),
+            new BigInteger("F517A55DD490FCD53A83392176BB9B1C6DD4E2F5EFDEE2F6454367BB", 16),
+            new BigInteger("2D60A182BF5899ACA2029D9D7802FF7EBF649F613B5C25A2E9A95DED", 16),
+            new BigInteger("900705C3013EA167CA394235736A35548221A9B513D4A5B18239810", 16),
+            new BigInteger("47"),
+            new BigInteger("DAA5F7E442CCC9D25D71DF9658DEF4454E9C0441CED7C0A6741754AA", 16)),
     SECP256K1Twist(
             new BigInteger("4B11C45CC1BB2C2F82DB5D12C7814ABD58C342FCBDA0040E9303A3A65B6DBA66", 16),
             new BigInteger("8FFE225FAD43C14B63ABC2CD14A20EC87AC83CA3E1DFD7FAD1FB92F7BACFD544", 16),
             new BigInteger("13"),
             NamedGroup.SECP256K1,
-            new BigInteger("7DC1351D8B1CB791B70411399271F823ED3AB9F54E52F591A5D4273D9F209570", 16)),
+            new BigInteger("7DC1351D8B1CB791B70411399271F823ED3AB9F54E52F591A5D4273D9F209570", 16),
+            new BigInteger("987BCDE3B17B59C4AB69517496E84BE739E8D3775B55B73EA5BDB24B557F7767", 16),
+            new BigInteger("AB935FB858895479C8DA27F04CCAF1FE051CE50BEE2E25234C2392C6A5EBA876", 16),
+            new BigInteger("3319"),
+            new BigInteger("472E817D3C6B1F0A865FAFAD839760BBC9AAE80171D243A161797C7FD9061337", 16)),
     SECP256R1Twist(
             new BigInteger("18F9BAE7747CD844E98525B7CCD0DAF6E1D20A818B2175A9A91E4EAE5343BC98", 16),
             new BigInteger("6212FB55CD57E1843CCBD1990DDA297E1C97DF1AED8B0DEE84F0EE33B5766859", 16),
             new BigInteger("5"),
             NamedGroup.SECP256R1,
-            new BigInteger("B609C031AA531AA580CB2239D8DC7968F7F91391D780DBCBCF753FAF716E196E", 16)),
+            new BigInteger("B609C031AA531AA580CB2239D8DC7968F7F91391D780DBCBCF753FAF716E196E", 16),
+            new BigInteger("32238D77F2AC3D7FDFE418D5BB0855D3E303C80D0E9BBA9DA5040A7ED4EBAB3C", 16),
+            new BigInteger("999D20A99AD3860FA6E72E5C8756709FEA0ABE398A2EEF7BF4941FAF576C96A3", 16),
+            new BigInteger("13"),
+            new BigInteger("D6904D6456EBE22335FDCC7B0E414E0417D5DEC00419BD0F02280855DF2ED671", 16)),
     // SECP384R1 twist is not feasible for large-scale scanning
     SECP521R1Twist(
             new BigInteger(
@@ -78,13 +110,27 @@ public enum TwistedCurvePoint {
             NamedGroup.SECP521R1,
             new BigInteger(
                     "191E30E8841160C9FFC64D162A21DCA0B0620A8DB76AD93D2047BB3E2251379C447DFC7F4D715DBE3D04BB051013CA8F0AF79BB45B27BAAFC5AF287A54FE462C1EE",
+                    16),
+            new BigInteger(
+                    "47B9CF28E04B38796858545D60D6133FBDC20EDE086E5D95111C982B8C276628235E536C075637A97C0A6C30D02B83B19E578203473EEA16DFDEAECCB1DC0D9B19",
+                    16),
+            new BigInteger(
+                    "93EF92BF8A66F492E0CD82071B8350299D541B82FF1A202DE9F974F0B720A5B4EC6EB84D2ACE0CD82BE837EC336957E18529E210A8F6E8704CC2F2F279CE93BA5F",
+                    16),
+            new BigInteger("7"),
+            new BigInteger(
+                    "17D3A076F32D076ACB269EDD813B1ADF86CD716FD9460C0A00DD8A0602F3AF4EF550AA6D91ECD2D5BCF41F6C17393D0EC4E9C9158FC9BD713E391FDFBB7C2C8D12F",
                     16)),
     BRAINPOOLP256R1Twist(
             new BigInteger("A8944F96DE0FE0D82489CBC7E71F2F529CFCFEA03CA593D91462278731E19A5", 16),
             new BigInteger("17BDECE85FF8A6475A9B3D23867F8E0D1860E7F02B7BE21A02EA4E715E685B6", 16),
             new BigInteger("5"),
             NamedGroup.BRAINPOOLP256R1,
-            new BigInteger("39248080291B8C5F9CE754E6045DA628B0B795AB3396C637844E48C4BC40FE54", 16)),
+            new BigInteger("39248080291B8C5F9CE754E6045DA628B0B795AB3396C637844E48C4BC40FE54", 16),
+            new BigInteger("3CDF33BEFF2FD3EECC9BDD1B0B8B699E73AAC0F2B34C209E241B10A17515CB90", 16),
+            new BigInteger("3E3920857D936F0571AD5E2A809C904FDA6B9AFDCE871D7A020074D7B14EA9D8", 16),
+            new BigInteger("175939"),
+            new BigInteger("8820D85C52D06395F8091EAD8773E891E1CF18990909DAF4949F6CA305BE4D9E", 16)),
     BRAINPOOLP384R1Twist(
             new BigInteger(
                     "6CFE5AB49B37D0798AA4265B02F40E9060764FAF2B96E9475CD58FB0A6E8B6D16A1A540430076E0E67D9399AA29B0084",
@@ -92,6 +138,12 @@ public enum TwistedCurvePoint {
                     "6B9BF25564CA942268D7EA63CCA04206FD7DD292C06538101F457E6CBB114B1FB151B249419234CDB026EB65BB1164C0",
                     16), new BigInteger("241"), NamedGroup.BRAINPOOLP384R1, new BigInteger(
                     "494C3442B50BD2543CBCE52C1C2210B8312667155D1E26262F45FFB85D216F2C6987B0D7DDB991156B4B3D473ECF81FA",
+                    16), new BigInteger(
+                    "10362D53362BB7AB2008AC9997840ED4D39FBED87FDA799C1913448C5EA62651E33C929CFCE879895662B1F6C1D386",
+                    16), new BigInteger(
+                    "4C154117E789F768ADD650BBF179B07C7CE8153C2512A8BBBAF09CBEA63C01097298B4395368A4DA0950E8BD6F932D60",
+                    16), new BigInteger("5557"), new BigInteger(
+                    "6EA882743A226AE62B2CE3D537E668048F04EC087A2AE4B9A729539331281B15A4AA7A36DAC33E70ABB722541A1D6DA0",
                     16)),
     BRAINPOOLP512R1Twist(
             new BigInteger(
@@ -104,6 +156,16 @@ public enum TwistedCurvePoint {
             NamedGroup.BRAINPOOLP512R1,
             new BigInteger(
                     "40B0D551038B96AD5557B4F4DBEA9CA80EDE1CAB267D90581D92EB7C40D1CA4F2C6C0543A283A87FD19BD7EA24E4908AD2B589589549F7015898DC99D6F43EDD",
+                    16),
+            new BigInteger(
+                    "97BAD04F24F0EB201824955FC15F36E3BCF49C3726D5C89C6E4A917C4E3D657F9AF54F9909634812372CC1B5F48047C94066C1FF32CCACC33D4361314A4740FE",
+                    16),
+            new BigInteger(
+                    "8F70C463B9826339789C24425AF5B2DFE20F6AFA14C5CF6A4838E6678E4AEF281A41F7F56B582E19039B2B3D41A3000F37E137D0891EB9DDA5B90EE4F8F5A809",
+                    16),
+            new BigInteger("41"),
+            new BigInteger(
+                    "D7B587C358E8E65D547BFDE5B27BE741B942EBD4467D2F8E586E90636920EB23EED57630583C3D9D1030858E0A1AADF12D3104CDE6A6E030DE90B035F1CBA08",
                     16)),
     // X-Curves use point of order 4 for evaluation of server behavior
     X25519Twist(
@@ -111,7 +173,11 @@ public enum TwistedCurvePoint {
             new BigInteger("5B8545C0F22DFADE38855A5CD1228352F134A9E655D637C03704BDE426506941", 16),
             new BigInteger("4"),
             NamedGroup.ECDH_X25519,
-            new BigInteger("CA6648A697DC4F37B1BB5C5809E9F265332D9C6138371C0809B54D69C303AC7", 16)),
+            new BigInteger("CA6648A697DC4F37B1BB5C5809E9F265332D9C6138371C0809B54D69C303AC7", 16),
+            null,
+            null,
+            null,
+            null),
     X448Twist(
             new BigInteger("1", 16),
             new BigInteger(
@@ -121,7 +187,11 @@ public enum TwistedCurvePoint {
             NamedGroup.ECDH_X448,
             new BigInteger(
                     "F151DA48F37BACE95DEE7E0F6F2477C60C131264C2A5B900D214C76115C10CC86A22E33E6C07933F6369E8544580C6780F256EE77F8F3513",
-                    16));
+                    16),
+            null,
+            null,
+            null,
+            null);
     private BigInteger publicPointBaseX;
 
     /**
@@ -143,8 +213,14 @@ public enum TwistedCurvePoint {
     private NamedGroup intendedNamedGroup;
     private BigInteger order;
 
+    private BigInteger redundantBaseX;
+    private BigInteger redundantBaseY;
+    private BigInteger redundantOrder;
+    private BigInteger redundantD;
+
     private TwistedCurvePoint(BigInteger publicPointBaseX, BigInteger publicPointBaseY, BigInteger order,
-            NamedGroup intendedNamedGroup, BigInteger d) {
+            NamedGroup intendedNamedGroup, BigInteger d, BigInteger redundantBaseX, BigInteger redundantBaseY,
+            BigInteger redundantOrder, BigInteger redundantD) {
         this.publicPointBaseX = publicPointBaseX;
         this.publicPointBaseY = publicPointBaseY;
         this.order = order;
@@ -250,5 +326,37 @@ public enum TwistedCurvePoint {
             default:
                 return true;
         }
+    }
+
+    public BigInteger getRedundantBaseX() {
+        return redundantBaseX;
+    }
+
+    public void setRedundantBaseX(BigInteger redundantBaseX) {
+        this.redundantBaseX = redundantBaseX;
+    }
+
+    public BigInteger getRedundantBaseY() {
+        return redundantBaseY;
+    }
+
+    public void setRedundantBaseY(BigInteger redundantBaseY) {
+        this.redundantBaseY = redundantBaseY;
+    }
+
+    public BigInteger getRedundantOrder() {
+        return redundantOrder;
+    }
+
+    public void setRedundantOrder(BigInteger redundantOrder) {
+        this.redundantOrder = redundantOrder;
+    }
+
+    public BigInteger getRedundantD() {
+        return redundantD;
+    }
+
+    public void setRedundantD(BigInteger redundantD) {
+        this.redundantD = redundantD;
     }
 }
