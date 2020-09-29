5f25652a17cf0e7320ca1bcea08195b7e1c5f6ae
ic0ns <robert.merget@rub.de>
2020-09-29 16:51:38 +0200

    renamed function

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/AlgorithmResolver.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/AlgorithmResolver.java
@@ -172,7 +172,7 @@ public class AlgorithmResolver {
      * @param suite
      * @return
      */
-    public CertificateKeyType getCertificateType(CipherSuite suite) {
+    public CertificateKeyType getCertificateKeyType(CipherSuite suite) {
         KeyExchangeAlgorithm keyExchangeAlgorithm = getKeyExchangeAlgorithm(suite);
         switch (keyExchangeAlgorithm) {
             case DHE_RSA:
