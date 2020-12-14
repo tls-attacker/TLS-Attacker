ee0f112f3e30543d9136ed981061cc4df6612d76
Jannis Pinter <jannis.pinter@rub.de>
2020-12-14 09:41:56 +0100

    Rewrite SignedCertificateTimestampListParser to extend Parser

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/certificate/transparency/SignedCertificateTimestampListParser.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/certificate/transparency/SignedCertificateTimestampListParser.java
@@ -9,37 +9,44 @@
  */
 package de.rub.nds.tlsattacker.core.certificate.transparency;
 
-import de.rub.nds.asn1.parser.ParserException;
+import de.rub.nds.tlsattacker.core.protocol.parser.Parser;
 import org.bouncycastle.crypto.tls.Certificate;
 
-import java.util.Arrays;
+public class SignedCertificateTimestampListParser extends Parser<SignedCertificateTimestampList> {
 
-public class SignedCertificateTimestampListParser {
+    private boolean isPreCertificateSct;
+    private Certificate certificateChain;
 
-    public static SignedCertificateTimestampList parseTimestampList(byte[] encodedTimestampList,
-            Certificate certificateChain, boolean isPreCertificateSct) throws ParserException {
+    /**
+     * Constructor for the Parser
+     *
+     * @param startposition Position in the array from which the Parser should start
+     *                      working
+     * @param array
+     */
+    public SignedCertificateTimestampListParser(int startposition, byte[] array, Certificate certificateChain,
+                                                boolean isPreCertificateSct) {
+        super(startposition, array);
 
+        this.isPreCertificateSct = isPreCertificateSct;
+        this.certificateChain = certificateChain;
+    }
+
+    @Override
+    public SignedCertificateTimestampList parse() {
         SignedCertificateTimestampList sctList = new SignedCertificateTimestampList();
-        sctList.setEncodedTimestampList(encodedTimestampList);
 
         org.bouncycastle.asn1.x509.Certificate leafCertificate = certificateChain.getCertificateAt(0);
         org.bouncycastle.asn1.x509.Certificate issuerCertificate = certificateChain.getCertificateAt(1);
 
-        // Extract total length of certificate timestamp list
-        int length = encodedTimestampList[0] << 8 | encodedTimestampList[1] & 0x00ff;
-
-        // Use index value to navigate through variable-length encoded SCT list
-        // and skip first 2 bytes (length field)
-        int index = 2;
-
         // Decode and parse every list entry
-        while (index < length) {
+        while (getBytesLeft() > 0) {
 
             // Determine length of variable-length encoded SCT entry
-            int entryLength = encodedTimestampList[index] << 8 | encodedTimestampList[index + 1] & 0x00ff;
+            int entryLength = parseIntField(2);
 
             // Decode and parse Signed Certificate Timestamp list entry
-            byte[] encodedEntryData = Arrays.copyOfRange(encodedTimestampList, index + 2, index + 2 + entryLength);
+            byte[] encodedEntryData = parseByteArrayField(entryLength);
             SignedCertificateTimestampParser signedCertificateTimestampParser = new SignedCertificateTimestampParser(0,
                     encodedEntryData);
             SignedCertificateTimestamp sct = signedCertificateTimestampParser.parse();
@@ -57,11 +64,9 @@ public class SignedCertificateTimestampListParser {
 
             // Add parsed SCT to the SCT-List data structure
             sctList.getCertificateTimestampList().add(sct);
-
-            // Adjust index for beginning of next entry
-            index += 2 + entryLength;
         }
 
+        sctList.setEncodedTimestampList(getAlreadyParsed());
         return sctList;
     }
 }
