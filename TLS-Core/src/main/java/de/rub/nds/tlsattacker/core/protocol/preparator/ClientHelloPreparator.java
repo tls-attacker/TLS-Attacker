a2c42171e8c5956ead664a3094a41bf635deedad
Nurullah Erinola <n.erinola@gmx.de>
2021-01-25 18:48:10 +0100

    Adapted cookie and random checks

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/preparator/ClientHelloPreparator.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/preparator/ClientHelloPreparator.java
@@ -155,11 +155,11 @@ public class ClientHelloPreparator extends HelloMessagePreparator<ClientHelloMes
     }
 
     private boolean hasClientRandom() {
-        return chooser.getContext().getClientRandom() != null;
+        return chooser.getContext().getClientRandom() != null && chooser.getContext().getClientRandom().length != 0;
     }
 
     private boolean hasHandshakeCookie() {
-        return chooser.getContext().getDtlsCookie() != null;
+        return chooser.getContext().getDtlsCookie() != null && chooser.getContext().getDtlsCookie().length != 0;
     }
 
     private void prepareCookie(ClientHelloMessage msg) {
