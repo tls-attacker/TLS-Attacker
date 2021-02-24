ca8acb5ca6d5ef9a2962a64d8de0f6bfa3ed0a74
ic0ns <robert.merget@rub.de>
2021-02-24 12:31:02 +0100

    added a constructor which accepts a Config value

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/message/extension/SessionTicketTLSExtensionMessage.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/message/extension/SessionTicketTLSExtensionMessage.java
@@ -13,6 +13,7 @@ package de.rub.nds.tlsattacker.core.protocol.message.extension;
 import de.rub.nds.modifiablevariable.ModifiableVariableFactory;
 import de.rub.nds.modifiablevariable.ModifiableVariableProperty;
 import de.rub.nds.modifiablevariable.bytearray.ModifiableByteArray;
+import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.ExtensionType;
 
 /**
@@ -27,6 +28,10 @@ public class SessionTicketTLSExtensionMessage extends ExtensionMessage {
      * Constructor
      */
     public SessionTicketTLSExtensionMessage() {
+        super(ExtensionType.SESSION_TICKET);
+    }
+
+    public SessionTicketTLSExtensionMessage(Config config) {
         super(ExtensionType.SESSION_TICKET);
     }
 
