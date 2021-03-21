2b7d865c9fdefe919439451972cda36b3ce09b4f
ic0ns <robert.merget@rub.de>
2021-03-21 02:38:22 +0100

    added xml root element annotation

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/https/HttpsResponseMessage.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/https/HttpsResponseMessage.java
@@ -29,7 +29,9 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlElements;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 public class HttpsResponseMessage extends ProtocolMessage {
 
     private ModifiableString responseProtocol;
