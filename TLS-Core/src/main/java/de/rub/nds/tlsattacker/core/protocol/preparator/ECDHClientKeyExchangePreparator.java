a555acc302f21bc5133907cca5872d9104590f05
ic0ns <robert.merget@rub.de>
2021-03-23 17:45:09 +0100

    formatted

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/preparator/ECDHClientKeyExchangePreparator.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/preparator/ECDHClientKeyExchangePreparator.java
@@ -6,6 +6,7 @@
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
+
 package de.rub.nds.tlsattacker.core.protocol.preparator;
 
 import de.rub.nds.modifiablevariable.util.ArrayConverter;
@@ -23,7 +24,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ECDHClientKeyExchangePreparator<T extends ECDHClientKeyExchangeMessage>
-        extends ClientKeyExchangePreparator<T> {
+    extends ClientKeyExchangePreparator<T> {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -49,7 +50,7 @@ public class ECDHClientKeyExchangePreparator<T extends ECDHClientKeyExchangeMess
         if (curve instanceof RFC7748Curve) {
             RFC7748Curve rfc7748Curve = (RFC7748Curve) curve;
             return rfc7748Curve.computeSharedSecretFromDecodedPoint(msg.getComputations().getPrivateKey().getValue(),
-                    publicKey);
+                publicKey);
         } else {
             Point sharedPoint = curve.mult(privateKey, publicKey);
             if (sharedPoint == null) {
@@ -69,14 +70,14 @@ public class ECDHClientKeyExchangePreparator<T extends ECDHClientKeyExchangeMess
     protected void preparePremasterSecret(T msg) {
         msg.getComputations().setPremasterSecret(premasterSecret);
         LOGGER.debug("PremasterSecret: "
-                + ArrayConverter.bytesToHexString(msg.getComputations().getPremasterSecret().getValue()));
+            + ArrayConverter.bytesToHexString(msg.getComputations().getPremasterSecret().getValue()));
     }
 
     protected void prepareClientServerRandom(T msg) {
         random = ArrayConverter.concatenate(chooser.getClientRandom(), chooser.getServerRandom());
         msg.getComputations().setClientServerRandom(random);
         LOGGER.debug("ClientServerRandom: "
-                + ArrayConverter.bytesToHexString(msg.getComputations().getClientServerRandom().getValue()));
+            + ArrayConverter.bytesToHexString(msg.getComputations().getClientServerRandom().getValue()));
     }
 
     @Override
@@ -117,7 +118,7 @@ public class ECDHClientKeyExchangePreparator<T extends ECDHClientKeyExchangeMess
             msg.getComputations().setPublicKeyX(publicKey.getFieldX().getData());
             msg.getComputations().setPublicKeyY(publicKey.getFieldY().getData());
             publicKey = curve.getPoint(msg.getComputations().getPublicKeyX().getValue(),
-                    msg.getComputations().getPublicKeyY().getValue());
+                msg.getComputations().getPublicKeyY().getValue());
             publicKeyBytes = PointFormatter.formatToByteArray(usedGroup, publicKey, pointFormat);
         }
         msg.setPublicKey(publicKeyBytes);
