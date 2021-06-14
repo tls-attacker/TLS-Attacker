75e9604528ee67955c51462a61830afaec8c22b5
markus <aldurimhecker@gmail.com>
2021-06-14 18:32:35 +0200

    This commit is an update about the recent changes in the RecordStreamCipherTest class
    The following methods were created:
    calculateMacSHA,
    testEncryptTLSv10,
    testDecryptTLSv10,
    calculateMacMD5
    
    The following implemented methods are finished:
    calculateMacSHA,
    calculateMacMD5

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/record/cipher/RecordStreamCipherTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/record/cipher/RecordStreamCipherTest.java
@@ -9,22 +9,28 @@
  */
 package de.rub.nds.tlsattacker.core.record.cipher;
 
+import de.rub.nds.modifiablevariable.util.ArrayConverter;
 import de.rub.nds.tlsattacker.core.connection.AliasedConnection;
 import de.rub.nds.tlsattacker.core.connection.InboundConnection;
 import de.rub.nds.tlsattacker.core.connection.OutboundConnection;
-import de.rub.nds.tlsattacker.core.constants.AlgorithmResolver;
-import de.rub.nds.tlsattacker.core.constants.CipherSuite;
-import de.rub.nds.tlsattacker.core.constants.CipherType;
-import de.rub.nds.tlsattacker.core.constants.ProtocolVersion;
+import de.rub.nds.tlsattacker.core.constants.*;
 import de.rub.nds.tlsattacker.core.exceptions.CryptoException;
+import de.rub.nds.tlsattacker.core.record.Record;
+import de.rub.nds.tlsattacker.core.record.cipher.cryptohelper.KeySet;
 import de.rub.nds.tlsattacker.core.record.cipher.cryptohelper.KeySetGenerator;
 import de.rub.nds.tlsattacker.core.state.TlsContext;
+import de.rub.nds.tlsattacker.transport.ConnectionEndType;
 import de.rub.nds.tlsattacker.util.UnlimitedStrengthEnabler;
+
+import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 public class RecordStreamCipherTest {
 
@@ -74,4 +80,172 @@ public class RecordStreamCipherTest {
         }
     }
 
+    @Test
+    public void calculateMacSHA() throws CryptoException {
+        context.setConnection(new OutboundConnection());
+        context.setSelectedCipherSuite(CipherSuite.TLS_RSA_WITH_RC4_128_SHA);
+        context.setSelectedProtocolVersion(ProtocolVersion.TLS10);
+
+        byte[] data = ArrayConverter
+                .hexStringToByteArray("01010101010101010101010101010101");
+
+        KeySet keySet = new KeySet();
+        keySet.setClientWriteKey(ArrayConverter
+                .hexStringToByteArray("DEADBEEFC0FEDEADBEEFC0FEDEADBEEF"));
+        keySet.setClientWriteMacSecret(ArrayConverter.hexStringToByteArray("DEADBEEFC0FEDEADBEEFC0FEDEADBEEFC0FEDEAD"));
+        keySet.setClientWriteIv(new byte[8]);
+        keySet.setServerWriteIv(new byte[8]); // ServerSide is not used
+        keySet.setServerWriteKey(new byte[16]); // ServerSide is not used
+        keySet.setServerWriteMacSecret(new byte[20]); // ServerSide is not used
+
+        RecordStreamCipher cipher = new RecordStreamCipher(context,
+                keySet);
+
+        assertArrayEquals(ArrayConverter
+                        .hexStringToByteArray("740b1374aac883ec9171730684b9f7bf84c56cc1"),
+                cipher.calculateMac(data, context.getTalkingConnectionEndType()));
+
+        context.setConnection(new InboundConnection());
+        cipher = new RecordStreamCipher(context, keySet);
+
+        assertArrayEquals(ArrayConverter
+                        .hexStringToByteArray("740b1374aac883ec9171730684b9f7bf84c56cc1"),
+                cipher.calculateMac(data, context.getTalkingConnectionEndType()));
+    }
+
+
+    @Test
+    public void testEncryptTLSv10() throws CryptoException, NoSuchAlgorithmException {
+        /* Outbound for Clients, Inbound for Servers */
+        context.setConnection(new OutboundConnection());
+        /* Sets the Ciphersuit for the Handshake */
+        context.setSelectedCipherSuite(CipherSuite.TLS_RSA_WITH_RC4_128_SHA);
+        /* Sets the SSL/TLS version */
+        context.setSelectedProtocolVersion(ProtocolVersion.TLS10);
+
+        /* Sets the data that should be encrypted later */
+        byte[] data = ArrayConverter
+                .hexStringToByteArray("01010101010101010101010101010101");
+
+        /* Sets a new keySet
+        a keySet contains the negotiated keys out of the pseudorandom bit stream*/
+        KeySet keySet = new KeySet();
+        /* Since we're on the client side we're setting EncWriteClient and MacWriteClient only
+        * Note that we have to consider the key length for the rc4 cipher since it can be used with different lengths
+        * rc4 can have a key length from 40 bit up to 256 bit
+        * First we're generating the keys for RC4 with SHA*/
+        keySet.setClientWriteKey(ArrayConverter
+                .hexStringToByteArray("DEADBEEFC0FEDEADBEEFC0FEDEADBEEF"));
+        /*The key length for HMAC-SHA are 20 bytes*/
+        keySet.setClientWriteMacSecret(ArrayConverter.hexStringToByteArray("DEADBEEFC0FEDEADBEEFC0FEDEADBEEFC0FEDEAD"));
+        keySet.setClientWriteIv(new byte[8]);           // RC4 is not a block cipher so we don't need an iv
+        keySet.setServerWriteIv(new byte[8]);           // ServerSide is not used
+        keySet.setServerWriteKey(new byte[16]);         // ServerSide is not used
+        keySet.setServerWriteMacSecret(new byte[20]);   // ServerSide is not used
+
+        RecordStreamCipher cipher = new RecordStreamCipher(context,
+                keySet);
+
+        Record record = new Record();
+        record.setContentType(ProtocolMessageType.HANDSHAKE.getValue());
+        record.prepareComputations();
+        /* sequence numbers are 64-bits long */
+        record.setSequenceNumber(new BigInteger("0"));
+        record.setProtocolVersion(ProtocolVersion.TLS10.getValue());
+        record.setCleanProtocolMessageBytes(data);
+
+        /* the cipher is computed over the plaintext(data) and the mac
+        * the mac is computed over the AuthenticatedMetaData + plaintext + LocalConnectionEndType*/
+        cipher.encrypt(record);
+        System.out.println(record.getComputations().getAuthenticatedMetaData());
+
+        /* checks the meta data of the record
+        * the MAC is computed from the MAC secret,
+        * the sequence number,                  00 00 00 00 00 00 00 00 ---+
+        * the type field,                                            16 ---+
+        * the protocol version,                                   03 01 ---+---> authenticated meta data +---+
+        * the message length,                                     00 10 ---+                                 |
+        * the message contents,                          01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 01 +---+---> HMAC ---> hash
+        * and two fixed character strings                                                  opad and ipad +---+
+        *  .*/
+        assertArrayEquals(ArrayConverter.hexStringToByteArray("00000000000000001603010010"), record.getComputations()
+                .getAuthenticatedMetaData().getValue());
+
+        assertArrayEquals(ArrayConverter
+                        .hexStringToByteArray("805264444f48ea5b98a0ceb3884c2ef78fa0cb307f1e7b1beef68fa824907314075768e4"),
+                record.getComputations().getCiphertext().getValue());
+
+
+    }
+
+    @Test
+    public void testDecryptTLSv10() throws CryptoException {
+        context.setConnection(new InboundConnection());
+        context.setSelectedCipherSuite(CipherSuite.TLS_RSA_WITH_RC4_128_SHA);
+        context.setSelectedProtocolVersion(ProtocolVersion.TLS10);
+
+        byte[] data = ArrayConverter
+                .hexStringToByteArray("805264444f48ea5b98a0ceb3884c2ef78fa0cb307f1e7b1beef68fa824907314075768e4");
+
+        KeySet keySet = new KeySet();
+        keySet.setClientWriteKey(ArrayConverter
+                .hexStringToByteArray("DEADBEEFC0FEDEADBEEFC0FEDEADBEEF"));
+        keySet.setClientWriteMacSecret(ArrayConverter
+                .hexStringToByteArray("DEADBEEFC0FEDEADBEEFC0FEDEADBEEFC0FEDEAD"));
+        keySet.setClientWriteIv(new byte[8]);
+        keySet.setServerWriteIv(new byte[8]); // ServerSide is not used
+        keySet.setServerWriteKey(new byte[16]); // ServerSide is not used
+        keySet.setServerWriteMacSecret(new byte[20]); // ServerSide is not used
+
+        RecordStreamCipher plaintext = new RecordStreamCipher(context,
+                keySet);
+
+        Record record = new Record();
+        record.setContentType(ProtocolMessageType.HANDSHAKE.getValue());
+        record.prepareComputations();
+        record.setSequenceNumber(new BigInteger("0"));
+        record.setProtocolVersion(ProtocolVersion.TLS10.getValue());
+        record.setProtocolMessageBytes(data);
+
+
+
+        plaintext.decrypt(record);
+
+        assertArrayEquals(ArrayConverter
+                        .hexStringToByteArray("01010101010101010101010101010101"),
+                record.getCleanProtocolMessageBytes().getValue());
+    }
+
+    @Test
+    public void calculateMacMD5() throws CryptoException {
+        context.setConnection(new OutboundConnection());
+        context.setSelectedCipherSuite(CipherSuite.TLS_RSA_WITH_RC4_128_MD5);
+        context.setSelectedProtocolVersion(ProtocolVersion.TLS10);
+
+        byte[] data = ArrayConverter
+                .hexStringToByteArray("01010101010101010101010101010101");
+
+        KeySet keySet = new KeySet();
+        keySet.setClientWriteKey(ArrayConverter
+                .hexStringToByteArray("DEADBEEFC0FEDEADBEEFC0FEDEADBEEF"));
+        keySet.setClientWriteMacSecret(ArrayConverter.hexStringToByteArray("DEADBEEFC0FEDEADBEEFC0FEDEADBEEFC0FEDEAD"));
+        keySet.setClientWriteIv(new byte[8]);
+        keySet.setServerWriteIv(new byte[8]); // ServerSide is not used
+        keySet.setServerWriteKey(new byte[16]); // ServerSide is not used
+        keySet.setServerWriteMacSecret(new byte[20]); // ServerSide is not used
+
+        RecordStreamCipher cipher = new RecordStreamCipher(context,
+                keySet);
+
+        assertArrayEquals(ArrayConverter
+                        .hexStringToByteArray("6af39a238e82675131e6a383f801674e"),
+                cipher.calculateMac(data, context.getTalkingConnectionEndType()));
+
+        context.setConnection(new InboundConnection());
+        cipher = new RecordStreamCipher(context, keySet);
+
+        assertArrayEquals(ArrayConverter
+                        .hexStringToByteArray("6af39a238e82675131e6a383f801674e"),
+                cipher.calculateMac(data, context.getTalkingConnectionEndType()));
+    }
 }
