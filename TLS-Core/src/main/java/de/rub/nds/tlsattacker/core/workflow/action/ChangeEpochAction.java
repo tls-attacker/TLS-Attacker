bf67d6454aa933e88b20cdbbd392370d55d352bc
Nurullah Erinola <n.erinola@gmx.de>
2021-10-12 12:37:23 +0200

    Added new actions

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/workflow/action/ChangeEpochAction.java
@@ -0,0 +1,53 @@
+/**
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2021 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package de.rub.nds.tlsattacker.core.workflow.action;
+
+import de.rub.nds.tlsattacker.core.exceptions.WorkflowExecutionException;
+import de.rub.nds.tlsattacker.core.state.State;
+import de.rub.nds.tlsattacker.core.state.TlsContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class ChangeEpochAction extends ConnectionBoundAction {
+
+    protected static final Logger LOGGER = LogManager.getLogger();
+
+    protected Integer epoch = null;
+
+    public ChangeEpochAction() {
+    }
+
+    public ChangeEpochAction(int epoch) {
+        this.epoch = epoch;
+    }
+
+    protected abstract void changeEpoch(TlsContext tlsContext);
+
+    @Override
+    public void execute(State state) throws WorkflowExecutionException {
+        TlsContext tlsContext = state.getTlsContext(getConnectionAlias());
+
+        if (isExecuted()) {
+            throw new WorkflowExecutionException("Action already executed!");
+        }
+        changeEpoch(tlsContext);
+        setExecuted(true);
+    }
+
+    @Override
+    public void reset() {
+        setExecuted(null);
+    }
+
+    @Override
+    public boolean executedAsPlanned() {
+        return isExecuted();
+    }
+}
