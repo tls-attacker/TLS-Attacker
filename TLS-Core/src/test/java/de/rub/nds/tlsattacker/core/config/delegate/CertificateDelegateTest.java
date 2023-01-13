4de70d8023da6013cd77106409a070ceee606121
Robert Merget <robert.merget@tii.ae>
2023-01-13 17:35:54 +0400

    made project compile again (without tests)

--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/config/delegate/CertificateDelegateTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/config/delegate/CertificateDelegateTest.java
@@ -1,7 +1,7 @@
 /*
  * TLS-Attacker - A Modular Penetration Testing Framework for TLS
  *
- * Copyright 2014-2022 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
@@ -135,10 +135,10 @@ public class CertificateDelegateTest extends AbstractDelegateTest<CertificateDel
                 args[3], delegate.getPassword(), "Password parameter gets not parsed correctly");
         assertEquals(args[5], delegate.getAlias(), "Alias parameter gets not parsed correctly");
         Config config = Config.createConfig();
-        config.setDefaultExplicitCertificateKeyPair(null);
+        config.setDefaultExplicitCertificateChain(null);
         delegate.applyDelegate(config);
         assertNotNull(
-                config.getDefaultExplicitCertificateKeyPair(), "Certificate could not be loaded");
+                config.getDefaultExplicitCertificateChain(), "Certificate could not be loaded");
     }
 
     @Test
@@ -153,7 +153,7 @@ public class CertificateDelegateTest extends AbstractDelegateTest<CertificateDel
                 args[1], delegate.getPassword(), "Password parameter gets not parsed correctly");
         assertEquals(args[3], delegate.getAlias(), "Alias parameter gets not parsed correctly");
         Config config = Config.createConfig();
-        config.setDefaultExplicitCertificateKeyPair(null);
+        config.setDefaultExplicitCertificateChain(null);
 
         ParameterException exception =
                 assertThrows(ParameterException.class, () -> delegate.applyDelegate(config));
--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/integration/handshakes/AbstractHandshakeIT.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/integration/handshakes/AbstractHandshakeIT.java
@@ -1,7 +1,7 @@
 /*
  * TLS-Attacker - A Modular Penetration Testing Framework for TLS
  *
- * Copyright 2014-2022 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
@@ -392,7 +392,6 @@ public abstract class AbstractHandshakeIT {
         config.setDefaultSelectedCipherSuite(cipherSuite);
         config.setDefaultServerNamedGroups(namedGroup);
         config.setDefaultSelectedNamedGroup(namedGroup);
-        config.setPreferredCertificateSignatureGroup(namedGroup);
         config.setDefaultEcCertificateCurve(namedGroup);
         config.setHighestProtocolVersion(protocolVersion);
         config.setDefaultSelectedProtocolVersion(protocolVersion);
