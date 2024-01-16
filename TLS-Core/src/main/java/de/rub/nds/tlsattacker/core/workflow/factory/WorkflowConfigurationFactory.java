fb88e9c28a7ee60ae90c578995b42717784ddc9c
NErinola <nurullah.erinola@rub.de>
2024-01-16 11:48:12 +0100

    Added missing quic frame

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/workflow/factory/WorkflowConfigurationFactory.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/workflow/factory/WorkflowConfigurationFactory.java
@@ -1316,6 +1316,9 @@ public class WorkflowConfigurationFactory {
                 trace.addTlsAction(
                         MessageActionFactory.createTLSAction(
                                 config, connection, ConnectionEndType.CLIENT, tls13Messages));
+                if (config.getExpectHandshakeDoneQuicFrame()) {
+                    trace.addTlsAction(new ReceiveQuicTillAction(new HandshakeDoneFrame()));
+                }
             } else {
                 if (Objects.equals(config.isClientAuthentication(), Boolean.TRUE)) {
                     trace.addTlsAction(new SendAction(new CertificateMessage()));
