fc6fdaa14a231d58198b478b2a2891ffe1e9ecf3
Robert Merget <robert.merget@tii.ae>
2024-04-19 12:53:50 +0400

    added unknown attribute model

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/stun/model/UnknownAttribute.java
@@ -0,0 +1,72 @@
+/*
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.tlsattacker.core.stun.model;
+
+import de.rub.nds.modifiablevariable.ModifiableVariableFactory;
+import de.rub.nds.modifiablevariable.bytearray.ModifiableByteArray;
+import de.rub.nds.tlsattacker.core.layer.data.Handler;
+import de.rub.nds.tlsattacker.core.layer.data.Parser;
+import de.rub.nds.tlsattacker.core.layer.data.Preparator;
+import de.rub.nds.tlsattacker.core.layer.data.Serializer;
+import de.rub.nds.tlsattacker.core.stun.StunContext;
+import java.io.InputStream;
+
+public class UnknownAttribute extends StunAttribute {
+
+    private ModifiableByteArray unknownContent;
+
+    public UnknownAttribute() {
+        super();
+    }
+
+    public ModifiableByteArray getUnknownContent() {
+        return unknownContent;
+    }
+
+    public void setUnknownContent(ModifiableByteArray unknownContent) {
+        this.unknownContent = unknownContent;
+    }
+
+    public void setUnknownContent(byte[] unknownContent) {
+        this.unknownContent =
+                ModifiableVariableFactory.safelySetValue(this.unknownContent, unknownContent);
+    }
+
+    @Override
+    public Handler<?> getHandler(StunContext context) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Parser<?> getParser(StunContext context, InputStream stream) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Preparator<?> getPreparator(StunContext context) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Serializer<?> getSerializer(StunContext context) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ModifiableByteArray getContent() {
+        return content;
+    }
+
+    public void setContent(ModifiableByteArray content) {
+        this.content = content;
+    }
+}
