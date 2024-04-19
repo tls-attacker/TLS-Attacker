6b0f75ee1e0367505895dc920370a0970f7662ea
Robert Merget <robert.merget@tii.ae>
2024-04-19 12:53:20 +0400

    added model for fingerprint attribute

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/stun/model/FingerprintAttribute.java
@@ -0,0 +1,63 @@
+/*
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.tlsattacker.core.stun.model;
+
+import de.rub.nds.modifiablevariable.ModifiableVariableFactory;
+import de.rub.nds.modifiablevariable.bytearray.ModifiableByteArray;
+import de.rub.nds.tlsattacker.core.layer.data.Handler;
+import de.rub.nds.tlsattacker.core.layer.data.Parser;
+import de.rub.nds.tlsattacker.core.layer.data.Preparator;
+import de.rub.nds.tlsattacker.core.layer.data.Serializer;
+import de.rub.nds.tlsattacker.core.stun.StunContext;
+import java.io.InputStream;
+
+public class FingerprintAttribute extends StunAttribute {
+
+    /** 8 byte */
+    private ModifiableByteArray crcChecksum;
+
+    public FingerprintAttribute() {
+        super();
+    }
+
+    public ModifiableByteArray getCrcChecksum() {
+        return crcChecksum;
+    }
+
+    public void setCrcChecksum(ModifiableByteArray crcChecksum) {
+        this.crcChecksum = crcChecksum;
+    }
+
+    public void setCrcChecksum(byte[] crcChecksum) {
+        this.crcChecksum = ModifiableVariableFactory.safelySetValue(this.crcChecksum, crcChecksum);
+    }
+
+    @Override
+    public Handler<?> getHandler(StunContext context) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Parser<?> getParser(StunContext context, InputStream stream) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Preparator<?> getPreparator(StunContext context) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Serializer<?> getSerializer(StunContext context) {
+        // TODO Auto-generated method stub
+    }
+}
