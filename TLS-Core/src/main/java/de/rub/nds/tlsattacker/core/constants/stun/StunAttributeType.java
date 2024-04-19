2f394b3e70cc9e3a76a14ecaf458ca404b34f4d8
Robert Merget <robert.merget@tii.ae>
2024-04-19 10:44:54 +0400

    added stun attribute type

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/stun/StunAttributeType.java
@@ -0,0 +1,35 @@
+/*
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.tlsattacker.core.constants.stun;
+
+import de.rub.nds.modifiablevariable.util.ArrayConverter;
+
+public enum StunAttributeType {
+    MAPPED_ADDRESS("0001"),
+    RESPONSE_ADDRESS("0002"),
+    CHANGE_REQUEST("0003"),
+    SOURCE_ADDRESS("0004"),
+    CHANGED_ADDRESS("0005"),
+    USERNAME("0006"),
+    PASSWORD("0007"),
+    MESSAGE_INTEGRITY("0008"),
+    ERROR_CODE("0009"),
+    UNKNOWN_ATTRIBUTES("000A"),
+    REFLECTED_FROM("000B");
+
+    private byte[] value;
+
+    private StunAttributeType(String value) {
+        this.value = ArrayConverter.hexStringToByteArray(value);
+    }
+
+    public byte[] getValue() {
+        return value;
+    }
+}
