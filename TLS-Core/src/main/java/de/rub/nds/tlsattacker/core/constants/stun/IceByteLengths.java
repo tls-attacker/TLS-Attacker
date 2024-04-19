94de4c990e22d5581a1a6048930eebfe0354d899
Robert Merget <robert.merget@tii.ae>
2024-04-19 15:41:19 +0400

    added ice byte lengths constants

---
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/stun/IceByteLengths.java
@@ -0,0 +1,23 @@
+/*
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.tlsattacker.core.constants.stun;
+
+public class IceByteLengths {
+    public static final int STUN_MESSAGE_TYPE = 2;
+
+    public static final int STUN_MESSAGE_LENGTH = 2;
+
+    public static final int STUN_MAGIC_COOKIE = 4;
+
+    public static final int STUN_TRANSACTION_ID = 12;
+
+    public static final int STUN_ATTRIBUTE_TYPE = 2;
+
+    private IceByteLengths() {}
+}
