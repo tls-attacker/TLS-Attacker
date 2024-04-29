fbc0af108eed4d75a52af534c8fee6dd2a2095b5
Robert Merget <robert.merget@tii.ae>
2024-04-29 12:32:19 +0400

    added xor mask on crc32 attribute

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/stun/preparator/FingerprintAttributePreparator.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/stun/preparator/FingerprintAttributePreparator.java
@@ -8,13 +8,21 @@
  */
 package de.rub.nds.tlsattacker.core.stun.preparator;
 
+import java.util.zip.CRC32;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import de.rub.nds.modifiablevariable.util.ArrayConverter;
 import de.rub.nds.tlsattacker.core.constants.stun.IceByteLengths;
 import de.rub.nds.tlsattacker.core.stun.model.FingerprintAttribute;
 import de.rub.nds.tlsattacker.core.workflow.chooser.Chooser;
-import java.util.zip.CRC32;
 
 public class FingerprintAttributePreparator extends StunAttributePreparator<FingerprintAttribute> {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    private static final byte[] XOR_MASK = new byte[] { (byte) 0x53, (byte) 0x54, (byte) 0x55, (byte) 0x4e };
 
     public FingerprintAttributePreparator(Chooser chooser, FingerprintAttribute attribute) {
         super(chooser, attribute);
@@ -23,14 +31,23 @@ public class FingerprintAttributePreparator extends StunAttributePreparator<Fing
     @Override
     public void prepareContent() {
 
-        getObject().setCrcChecksum(computeCrc32Checksum());
+        getObject().setCrcChecksum(xorChecksumWithMask(computeCrc32Checksum()));
     }
 
     public byte[] computeCrc32Checksum() {
         byte[] transcript = chooser.getContext().getIceContext().getMessageTranscript();
         CRC32 crc32 = new CRC32();
         crc32.update(transcript);
+        System.out.println("Transcript:" + ArrayConverter.bytesToHexString(transcript));
         return ArrayConverter.longToBytes(
                 crc32.getValue(), IceByteLengths.STUN_FINGERPRINT_CRC_CHECKSUM);
+    }
+
+    private byte[] xorChecksumWithMask(byte[] checksum) {
+        byte[] xoredChecksum = new byte[checksum.length];
+        for (int i = 0; i < checksum.length; i++) {
+            xoredChecksum[i] = (byte) (checksum[i] ^ XOR_MASK[i]);
+        }
+        return xoredChecksum;
     }
 }
