bd7f4c1b7993fcb6bbb50e49a8ab387f53d37459
Tim Storm <tim.storm@gmx.net>
2024-07-17 15:27:36 +0200

    minor fixes

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/smtp/reply/SmtpNOOPReply.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/smtp/reply/SmtpNOOPReply.java
@@ -1,24 +1,13 @@
 package de.rub.nds.tlsattacker.core.smtp.reply;
 
 import de.rub.nds.tlsattacker.core.layer.context.SmtpContext;
-import de.rub.nds.tlsattacker.core.smtp.SmtpMessage;
-import de.rub.nds.tlsattacker.core.smtp.parser.NOOPReplyParser;
-import de.rub.nds.tlsattacker.core.smtp.parser.SmtpMessageParser;
 import de.rub.nds.tlsattacker.core.smtp.preparator.NOOPReplyPreparator;
-import de.rub.nds.tlsattacker.core.smtp.preparator.SmtpMessagePreparator;
 import jakarta.xml.bind.annotation.XmlRootElement;
-
-import java.io.InputStream;
 
 @XmlRootElement
 public class SmtpNOOPReply extends SmtpReply {
     //TODO: shift this to Config somehow
     private String noopMessage = "OK";
-
-    @Override
-    public NOOPReplyParser getParser(SmtpContext context, InputStream stream) {
-        return new NOOPReplyParser(stream);
-    }
 
     @Override
     public NOOPReplyPreparator getPreparator(SmtpContext context) {
--- TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/smtp/reply/NOOPReplyTest.java
+++ TLS-Core/src/test/java/de/rub/nds/tlsattacker/core/smtp/reply/NOOPReplyTest.java
@@ -10,21 +10,13 @@ package de.rub.nds.tlsattacker.core.smtp.reply;
 
 import de.rub.nds.tlsattacker.core.connection.OutboundConnection;
 import de.rub.nds.tlsattacker.core.layer.context.SmtpContext;
-import de.rub.nds.tlsattacker.core.layer.data.Preparator;
-import de.rub.nds.tlsattacker.core.layer.data.Serializer;
-import de.rub.nds.tlsattacker.core.smtp.extensions.ATRNExtension;
-import de.rub.nds.tlsattacker.core.smtp.extensions.HELPExtension;
-import de.rub.nds.tlsattacker.core.smtp.extensions.STARTTLSExtension;
-import de.rub.nds.tlsattacker.core.smtp.extensions._8BITMIMEExtension;
-import de.rub.nds.tlsattacker.core.smtp.parser.EHLOReplyParser;
-import de.rub.nds.tlsattacker.core.smtp.parser.NOOPReplyParser;
+import de.rub.nds.tlsattacker.core.layer.data.Parser;
 import de.rub.nds.tlsattacker.core.state.Context;
 import de.rub.nds.tlsattacker.core.state.State;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -37,7 +29,7 @@ class NOOPReplyTest {
 
         SmtpContext context = new SmtpContext(new Context(new State(), new OutboundConnection()));
         SmtpNOOPReply noop = new SmtpNOOPReply();
-        NOOPReplyParser parser =
+        Parser parser =
                 noop.getParser(
                         context,
                         new ByteArrayInputStream(stringMessage.getBytes(StandardCharsets.UTF_8)));
@@ -45,89 +37,5 @@ class NOOPReplyTest {
 
         assertEquals(250, noop.getReplyCode());
         assertEquals("NOOP acknowledged", noop.getHumanReadableMessage());
-    }
-
-    @Test
-    public void testParseSimpleNoGreeting() {
-        String stringMessage = "250 seal.cs.upb.de\r\n";
-
-        SmtpContext context = new SmtpContext(new Context(new State(), new OutboundConnection()));
-        SmtpEHLOReply ehlo = new SmtpEHLOReply();
-        EHLOReplyParser parser =
-                ehlo.getParser(
-                        context,
-                        new ByteArrayInputStream(stringMessage.getBytes(StandardCharsets.UTF_8)));
-        parser.parse(ehlo);
-
-        assertEquals(250, ehlo.getReplyCode());
-        assertEquals("seal.cs.upb.de", ehlo.getDomain());
-        assertNull(ehlo.getGreeting());
-    }
-
-    @Test
-    public void testParseMultipleLinesWithExtensions() {
-        String stringMessage =
-                "250-seal.cs.upb.de says Greetings\r\n"
-                        + "250-8BITMIME\r\n"
-                        + "250-SIZE 12345678\r\n"
-                        + "250-STARTTLS\r\n"
-                        + "250 HELP\r\n";
-
-        SmtpContext context = new SmtpContext(new Context(new State(), new OutboundConnection()));
-        SmtpEHLOReply ehlo = new SmtpEHLOReply();
-        EHLOReplyParser parser =
-                ehlo.getParser(
-                        context,
-                        new ByteArrayInputStream(stringMessage.getBytes(StandardCharsets.UTF_8)));
-        parser.parse(ehlo);
-
-        assertEquals(250, ehlo.getReplyCode());
-        assertEquals("seal.cs.upb.de", ehlo.getDomain());
-        assertEquals("says Greetings", ehlo.getGreeting());
-        assertEquals(4, ehlo.getExtensions().size());
-        assertEquals("8BITMIME", ehlo.getExtensions().get(0).getEhloKeyword());
-        // TODO: Parse the extension parameters
-        // assertEquals("SIZE 12345678", ehlo.getExtensions().get(1).getEhloKeyword());
-        assertEquals("STARTTLS", ehlo.getExtensions().get(2).getEhloKeyword());
-        assertEquals("HELP", ehlo.getExtensions().get(3).getEhloKeyword());
-    }
-
-    @Test
-    void serializeSimple() {
-        SmtpEHLOReply ehlo = new SmtpEHLOReply();
-        ehlo.setReplyCode(250);
-        ehlo.setDomain("seal.cs.upb.de");
-        ehlo.setGreeting("says Greetings");
-
-        SmtpContext context = new SmtpContext(new Context(new State(), new OutboundConnection()));
-        Preparator preparator = ehlo.getPreparator(context);
-        Serializer serializer = ehlo.getSerializer(context);
-        preparator.prepare();
-        serializer.serialize();
-        assertEquals(
-                "250 seal.cs.upb.de says Greetings\r\n", serializer.getOutputStream().toString());
-    }
-
-    @Test
-    void serializeWithExtensions() {
-        SmtpEHLOReply ehlo = new SmtpEHLOReply();
-        ehlo.setReplyCode(250);
-        ehlo.setDomain("seal.cs.upb.de");
-        ehlo.setGreeting("says Greetings");
-        ehlo.setExtensions(
-                List.of(
-                        new _8BITMIMEExtension(),
-                        new ATRNExtension(),
-                        new STARTTLSExtension(),
-                        new HELPExtension()));
-
-        SmtpContext context = new SmtpContext(new Context(new State(), new OutboundConnection()));
-        Preparator preparator = ehlo.getPreparator(context);
-        Serializer serializer = ehlo.getSerializer(context);
-        preparator.prepare();
-        serializer.serialize();
-        assertEquals(
-                "250-seal.cs.upb.de says Greetings\r\n250-8BITMIME\r\n250-ATRN\r\n250-STARTTLS\r\n250 HELP\r\n",
-                serializer.getOutputStream().toString());
     }
 }
