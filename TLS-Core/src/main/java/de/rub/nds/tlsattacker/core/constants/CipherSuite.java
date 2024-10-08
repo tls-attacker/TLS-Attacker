11115c17cfd73de3a9be761acb6a7187a7089e3d
Robert Merget <robert.merget@tii.ae>
2024-10-08 10:48:10 +0400

    formatted

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/CipherSuite.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/constants/CipherSuite.java
@@ -11,6 +11,7 @@ package de.rub.nds.tlsattacker.core.constants;
 import de.rub.nds.modifiablevariable.util.ArrayConverter;
 import de.rub.nds.protocol.constants.HashAlgorithm;
 import de.rub.nds.tlsattacker.core.exceptions.UnknownCipherSuiteException;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -3299,7 +3300,7 @@ public enum CipherSuite {
                 return false;
             }
             String[] hashFunctionNames = {
-                "MD5", "SHA", "SHA256", "SHA384", "SHA512", "IMIT", "GOSTR3411"
+                    "MD5", "SHA", "SHA256", "SHA384", "SHA512", "IMIT", "GOSTR3411"
             };
             for (String hashFunction : hashFunctionNames) {
                 if (cipher.endsWith(hashFunction)) {
