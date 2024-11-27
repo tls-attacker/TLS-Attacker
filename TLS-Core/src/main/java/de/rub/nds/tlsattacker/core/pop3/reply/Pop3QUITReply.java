7ab26f47b0f42f9e2b2a32ac129d6ec3220f1793
Yunat1 <reissmuellerm@gmail.com>
2024-11-27 12:54:50 +0100

    Applied spotless

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/pop3/reply/Pop3QUITReply.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/pop3/reply/Pop3QUITReply.java
@@ -1,14 +1,22 @@
+/*
+ * TLS-Attacker - A Modular Penetration Testing Framework for TLS
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
 package de.rub.nds.tlsattacker.core.pop3.reply;
 
 import de.rub.nds.tlsattacker.core.layer.context.Pop3Context;
-import de.rub.nds.tlsattacker.core.pop3.parser.reply.Pop3ReplyParser;
 import de.rub.nds.tlsattacker.core.pop3.parser.reply.QUITReplyParser;
-
 import java.io.InputStream;
 
 public class Pop3QUITReply extends Pop3Reply {
 
-    public Pop3QUITReply() {super();}
+    public Pop3QUITReply() {
+        super();
+    }
 
     @Override
     public QUITReplyParser getParser(Pop3Context context, InputStream stream) {
