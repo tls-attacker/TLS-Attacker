5b467a60bb0116e6b2b03d0005d2bc6c7ffcd989
mmaehren <32199075+mmaehren@users.noreply.github.com>
2025-02-03 15:17:34 +0100

    Use modvar when determining extension content length

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/preparator/extension/ExtensionPreparator.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/preparator/extension/ExtensionPreparator.java
@@ -68,7 +68,7 @@ public abstract class ExtensionPreparator<T extends ExtensionMessage> extends Pr
     }
 
     private void prepareExtensionLength(ExtensionMessage msg) {
-        msg.setExtensionLength(content.length);
+        msg.setExtensionLength(msg.getExtensionContent().getValue().length);
         LOGGER.debug("ExtensionLength: " + msg.getExtensionLength().getValue());
     }
 
