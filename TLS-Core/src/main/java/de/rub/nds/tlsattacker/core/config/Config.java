a5271c9683f0be02d1f51c8fedc29835c7f02995
Robert Merget <robert.merget@tii.ae>
2025-06-16 23:09:51 +0400

    fix: use BigInteger.valueOf(2) instead of new BigInteger("2") for SRP generator
    Replace new BigInteger("2") with BigInteger.valueOf(2) for better performance.
    Using valueOf is more efficient for small integers.

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/config/Config.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/config/Config.java
@@ -959,7 +959,7 @@ public class Config implements Serializable {
                     ArrayConverter.hexStringToByteArray(
                             "EEAF0AB9ADB38DD69C33F80AFA8FC5E86072618775FF3C0B9EA2314C9C256576D674DF7496EA81D3383B4813D692C6E0E0D5D8E250B98BE48E495C1D6089DAD15DC7D7B46154D6B6CE8EF4AD69B15D4982559B297BCF1885C529F566660E57EC68EDBC3C05726CC02FD4CBF4976EAA9AFD5138FE8376435B9FC61D2FC0EB06E3"));
 
-    private BigInteger defaultSRPGenerator = new BigInteger("2");
+    private BigInteger defaultSRPGenerator = BigInteger.valueOf(2);
 
     private BigInteger defaultSRPServerPrivateKey = new BigInteger("3");
 
