95ea4c22c910a4da5d955c3d64e5059c8716335b
Robert Merget <robert.merget@tii.ae>
2025-06-16 23:24:34 +0400

    fix: remove unused LOGGER field from NewConnectionIdHandler
    Remove unused private static final LOGGER field and its imports
    to clean up the code and fix PMD UnusedPrivateField violation.

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/handler/NewConnectionIdHandler.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/handler/NewConnectionIdHandler.java
@@ -11,12 +11,9 @@ package de.rub.nds.tlsattacker.core.protocol.handler;
 import de.rub.nds.tlsattacker.core.constants.ConnectionIdUsage;
 import de.rub.nds.tlsattacker.core.layer.context.TlsContext;
 import de.rub.nds.tlsattacker.core.protocol.message.NewConnectionIdMessage;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class NewConnectionIdHandler extends HandshakeMessageHandler<NewConnectionIdMessage> {
 
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public NewConnectionIdHandler(TlsContext tlsContext) {
         super(tlsContext);
