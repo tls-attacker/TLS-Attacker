616ca92e641f3c608628b57f5ffbeba344ca2acc
Nico Heitmann <heitnico@mail.upb.de>
2026-06-03 10:37:28 +0200

    Use enhanced switch in MessageFactory.

--- TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/MessageFactory.java
+++ TLS-Core/src/main/java/de/rub/nds/tlsattacker/core/protocol/MessageFactory.java
@@ -28,135 +28,77 @@ public class MessageFactory {
 
     public static HandshakeMessage generateHandshakeMessage(
             HandshakeMessageType type, TlsContext tlsContext) {
-        switch (type) {
-            case CERTIFICATE:
-                return new CertificateMessage();
-            case CERTIFICATE_REQUEST:
-                return new CertificateRequestMessage();
-            case CERTIFICATE_STATUS:
-                return new CertificateStatusMessage();
-            case CERTIFICATE_VERIFY:
-                return new CertificateVerifyMessage();
-            case CLIENT_HELLO:
-                return new ClientHelloMessage();
-            case CLIENT_KEY_EXCHANGE:
-                return getClientKeyExchangeMessage(tlsContext);
-            case ENCRYPTED_EXTENSIONS:
-                return new EncryptedExtensionsMessage();
-            case END_OF_EARLY_DATA:
-                return new EndOfEarlyDataMessage();
-            case FINISHED:
-                return new FinishedMessage();
-            case HELLO_REQUEST:
-                return new HelloRequestMessage();
-            case HELLO_VERIFY_REQUEST:
-                return new HelloVerifyRequestMessage();
-            case KEY_UPDATE:
-                return new KeyUpdateMessage();
-            case MESSAGE_HASH:
+        return switch (type) {
+            case CERTIFICATE -> new CertificateMessage();
+            case CERTIFICATE_REQUEST -> new CertificateRequestMessage();
+            case CERTIFICATE_STATUS -> new CertificateStatusMessage();
+            case CERTIFICATE_VERIFY -> new CertificateVerifyMessage();
+            case CLIENT_HELLO -> new ClientHelloMessage();
+            case CLIENT_KEY_EXCHANGE -> getClientKeyExchangeMessage(tlsContext);
+            case ENCRYPTED_EXTENSIONS -> new EncryptedExtensionsMessage();
+            case END_OF_EARLY_DATA -> new EndOfEarlyDataMessage();
+            case FINISHED -> new FinishedMessage();
+            case HELLO_REQUEST -> new HelloRequestMessage();
+            case HELLO_VERIFY_REQUEST -> new HelloVerifyRequestMessage();
+            case KEY_UPDATE -> new KeyUpdateMessage();
+            case MESSAGE_HASH -> {
                 LOGGER.warn(
                         "Received MessageHash HandshakeMessageType - not implemented yet. Treating as UnknownHandshakeMessage");
-                return new UnknownHandshakeMessage();
-            case NEW_SESSION_TICKET:
-                return new NewSessionTicketMessage();
-            case SERVER_HELLO:
-                return new ServerHelloMessage();
-            case SERVER_HELLO_DONE:
-                return new ServerHelloDoneMessage();
-            case SERVER_KEY_EXCHANGE:
-                return getServerKeyExchangeMessage(tlsContext);
-            case UNKNOWN:
-                return new UnknownHandshakeMessage();
-            case SUPPLEMENTAL_DATA:
-                return new SupplementalDataMessage();
-            default:
-                throw new RuntimeException("Unexpected HandshakeMessage Type " + type);
-        }
+                yield new UnknownHandshakeMessage();
+            }
+            case NEW_SESSION_TICKET -> new NewSessionTicketMessage();
+            case SERVER_HELLO -> new ServerHelloMessage();
+            case SERVER_HELLO_DONE -> new ServerHelloDoneMessage();
+            case SERVER_KEY_EXCHANGE -> getServerKeyExchangeMessage(tlsContext);
+            case UNKNOWN -> new UnknownHandshakeMessage();
+            case SUPPLEMENTAL_DATA -> new SupplementalDataMessage();
+            default -> throw new RuntimeException("Unexpected HandshakeMessage Type " + type);
+        };
     }
 
     private static ServerKeyExchangeMessage getServerKeyExchangeMessage(TlsContext tlsContext) {
         CipherSuite cs = tlsContext.getChooser().getSelectedCipherSuite();
         KeyExchangeAlgorithm algorithm = cs.getKeyExchangeAlgorithm();
-        if (algorithm == null) {
-            throw new UnsupportedOperationException(
-                    "CipherSuite '" + cs + "'does not have a KeyExchangeAlgorithm");
-        }
-        switch (algorithm) {
-            case ECDHE_ECDSA:
-            case ECDH_ECDSA:
-            case ECDH_RSA:
-            case ECDHE_RSA:
-            case ECDH_ANON:
-                return new ECDHEServerKeyExchangeMessage();
-            case DHE_DSS:
-            case DHE_RSA:
-            case DH_ANON:
-            case DH_DSS:
-            case DH_RSA:
-                return new DHEServerKeyExchangeMessage();
-            case RSA:
-            case RSA_EXPORT:
-                return new RSAServerKeyExchangeMessage();
-            case PSK:
-                return new PskServerKeyExchangeMessage();
-            case DHE_PSK:
-                return new PskDheServerKeyExchangeMessage();
-            case ECDHE_PSK:
-                return new PskEcDheServerKeyExchangeMessage();
-            case SRP_SHA_DSS:
-            case SRP_SHA_RSA:
-            case SRP_SHA:
-                return new SrpServerKeyExchangeMessage();
-            case ECCPWD:
-                return new PWDServerKeyExchangeMessage();
-            default:
-                throw new UnsupportedOperationException(
-                        "Algorithm " + algorithm + " NOT supported yet.");
-        }
+        return switch (algorithm) {
+            case null ->
+                    throw new UnsupportedOperationException(
+                            "CipherSuite '" + cs + "'does not have a KeyExchangeAlgorithm");
+            case ECDHE_ECDSA, ECDH_ECDSA, ECDH_RSA, ECDHE_RSA, ECDH_ANON ->
+                    new ECDHEServerKeyExchangeMessage();
+            case DHE_DSS, DHE_RSA, DH_ANON, DH_DSS, DH_RSA -> new DHEServerKeyExchangeMessage();
+            case RSA, RSA_EXPORT -> new RSAServerKeyExchangeMessage();
+            case PSK -> new PskServerKeyExchangeMessage();
+            case DHE_PSK -> new PskDheServerKeyExchangeMessage();
+            case ECDHE_PSK -> new PskEcDheServerKeyExchangeMessage();
+            case SRP_SHA_DSS, SRP_SHA_RSA, SRP_SHA -> new SrpServerKeyExchangeMessage();
+            case ECCPWD -> new PWDServerKeyExchangeMessage();
+            default ->
+                    throw new UnsupportedOperationException(
+                            "Algorithm " + algorithm + " NOT supported yet.");
+        };
     }
 
     private static ClientKeyExchangeMessage getClientKeyExchangeMessage(TlsContext tlsContext) {
         CipherSuite cs = tlsContext.getChooser().getSelectedCipherSuite();
         KeyExchangeAlgorithm algorithm = cs.getKeyExchangeAlgorithm();
-        if (algorithm == null) {
-            throw new UnsupportedOperationException(
-                    "CipherSuite '" + cs + "'does not have a KeyExchangeAlgorithm");
-        }
-        switch (algorithm) {
-            case RSA:
-                return new RSAClientKeyExchangeMessage();
-            case ECDHE_ECDSA:
-            case ECDH_ECDSA:
-            case ECDH_RSA:
-            case ECDHE_RSA:
-                return new ECDHClientKeyExchangeMessage();
-            case DHE_DSS:
-            case DHE_RSA:
-            case DH_ANON:
-            case DH_DSS:
-            case DH_RSA:
-                return new DHClientKeyExchangeMessage();
-            case DHE_PSK:
-                return new PskDhClientKeyExchangeMessage();
-            case ECDHE_PSK:
-                return new PskEcDhClientKeyExchangeMessage();
-            case RSA_PSK:
-                return new PskRsaClientKeyExchangeMessage();
-            case PSK:
-                return new PskClientKeyExchangeMessage();
-            case SRP_SHA_DSS:
-            case SRP_SHA_RSA:
-            case SRP_SHA:
-                return new SrpClientKeyExchangeMessage();
-            case VKO_GOST01:
-            case VKO_GOST12:
-                return new GOSTClientKeyExchangeMessage();
-            case ECCPWD:
-                return new PWDClientKeyExchangeMessage();
-            default:
-                throw new UnsupportedOperationException(
-                        "Algorithm " + algorithm + " NOT supported yet.");
-        }
+        return switch (algorithm) {
+            case null ->
+                    throw new UnsupportedOperationException(
+                            "CipherSuite '" + cs + "'does not have a KeyExchangeAlgorithm");
+            case RSA -> new RSAClientKeyExchangeMessage();
+            case ECDHE_ECDSA, ECDH_ECDSA, ECDH_RSA, ECDHE_RSA -> new ECDHClientKeyExchangeMessage();
+            case DHE_DSS, DHE_RSA, DH_ANON, DH_DSS, DH_RSA -> new DHClientKeyExchangeMessage();
+            case DHE_PSK -> new PskDhClientKeyExchangeMessage();
+            case ECDHE_PSK -> new PskEcDhClientKeyExchangeMessage();
+            case RSA_PSK -> new PskRsaClientKeyExchangeMessage();
+            case PSK -> new PskClientKeyExchangeMessage();
+            case SRP_SHA_DSS, SRP_SHA_RSA, SRP_SHA -> new SrpClientKeyExchangeMessage();
+            case VKO_GOST01, VKO_GOST12 -> new GOSTClientKeyExchangeMessage();
+            case ECCPWD -> new PWDClientKeyExchangeMessage();
+            default ->
+                    throw new UnsupportedOperationException(
+                            "Algorithm " + algorithm + " NOT supported yet.");
+        };
     }
 
     public static List<ProtocolMessage> generateProtocolMessages() {
